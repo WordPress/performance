d2e9cb3673f0dc307fa042293b126895e5e4fb75
Felix Arntz <felixarntz@google.com>
2021-12-01 12:17:56 -0800

    Scaffold PHPUnit infrastructure using wp-env.

---
+++ tests/load-tests.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Tests for load.php
+ *
+ * @package performance-lab
+ */
+
+class Load_Tests extends WP_UnitTestCase {
+
+	public function testDemo() {
+		$this->assertTrue( true );
+	}
+}
