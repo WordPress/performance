d8618e47ec3b38449c6f111f4b7d9fe37263f5a6
Justin Ahinon <justiny.ahinon@gmail.com>
2022-01-24 09:40:41 +0100

    Base implementation for the new site health module

---
+++ modules/site-health/performance-metrics/load.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Module Name: Performance Metrics
+ * Description: Adds a new tab to the Site Health page with key performance metrics.
+ * Experimental: No
+ *
+ * @package performance-lab
+ * @since 1.0.0
+ */
+
+/**
+ * Add a new tab to the Site Health page with key performance metrics.
+ */
+function performance_lab_add_site_health_metrics_tab($tabs) {
+	$tabs['performance-metrics-tab'] = esc_html_x('Performance Metrics', 'Site Health', 'performance-lab');
+
+	return $tabs;
+}
+add_filter('site_health_navigation_tabs', 'performance_lab_add_site_health_metrics_tab');
+
+/**
+ * Gathering the performance metrics.
+ */
+function performance_lab_get_site_health_metrics() {
+	$metrics = array();
+
+	$metrics['loaded-assets'] = array(
+		'label' => esc_html__('Loaded Assets', 'performance-lab'),
+		'fields' => array(
+			'scripts-number' => array(
+				'label' => esc_html__('Number of loaded scripts', 'performance-lab'),
+				'value' => 0,
+			),
+			'scripts-size' => array(
+				'label' => esc_html__('Total size of loaded scripts', 'performance-lab'),
+				'value' => 0,
+			),
+			'styles-number' => array(
+				'label' => esc_html__('Number of loaded styles', 'performance-lab'),
+				'value' => 0,
+			),
+			'styles-size' => array(
+				'label' => esc_html__('Total size of loaded styles', 'performance-lab'),
+				'value' => 0,
+			),
+		),
+	);
+
+	$metrics['images'] = array(
+		'label' => esc_html__('Images', 'performance-lab'),
+		'fields' => array(
+			'webp-support' => array(
+				'label' => esc_html__('WebP Support', 'performance-lab'),
+				'value' => 'Yes',
+			),
+		),
+	);
+
+	return $metrics;
+}
+
+/**
+ * Add the content for the new tab.
+ */
+function performance_lab_add_site_health_metrics_tab_content($tab) {
+	if ('performance-metrics-tab' !== $tab) {
+		return;
+	}
+
+    // Include the view.
+	include trailingslashit(plugin_dir_path(__FILE__)) . 'performance-metrics-view.php';
+}
+add_filter('site_health_tab_content', 'performance_lab_add_site_health_metrics_tab_content');
+
---
+++ modules/site-health/performance-metrics/performance-metrics-view.php
@@ -0,0 +1,61 @@
+<?php
+$metrics = performance_lab_get_site_health_metrics();
+?>
+
+<div class="health-check-body health-check-debug-tab hide-if-no-js">
+	<h2>
+		<?php _e( 'Performance metrics' ); ?>
+	</h2>
+
+	<p>
+		<?php _e( 'This page shows details about performance metrics of your website.' )?>
+	</p>
+	<p>
+		<?php _e( 'If you want to export a handy list of all the information on this page, you can use the button below to copy it to the clipboard. You can then paste it in a text file and save it to your device, or paste it in an email exchange with a support engineer or theme/plugin developer for example.' ); ?>
+	</p>
+	
+
+	<?php
+	// The formatting helper still needs to be implemented.
+	?>
+	<div class="site-health-copy-buttons">
+		<div class="copy-button-wrapper">
+			<button type="button" class="button copy-button" data-clipboard-text="<?php echo esc_attr( $metrics ); ?>">
+				<?php _e( 'Copy site info to clipboard' ); ?>
+			</button>
+			<span class="success hidden" aria-hidden="true"><?php _e( 'Copied!' ); ?></span>
+		</div>
+	</div>
+
+	<div id="health-check-debug" class="health-check-accordion">
+		<?php
+		foreach ( $metrics as $section => $details ) {
+			if ( ! isset( $details['fields'] ) || empty( $details['fields'] ) ) {
+				continue;
+			}
+			?>
+
+			<h3 class="health-check-accordion-heading">
+				<button aria-expanded="true" class="health-check-accordion-trigger" aria-controls="health-check-accordion-block-<?php echo esc_attr( $section ); ?>" type="button">
+					<span class="title"><?php echo esc_html( $details['label'] ); ?></span>
+					<span class="icon"></span>
+				</button>
+			</h3>
+			<div id="health-check-accordion-block-<?php echo esc_attr( $section ); ?>" class="health-check-accordion-panel">
+				<table class="widefat striped health-check-table" role="presentation">
+					<tbody>
+						<?php
+						foreach ( $details['fields'] as $field_name => $field ) {
+							printf(
+								'<tr><td class="health-check-table-label">%s</td><td>%s</td></tr>',
+								esc_html( $field['label'] ),
+								esc_html( $field['value'] )
+							);
+						}
+						?>
+					</tbody>
+				</table>
+			</div>
+		<? } ?>
+	</div>
+</div>
