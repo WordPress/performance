8be4eb16792ba9ac394f5301bbaaaceb4c1c6aff
Adam Silverstein <adamjs@google.com>
2022-02-02 14:08:32 -0700

    Adjust single format to use image_editor_output_formats filter

--- modules/images/webp-uploads/load.php
+++ modules/images/webp-uploads/load.php
@@ -11,7 +11,8 @@
 /**
  * Filter the image editor default output format mapping.
  *
- * For uploaded JPEG images, map the default output format to WebP.
+ * Only applies when the image_editor_output_formats filter returns a single mime
+ * type. In that case, the mime type is used as the default output format.
  *
  * @since 1.0.0
  *
@@ -20,22 +21,28 @@
  * @param string $mime_type     The source image mime type.
  * @return string The new output format mapping.
  */
-function webp_uploads_filter_image_editor_output_format( $output_format, $filename, $mime_type ) {
-	// Only enable if the server supports WebP.
-	if ( ! wp_image_editor_supports( array( 'mime_type' => 'image/webp' ) ) ) {
+function filter_image_editor_output_format( $output_format, $filename, $mime_type ) {
+
+
+	// Only enable if WebP is the only output format.
+	$output_formats = apply_filters( 'image_editor_output_formats', array( 'image/webp', 'image/jpeg' ), $attachment_id, $context );
+	$default_format = $output_formats[0];
+
+	// Only enable if the server supports the output format.
+	if ( ! wp_image_editor_supports( array( 'mime_type' => $default_format ) ) ) {
 		return $output_format;
 	}
 
 	// WebP lossless support is still limited on servers, so only apply to JPEGs.
-	if ( 'image/jpeg' !== $mime_type ) {
+	if ( 'image/webp' === $default_format && 'image/jpeg' !== $mime_type ) {
 		return $output_format;
 	}
 
-	$output_format['image/jpeg'] = 'image/webp';
+	$output_format[ $mime_type ] = $default_format;
 
 	return $output_format;
 }
-add_filter( 'image_editor_output_format', 'webp_uploads_filter_image_editor_output_format', 10, 3 );
+add_filter( 'image_editor_output_format', 'filter_image_editor_output_format', 10, 3 );
 
 /**
  * Create additional mime types with uploaded images by hooking into the 'image_editor_output_formats' filter.
