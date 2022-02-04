3ca6f3c8d8023e7b9299e4e4e15d061ba1585ed2
Adam Silverstein <adamjs@google.com>
2022-02-03 22:50:54 -0700

    Enable multi mime type output

--- modules/images/webp-uploads/load.php
+++ modules/images/webp-uploads/load.php
@@ -23,10 +23,14 @@
  */
 function filter_image_editor_output_format( $output_format, $filename, $mime_type ) {
 
-
-	// Only enable if WebP is the only output format.
-	$output_formats = apply_filters( 'image_editor_output_formats', array( 'image/webp', 'image/jpeg' ), $attachment_id, $context );
+	// Get the default format.
+	$output_formats = apply_filters( 'image_editor_output_formats', array( 'image/webp', 'image/jpeg' ), $filename );
 	$default_format = $output_formats[0];
+
+	// If the file mime and output mime are the same, return the default format.
+	if ( $mime_type === $default_format ) {
+		return $output_format;
+	}
 
 	// Only enable if the server supports the output format.
 	if ( ! wp_image_editor_supports( array( 'mime_type' => $default_format ) ) ) {
@@ -38,11 +42,16 @@ function filter_image_editor_output_format( $output_format, $filename, $mime_typ
 		return $output_format;
 	}
 
-	$output_format[ $mime_type ] = $default_format;
+	// Skip conversion when creating the `-scaled` image (for large image uploads).
+	if ( preg_match( '/-scaled\..{3}.?$/', $filename ) ) {
+		return $output_format;
+	}
+
+	$output_format = array( $mime_type => $default_format );
 
 	return $output_format;
 }
-add_filter( 'image_editor_output_format', 'filter_image_editor_output_format', 10, 3 );
+add_filter( 'image_editor_output_format', 'filter_image_editor_output_format', 9, 3 );
 
 /**
  * Create additional mime types with uploaded images by hooking into the 'image_editor_output_formats' filter.
@@ -55,7 +64,6 @@ add_filter( 'image_editor_output_format', 'filter_image_editor_output_format', 1
  *
  * @since 1.0.0
  *
- *
  * @param array  $metadata      An array of attachment meta data.
  * @param int    $attachment_id Current attachment ID.
  * @param string $context       Additional context. Can be 'create' when metadata was initially created for new attachment
@@ -67,45 +75,99 @@ function filter_wp_generate_attachment_metadata( $metadata, $attachment_id, $con
 	if ( 'create' !== $context ) {
 		return $metadata;
 	}
+	$file = wp_get_original_image_path( $attachment_id );
+
 	/**
 	 * Filter the output image formats.
 	 *
 	 * @since 1.0.0
 	 * @param array  $output_formats The image editor output formats.
- 	 * @param int    $attachment_id  Current attachment ID.
- 	 * @param string $context        Additional context. Can be 'create' when metadata was initially created for new attachment
- 	 *                               or 'update' when the metadata was updated.
+	 * @param string $file           Path to the image.
 	 */
-	$output_formats = apply_filters( 'image_editor_output_formats', array( 'image/webp', 'image/jpeg' ), $attachment_id, $context );
+	$output_formats = apply_filters( 'image_editor_output_formats', array( 'image/webp', 'image/jpeg' ), $file );
+
+	// Create the full sized image in the new format.
+	$editor = wp_get_image_editor( $file );
+	if ( is_wp_error( $editor ) ) {
+		return $metadata;
+	}
+	$upload_mime_type = get_post_mime_type( $attachment_id );
+	$new_sizes        = array();
 
 	// When only a single mime output type is specified, the default mime type will be set
 	// using the 'image_editor_output_format' filter.
 	if ( 1 === sizeof( $output_formats ) ) {
-		return $metadata;
+		// If the output mime type doesn't match the source mime type, convert the
+		// full size image to the new mime type.
+		if ( $upload_mime_type !== $output_formats[0] ) {
+			$extension    = wp_get_default_extension_for_mime_type( $output_formats[0] );
+			$new_metadata = $editor->save( $editor->generate_filename( $extension ) );
+			array_push( $new_sizes, $new_metadata );
+		}
+	} else {
+
+		// Only handle jpeg and webp mime type files.
+		if ( 'image/jpeg' !== $upload_mime_type && 'image/webp' !== $upload_mime_type ) {
+			return $metadata;
+		}
+
+		// Generate the additional mime type images, skipping the first mime type
+		// which has already been created at this point.
+		for ( $i = 1; $i < sizeof( $output_formats ); $i++ ) {
+			$mime_type = $output_formats[ $i ];
+			// If the output mime type doesn't match the source mime type, convert the
+			// full size image to the new mime type.
+			// Generate all sub sizes in the mime type, preventing meta update.
+			$output_use_mime = function() use ( $upload_mime_type, $mime_type ) {
+				return array( $upload_mime_type => $mime_type );
+			};
+
+			// If the mime type doesn't match the original, create a full sized image.
+			if ( $upload_mime_type !== $mime_type ) {
+				// Construct the filename with the format image-{mime-extension}.{mime-extension}.
+				$extension = wp_get_default_extension_for_mime_type( $mime_type );
+				$filename  = $editor->generate_filename( $extension );
+
+				// Output files with the current mime type.
+				add_filter( 'image_editor_output_format', $output_use_mime );
+				$new_metadata = $editor->save( $filename );
+				remove_filter( 'image_editor_output_format', $output_use_mime );
+				array_push( $new_sizes, $new_metadata );
+			}
+
+			// Output files with the current mime type.
+			add_filter( 'image_editor_output_format', $output_use_mime );
+
+			// Don't update meta when creating these images, we will update it later.
+			add_filter( 'wp_update_attachment_metadata', 'use_original_image_meta_data', 10, 2 );
+			$new_metadata = wp_create_image_subsizes( $file, $attachment_id );
+
+			// Remove the filters to set output mime type and prevent meta update.
+			remove_filter( 'image_editor_output_format', $output_use_mime );
+			remove_filter( 'wp_update_attachment_metadata', 'use_original_image_meta_data', 10, 2 );
+
+			array_push( $new_sizes, $new_metadata );
+		}
 	}
 
-	// Only handle jpeg and webp mime type files.
-	$upload_mime_type = get_post_mime_type( $attachment_id );
-	if ( 'image/jpeg' !== $upload_mime_type && 'image/webp' !== $upload_mime_type ) {
-		return $metadata;
+	// Add the additional mime type images to the meta data with the 'sizes->variations' key.
+	if ( ! isset( $metadata['sizes']['variations'] ) ) {
+		$metadata['sizes']['variations'] = array();
 	}
-	$file = wp_get_original_image_path( $attachment_id );
-	$new_sizes = array();
-	// Generate the additional mime type images, skipping the first mime type.
-	for( $i = 1; $i < sizeof( $output_formats ); $i++ ) {
-		$mime_type = $output_formats[ $i ];
-		// Generate all sub sizes in the mime type, preventing meta update.
-		add_filter( 'wp_update_attachment_metadata', 'use_original_image_meta_data', 10, 2 );
-		$metadata = wp_create_image_subsizes( $file, $attachment_id );
-		remove_filter( 'wp_update_attachment_metadata', 'use_original_image_meta_data', 10, 2 );
-		array_push( $new_sizes, $metadata );
-	}
-	error_log( json_encode( $new_sizes, JSON_PRETTY_PRINT));
-	// Add the additional mime type images to the meta data with the 'additional_sizes' key.
+	$metadata['sizes']['variations'] = array_merge( $metadata['sizes']['variations'], $new_sizes );
+
 	return $metadata;
 }
 add_filter( 'wp_generate_attachment_metadata', 'filter_wp_generate_attachment_metadata', 10, 3 );
 
+/**
+ * Shim to prevent core from updating the image meta data when we
+ * create additional mime type images with `wp_create_image_subsizes`.
+ *
+ * @param array $metadata      Array of updated attachment meta data.
+ * @param int   $attachment_id Attachment ID.
+ * @return array $metadata Array of meta data.
+ */
 function use_original_image_meta_data( $metadata, $attachment_id ) {
 	$original_image_meta = wp_get_attachment_metadata( $attachment_id );
 	if ( ! empty( $original_image_meta ) ) {
