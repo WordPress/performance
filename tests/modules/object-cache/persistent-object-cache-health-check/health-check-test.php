54409698e336c7df2ac94f7ca10509ef7cf9b1e3
Till Krüss <till@kruss.io>
2022-02-17 14:50:24 -0800

    use prefixed names in tests

--- tests/modules/object-cache/persistent-object-cache-health-check/health-check-test.php
+++ tests/modules/object-cache/persistent-object-cache-health-check/health-check-test.php
@@ -12,7 +12,7 @@ class Object_Cache_Health_Check_Tests extends WP_UnitTestCase {
 		$bypassed = true;
 
 		add_filter(
-			'site_status_persistent_object_cache_thresholds',
+			'perflab_oc_site_status_persistent_object_cache_thresholds',
 			function ( $thresholds ) use ( $bypassed ) {
 				$bypassed = false;
 
@@ -20,12 +20,12 @@ class Object_Cache_Health_Check_Tests extends WP_UnitTestCase {
 			}
 		);
 
-		$result = oc_health_should_persistent_object_cache( true );
+		$result = perflab_oc_health_should_persistent_object_cache( true );
 
 		$this->assertTrue( $result );
 		$this->assertTrue( $bypassed );
 
-		remove_all_filters( 'site_status_persistent_object_cache_thresholds' );
+		remove_all_filters( 'perflab_oc_site_status_persistent_object_cache_thresholds' );
 	}
 
 	function test_object_cache_default_thresholds() {
@@ -36,107 +36,107 @@ class Object_Cache_Health_Check_Tests extends WP_UnitTestCase {
 
 	function test_object_cache_comments_threshold() {
 		add_filter(
-			'site_status_persistent_object_cache_thresholds',
+			'perflab_oc_site_status_persistent_object_cache_thresholds',
 			function ( $thresholds ) {
 				return array_merge( $thresholds, array( 'comments_count' => 0 ) );
 			}
 		);
 
-		$result = oc_health_should_persistent_object_cache( false );
+		$result = perflab_oc_health_should_persistent_object_cache( false );
 
 		$this->assertTrue( $result );
 
-		remove_all_filters( 'site_status_persistent_object_cache_thresholds' );
+		remove_all_filters( 'perflab_oc_site_status_persistent_object_cache_thresholds' );
 	}
 
 	function test_object_cache_posts_threshold() {
 		add_filter(
-			'site_status_persistent_object_cache_thresholds',
+			'perflab_oc_site_status_persistent_object_cache_thresholds',
 			function ( $thresholds ) {
 				return array_merge( $thresholds, array( 'posts_count' => 0 ) );
 			}
 		);
 
-		$result = oc_health_should_persistent_object_cache( false );
+		$result = perflab_oc_health_should_persistent_object_cache( false );
 
 		$this->assertTrue( $result );
 
-		remove_all_filters( 'site_status_persistent_object_cache_thresholds' );
+		remove_all_filters( 'perflab_oc_site_status_persistent_object_cache_thresholds' );
 	}
 
 	function test_object_cache_terms_threshold() {
 		add_filter(
-			'site_status_persistent_object_cache_thresholds',
+			'perflab_oc_site_status_persistent_object_cache_thresholds',
 			function ( $thresholds ) {
 				return array_merge( $thresholds, array( 'terms_count' => 1 ) );
 			}
 		);
 
-		$result = oc_health_should_persistent_object_cache( false );
+		$result = perflab_oc_health_should_persistent_object_cache( false );
 
 		$this->assertTrue( $result );
 
-		remove_all_filters( 'site_status_persistent_object_cache_thresholds' );
+		remove_all_filters( 'perflab_oc_site_status_persistent_object_cache_thresholds' );
 	}
 
 	function test_object_cache_options_threshold() {
 		add_filter(
-			'site_status_persistent_object_cache_thresholds',
+			'perflab_oc_site_status_persistent_object_cache_thresholds',
 			function ( $thresholds ) {
 				return array_merge( $thresholds, array( 'options_count' => 100 ) );
 			}
 		);
 
-		$result = oc_health_should_persistent_object_cache( false );
+		$result = perflab_oc_health_should_persistent_object_cache( false );
 
 		$this->assertTrue( $result );
 
-		remove_all_filters( 'site_status_persistent_object_cache_thresholds' );
+		remove_all_filters( 'perflab_oc_site_status_persistent_object_cache_thresholds' );
 	}
 
 	function test_object_cache_users_threshold() {
 		add_filter(
-			'site_status_persistent_object_cache_thresholds',
+			'perflab_oc_site_status_persistent_object_cache_thresholds',
 			function ( $thresholds ) {
 				return array_merge( $thresholds, array( 'users_count' => 0 ) );
 			}
 		);
 
-		$result = oc_health_should_persistent_object_cache( false );
+		$result = perflab_oc_health_should_persistent_object_cache( false );
 
 		$this->assertTrue( $result );
 
-		remove_all_filters( 'site_status_persistent_object_cache_thresholds' );
+		remove_all_filters( 'perflab_oc_site_status_persistent_object_cache_thresholds' );
 	}
 
 	function test_object_cache_alloptions_count_threshold() {
 		add_filter(
-			'site_status_persistent_object_cache_thresholds',
+			'perflab_oc_site_status_persistent_object_cache_thresholds',
 			function ( $thresholds ) {
 				return array_merge( $thresholds, array( 'alloptions_count' => 100 ) );
 			}
 		);
 
-		$result = oc_health_should_persistent_object_cache( false );
+		$result = perflab_oc_health_should_persistent_object_cache( false );
 
 		$this->assertTrue( $result );
 
-		remove_all_filters( 'site_status_persistent_object_cache_thresholds' );
+		remove_all_filters( 'perflab_oc_site_status_persistent_object_cache_thresholds' );
 	}
 
 	function test_object_cache_alloptions_bytes_threshold() {
 		add_filter(
-			'site_status_persistent_object_cache_thresholds',
+			'perflab_oc_site_status_persistent_object_cache_thresholds',
 			function ( $thresholds ) {
 				return array_merge( $thresholds, array( 'alloptions_bytes' => 1000 ) );
 			}
 		);
 
-		$result = oc_health_should_persistent_object_cache( false );
+		$result = perflab_oc_health_should_persistent_object_cache( false );
 
 		$this->assertTrue( $result );
 
-		remove_all_filters( 'site_status_persistent_object_cache_thresholds' );
+		remove_all_filters( 'perflab_oc_site_status_persistent_object_cache_thresholds' );
 	}
 
 }
