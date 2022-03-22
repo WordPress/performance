5278ccf9f902448fedddbfc6feeceee3a68b9053
Eugene Manuilov <manuilov@google.com>
2022-03-22 15:56:41 +0200

    Remove the array_reverse function call.

--- modules/images/webp-uploads/load.php
+++ modules/images/webp-uploads/load.php
@@ -612,8 +612,6 @@ function webp_uploads_img_tag_update_mime_type( $image, $context, $attachment_id
 	$target_mimes = apply_filters( 'webp_uploads_content_image_mimes', array( 'image/jpeg', 'image/webp' ), $attachment_id, $context );
 
 	$target_mime = null;
-	// Look for the most progressive image format first.
-	$target_mimes = array_reverse( $target_mimes );
 	foreach ( $target_mimes as $mime ) {
 		if ( isset( $metadata['sources'][ $mime ] ) ) {
 			$target_mime = $mime;
