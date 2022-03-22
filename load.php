8825a2f414236bd19991c8f68192c890c02d06d2
Felix Arntz <felixarntz@google.com>
2022-03-22 08:27:08 -0700

    Update plugin version number.

--- load.php
+++ load.php
@@ -5,7 +5,7 @@
  * Description: Performance plugin from the WordPress Performance Group, which is a collection of standalone performance modules.
  * Requires at least: 5.8
  * Requires PHP: 5.6
- * Version: 1.0.0-beta.2
+ * Version: 1.0.0-beta.3
  * Author: WordPress Performance Group
  * Author URI: https://make.wordpress.org/core/tag/performance/
  * License: GPLv2 or later
@@ -15,7 +15,7 @@
  * @package performance-lab
  */
 
-define( 'PERFLAB_VERSION', '1.0.0-beta.2' );
+define( 'PERFLAB_VERSION', '1.0.0-beta.3' );
 define( 'PERFLAB_MAIN_FILE', __FILE__ );
 define( 'PERFLAB_MODULES_SETTING', 'perflab_modules_settings' );
 define( 'PERFLAB_MODULES_SCREEN', 'perflab-modules' );
