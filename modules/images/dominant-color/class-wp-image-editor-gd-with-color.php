80aad46ec019af69fb901e6fdd98385d9ecfe1bf
pbearne <pbearne@gmail.com>
2022-03-31 10:51:56 -0400

    more linting fixes

--- modules/images/dominant-color/class-wp-image-editor-gd-with-color.php
+++ modules/images/dominant-color/class-wp-image-editor-gd-with-color.php
@@ -10,6 +10,8 @@
 class WP_Image_Editor_GD_With_Color extends WP_Image_Editor_GD {
 
 	/**
+	 * Get dominant color from a file.
+	 *
 	 * @param string $default_color default is light grey.
 	 *
 	 * @return string
