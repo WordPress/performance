5715c656cfca1e319a1b2fbf81797fe447b50c63
Paul Bearne <pbearne@gmail.com>
2022-04-21 12:43:11 -0400

    added return early to the images class function if no image object set

--- modules/images/dominant-color/class-dominant-color-image-editor-imagick.php
+++ modules/images/dominant-color/class-dominant-color-image-editor-imagick.php
@@ -71,7 +71,7 @@ class Dominant_Color_Image_Editor_Imagick extends WP_Image_Editor_Imagick {
 			return (bool) @$this->image->getImageAlphaChannel();
 		} catch ( Exception $e ) {
 
-			return true;
+			return false;
 		}
 
 		return false;
