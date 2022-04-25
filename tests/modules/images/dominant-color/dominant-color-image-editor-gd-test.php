bebdb7a0bde4b296f9254b1803bbc5151fa3e52c
Paul Bearne <pbearne@gmail.com>
2022-04-25 16:43:45 -0400

    Fixed test after changing default color

--- tests/modules/images/dominant-color/dominant-color-image-editor-gd-test.php
+++ tests/modules/images/dominant-color/dominant-color-image-editor-gd-test.php
@@ -97,7 +97,7 @@ class Dominant_Color_Image_Editor_GD_Test extends WP_UnitTestCase {
 			),
 			'trans_png'   => array(
 				'image_path'     => TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/dominant-color/trans.png',
-				'expected_color' => array( 'eee' ),
+				'expected_color' => array( 'eeeeee' ),
 			),
 			'trans4_png'  => array(
 				'image_path'     => TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/dominant-color/trans4.png',
@@ -118,7 +118,7 @@ class Dominant_Color_Image_Editor_GD_Test extends WP_UnitTestCase {
 			),
 			'trans_webp'  => array(
 				'image_path'     => TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/dominant-color/trans.webp',
-				'expected_color' => array( 'eee' ),
+				'expected_color' => array( 'eeeeee' ),
 			),
 			'trans4_webp' => array(
 				'image_path'     => TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/dominant-color/trans4.webp',
