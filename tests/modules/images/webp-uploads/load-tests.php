09c953eaa645cfe4a1b50b71d127d031e86359d7
jjgrainger <github@jjgrainger.co.uk>
2022-04-27 12:56:11 +0100

    add tests for preferr smaller image size

--- tests/modules/images/webp-uploads/load-tests.php
+++ tests/modules/images/webp-uploads/load-tests.php
@@ -570,4 +570,25 @@ class WebP_Uploads_Load_Tests extends ImagesTestCase {
 		$this->assertImageHasSizeSource( $attachment_id, 'thumbnail', 'image/jpeg' );
 		$this->assertImageHasSizeSource( $attachment_id, 'thumbnail', 'image/webp' );
 	}
+
+	/**
+	 * Allow the upload of a WebP image if at least one editor supports the format
+	 *
+	 * @test
+	 */
+	public function it_should_use_smaller_jpg_image_when_smaller_size_preferred() {
+		// Prefer smaller image size.
+		add_filter( 'webp_uploads_prefer_smaller_image_file', '__return_true' );
+
+		// File produces larger webp image, original jpg is smaller.
+		$attachment_id = $this->factory->attachment->create_upload_object( TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/leafs.jpg' );
+		$metadata      = wp_get_attachment_metadata( $attachment_id );
+		$tag           = wp_get_attachment_image( $attachment_id, 'full', false, array( 'class' => "wp-image-{$attachment_id}" ) );
+		$updated_tag   = webp_uploads_img_tag_update_mime_type( $tag, 'the_content', $attachment_id );
+
+		// The full size WebP is smaller.
+		$expected_tag = str_replace( $metadata['sources']['image/jpeg']['file'], $metadata['sources']['image/webp']['file'], $tag );
+
+		$this->assertSame( $expected_tag, $updated_tag );
+	}
 }
