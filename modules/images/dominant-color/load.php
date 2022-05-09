3cdc161130b57161cb05b972fbaa9b4ecd42e8f2
Paul Bearne <pbearne@gmail.com>
2022-05-09 15:42:53 -0400

    whitespace

--- modules/images/dominant-color/load.php
+++ modules/images/dominant-color/load.php
@@ -155,7 +155,7 @@ function dominant_color_img_tag_add_dominant_color( $filtered_image, $context, $
 			$style = ' style="--dominant-color: #' . $dominant_color . ';"';
 		}
 
-		$extra_class  = ( dominant_color_color_is_light( $dominant_color ) ) ? 'dominant-color-light' : 'dominant-color-dark';
+		$extra_class = ( dominant_color_color_is_light( $dominant_color ) ) ? 'dominant-color-light' : 'dominant-color-dark';
 		if ( isset( $image_meta['has_transparency'] ) && true === $image_meta['has_transparency'] ) {
 			$data        .= 'data-has-transparency="true" ';
 			$extra_class .= 'has-transparency';
