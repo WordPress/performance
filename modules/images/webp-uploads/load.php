38900a0718f70585e3a8feeae2749b230b0dd9e9
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2022-05-10 20:02:19 +0530

    Update modules/images/webp-uploads/load.php
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>

--- modules/images/webp-uploads/load.php
+++ modules/images/webp-uploads/load.php
@@ -543,7 +543,7 @@ function webp_uploads_img_tag_update_mime_type( $image, $context, $attachment_id
 		/** This filter is documented in modules/images/webp-uploads/load.php */
 		$filtered_image = (string) apply_filters( 'webp_uploads_pre_replace_additional_image_source', $image, $attachment_id, $size, $target_mime, $context );
 
-		// Check if filtered image is same as the image, replace the image otherwise not.
+		// If filtered image is same as the image, run our own replacement logic, otherwise rely on the filtered image.
 		if ( $filtered_image === $image ) {
 			$image = str_replace(
 				$size_data['file'],
