45ee13766d40ea8c7e363330e1edc1d345b03d99
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2022-05-24 09:36:27 +0530

    Update code

--- modules/images/webp-uploads/load.php
+++ modules/images/webp-uploads/load.php
@@ -418,7 +418,7 @@ function webp_uploads_update_image_references( $content ) {
 		// Get attachment id by image src
 		$updated_id = attachment_url_to_postid( $image_src[1] );
 		
-		if ( $attachment_id != $updated_id ) {
+		if ( $attachment_id !== $updated_id ) {
 			$updated_img = preg_replace( '/wp-image-([\d]+)/i', 'wp-image-'.$updated_id, $img );
 			$content = str_replace( $img, webp_uploads_img_tag_update_mime_type( $updated_img, 'the_content', $updated_id ), $content );
 		} else {
