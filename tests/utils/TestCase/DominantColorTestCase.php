360d44a859ce9ca5d2a59bfcc88dbf29d92efc5c
Paul Bearne <paul@bearne.com>
2022-06-02 16:17:30 +0100

    added additional colors to test

--- tests/utils/TestCase/DominantColorTestCase.php
+++ tests/utils/TestCase/DominantColorTestCase.php
@@ -15,12 +15,12 @@ abstract class DominantColorTestCase extends WP_UnitTestCase {
 
 			'red_jpg'     => array(
 				'image_path'     => TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/dominant-color/red.jpg',
-				'expected_color' => array( 'ff0505', 'ff0506' ),
+				'expected_color' => array( 'ff56', 'ff0505', 'ff0506' ),
 				'is_wp_error'    => false,
 			),
 			'green_jpg'   => array(
 				'image_path'     => TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/dominant-color/green.jpg',
-				'expected_color' => array( '1ee204' ),
+				'expected_color' => array( '1ee204', '1ee24' ),
 				'is_wp_error'    => false,
 			),
 			'white_jpg'   => array(
@@ -35,18 +35,18 @@ abstract class DominantColorTestCase extends WP_UnitTestCase {
 			),
 			'trans4_jpg'  => array(
 				'image_path'     => TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/dominant-color/trans4.jpg',
-				'expected_color' => array( 'd3febf' ),
+				'expected_color' => array( 'd3febf', 'd2febf' ),
 				'is_wp_error'    => false,
 			),
 
 			'red_gif'     => array(
 				'image_path'     => TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/dominant-color/red.gif',
-				'expected_color' => array( 'ff0505', 'ff0506' ),
+				'expected_color' => array( 'ff0505', 'ff55', 'ff0506' ),
 				'is_wp_error'    => false,
 			),
 			'green_gif'   => array(
 				'image_path'     => TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/dominant-color/green.gif',
-				'expected_color' => array( '1ee204' ),
+				'expected_color' => array( '1ee204', '1ee24' ),
 				'is_wp_error'    => false,
 			),
 			'white_gif'   => array(
@@ -61,13 +61,13 @@ abstract class DominantColorTestCase extends WP_UnitTestCase {
 			),
 			'trans4_gif'  => array(
 				'image_path'     => TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/dominant-color/trans4.gif',
-				'expected_color' => array( '133f00' ),
+				'expected_color' => array( '133f00', '4cfc0' ),
 				'is_wp_error'    => false,
 			),
 
 			'red_png'     => array(
 				'image_path'     => TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/dominant-color/red.png',
-				'expected_color' => array( 'ff0505', 'ff0506' ),
+				'expected_color' => array( 'ff0505', 'ff55', 'ff0506', 'ff56' ),
 				'is_wp_error'    => false,
 			),
 			'green_png'   => array(
