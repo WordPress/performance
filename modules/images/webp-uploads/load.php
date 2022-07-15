c7b64c66f0e4c800102751b829762671fe9ba4c2
Eugene Manuilov <manuilov@google.com>
2022-07-15 13:47:26 +0300

    Fixed infinit loop in the fallback mechanism for webp images.

--- modules/images/webp-uploads/load.php
+++ modules/images/webp-uploads/load.php
@@ -664,6 +664,7 @@ function webp_uploads_wepb_fallback() {
 		i = d.createElement( i );
 		i.src = p + 'jIAAABXRUJQVlA4ICYAAACyAgCdASoCAAEALmk0mk0iIiIiIgBoSygABc6zbAAA/v56QAAAAA==';
 		i.onload = function() {
+			i.onload = undefined;
 			i.src = p + 'h4AAABXRUJQVlA4TBEAAAAvAQAAAAfQ//73v/+BiOh/AAA=';
 		};
 
