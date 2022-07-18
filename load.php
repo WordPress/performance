1397e78e646eaec5020cf7902dcb50522c2fe5a5
Bethany Chobanian Lang <bethanylang@google.com>
2022-07-18 09:57:41 -0400

    Update language throughout the plugin from Group to Team and update appropriate capitalization

--- load.php
+++ load.php
@@ -2,11 +2,11 @@
 /**
  * Plugin Name: Performance Lab
  * Plugin URI: https://github.com/WordPress/performance
- * Description: Performance plugin from the WordPress Performance Group, which is a collection of standalone performance modules.
+ * Description: Performance plugin from the WordPress Performance Team, which is a collection of standalone performance modules.
  * Requires at least: 5.8
  * Requires PHP: 5.6
  * Version: 1.3.0
- * Author: WordPress Performance Group
+ * Author: WordPress Performance Team
  * Author URI: https://make.wordpress.org/core/tag/performance/
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
