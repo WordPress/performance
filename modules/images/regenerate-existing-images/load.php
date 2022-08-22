40a56198f6bad3b531986ac47480831f45f0f0d8
Ankit Gade <ankit.gade@rtcamp.com>
2022-08-22 19:10:46 +0530

    Add image regeneration module

---
+++ modules/images/regenerate-existing-images/load.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Module Name: Regenarate Existing Images
+ * Description: Regenerate existing images when the image sizes changes during plugin/theme
+ * activation/deactivation or for any other reasons.
+ * Experimental: No
+ *
+ * @since   n.e.x.t
+ * @package performance-lab
+ */
