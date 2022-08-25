e4ee10fcdf59dab4e3fbbdf361e2132aba470e50
Ankit Gade <ankit.gade@rtcamp.com>
2022-08-25 16:35:26 +0530

    Fix formatting issues

--- tests/modules/images/regenerate-existing-images/load-tests.php
+++ tests/modules/images/regenerate-existing-images/load-tests.php
@@ -9,28 +9,28 @@
 
 class Regenerate_Existing_Images_Load_Test extends WP_UnitTestCase {
 
-    /**
-     * Test that background_job taxonomy is present in system.
-     *
-     * @return void
-     */
-    public function test_background_job_taxonomy_exists() {
-        $taxonomies = get_taxonomies();
+	/**
+	 * Test that background_job taxonomy is present in system.
+	 *
+	 * @return void
+	 */
+	public function test_background_job_taxonomy_exists() {
+		$taxonomies = get_taxonomies();
 
-        $this->assertIsArray( $taxonomies );
-        $this->assertContains( 'background_job', $taxonomies );
-    }
+		$this->assertIsArray( $taxonomies );
+		$this->assertContains( 'background_job', $taxonomies );
+	}
 
-    /**
-     * Ensure taxonomy is non public and would not show in REST.
-     *
-     * @return void
-     */
-    public function test_background_job_taxonomy_is_non_public() {
-        $job_tax = get_taxonomy( 'background_job' );
+	/**
+	 * Ensure taxonomy is non public and would not show in REST.
+	 *
+	 * @return void
+	 */
+	public function test_background_job_taxonomy_is_non_public() {
+		$job_tax = get_taxonomy( 'background_job' );
 
-        $this->assertFalse( $job_tax->public );
-        $this->assertFalse( $job_tax->show_in_rest );
-        $this->assertFalse( $job_tax->hierarchical );
-    }
+		$this->assertFalse( $job_tax->public );
+		$this->assertFalse( $job_tax->show_in_rest );
+		$this->assertFalse( $job_tax->hierarchical );
+	}
 }
