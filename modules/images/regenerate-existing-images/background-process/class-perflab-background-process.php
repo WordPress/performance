3cc1a8ca37a8d132b7add0f7ea6a29da7261780b
Ankit Gade <ankit.gade@rtcamp.com>
2022-08-29 18:09:40 +0530

    BG process handler method

--- modules/images/regenerate-existing-images/background-process/class-perflab-background-process.php
+++ modules/images/regenerate-existing-images/background-process/class-perflab-background-process.php
@@ -13,6 +13,14 @@
  */
 class Perflab_Background_Process {
 	/**
+	 * Name of the action which will trigger background
+	 * process to run the job.
+	 *
+	 * @const string
+	 */
+	const BG_PROCESS_ACTION = 'background_process_handle_request';
+
+	/**
 	 * Job instance.
 	 *
 	 * @var Perflab_Background_Job
@@ -20,12 +28,67 @@ class Perflab_Background_Process {
 	private $job;
 
 	/**
-	 * Checks if the background process is completed.
+	 * Perflab_Background_Process constructor.
 	 *
-	 * @return bool
+	 * Adds the necessary hooks to trigger process handling.
+	 *
+	 * @return void
 	 */
-	public function completed() {
-		return false;
+	public function __construct() {
+		add_action( 'wp_ajax_' . Perflab_Background_Process::BG_PROCESS_ACTION, array( $this, 'handle_request' ) );
+		add_action( 'wp_ajax_nopriv' . Perflab_Background_Process::BG_PROCESS_ACTION, array( $this, 'handle_request' ) );
+	}
+
+	/**
+	 * Handle incoming request to run the batch of job.
+	 *
+	 * @throws Exception Invalid request for background process.
+	 *
+	 * @return void
+	 */
+	public function handle_request() {
+		try {
+
+			check_ajax_referer( Perflab_Background_Process::BG_PROCESS_ACTION, 'nonce' );
+
+			$job_id = isset( $_POST['job_id'] ) ? absint( sanitize_text_field( $_POST['job_id'] ) ) : 0;
+
+			// Job ID is mandatory to be specified.
+			if ( empty( $job_id ) ) {
+				throw new Exception( 'empty_background_job_id', __( 'No job specified to execute.', 'performance-lab' ) );
+			}
+
+			$this->job = new Perflab_Background_Job( $job_id );
+
+			// Checks if job is valid or not.
+			if ( $this->job->exists() && ! $this->job->completed() ) {
+				throw new Exception( 'invalid_background_job', __( 'This job may have been completed already or does not exist.', 'performance-lab' ) );
+			}
+
+			$this->lock();
+
+			// If everything seems fine, attempt to run the job.
+
+			$this->unlock();
+
+		} catch ( Exception $e ) {
+			$error = new WP_Error( 'background_job_failed', $e->getMessage() );
+			$this->record_error( $error );
+			$this->unlock();
+		}
+	}
+
+	/**
+	 * Records the error for the current process run.
+	 *
+	 * @param WP_Error $error Error instance.
+	 *
+	 * @todo Implement the error recording activity.
+	 *
+	 * @return void
+	 */
+	public function record_error( WP_Error $error ) {
+		update_term_meta( $this->job->job_id, 'job_status', 'failed' );
 	}
 
 	/**
@@ -52,7 +115,10 @@ class Perflab_Background_Process {
 	 * @return void
 	 */
 	private function lock() {
+		$time = microtime();
 
+		update_term_meta( $this->job->job_id, 'job_lock', $time );
+		update_term_meta( $this->job->job_id, 'job_status', 'running' );
 	}
 
 	/**
