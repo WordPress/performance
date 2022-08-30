b422b4c0d254915d6c2712d5f12eda25b2d84f24
OllieJones <olliejones@gmail.com>
2022-08-30 11:04:46 -0400

    Results simplification.

--- modules/site-health/audit-database/class-perflabdbtests.php
+++ modules/site-health/audit-database/class-perflabdbtests.php
@@ -51,6 +51,12 @@ class PerflabDbTests {
 	 */
 	private $table_stats;
 
+	/** Results accumulator.
+	 *
+	 * @var [string]
+	 */
+	private $results = array();
+
 	/** Constructor for tests.
 	 *
 	 * @param PerflabDbMetrics $metrics Metrics-retrieving instance.
@@ -71,32 +77,86 @@ class PerflabDbTests {
 	 * @return array Augmented tests.
 	 */
 	public function add_all_database_performance_checks( $tests ) {
-		$test_number = 0;
-		$label       = __( 'Database', 'performance-lab' );
-		$tests['direct'][ 'database_performance' . $test_number ++ ] = array(
-			'label' => $label,
-			'test'  => array( $this, 'server_version_test' ),
+		$tests['direct']['database_performance'] = array(
+			'label' => __( 'Database', 'performance-lab' ),
+			'test'  => array( $this, 'alltests' ),
 		);
-		$tests['direct'][ 'database_performance' . $test_number ++ ] = array(
-			'label' => $label,
-			'test'  => array( $this, 'minimal_server_response_test' ),
-		);
-		$tests['direct'][ 'database_performance' . $test_number ++ ] = array(
-			'label' => $label,
-			'test'  => array( $this, 'core_tables_format_test' ),
-		);
-
-		$tests['direct'][ 'database_performance' . $test_number ++ ] = array(
-			'label' => $label,
-			'test'  => array( $this, 'extra_tables_format_test' ),
-		);
-
-		$tests['direct'][ 'database_performance' . $test_number ++ ] = array(
-			'label' => $label,
-			'test'  => array( $this, 'buffer_pool_size_test' ),
-		);
-
 		return $tests;
+	}
+
+	/** Accumulate results, tracking status.
+	 *
+	 * @param array $result Test result from individual test.
+	 *
+	 * @return void
+	 */
+	private function add_result( $result ) {
+		$status = $result['status'];
+		if ( ! array_key_exists( $status, $this->results ) ) {
+			$this->results[ $status ] = array();
+		}
+		$this->results[ $status ] [] = $result;
+	}
+
+	/** Run all detailed tests.
+	 *
+	 * @return array
+	 */
+	public function alltests() {
+		$this->add_result( $this->server_version_test() );
+		$this->add_result( $this->tables_format_test() );
+		$this->add_result( $this->buffer_pool_size_test() );
+		$this->add_result( $this->minimal_server_response_test() );
+
+		$description = array();
+
+		$result_status = 'good';
+		$result_status = array_key_exists( 'recommended', $this->results ) ? 'recommended' : $result_status;
+		$result_status = array_key_exists( 'critical', $this->results ) ? 'critical' : $result_status;
+
+		$status_display = 'good' === $result_status
+			? array( 'critical', 'recommended', 'good' )
+			: array( 'critical', 'recommended' );
+		foreach ( $status_display as $status ) {
+			if ( array_key_exists( $status, $this->results ) ) {
+				foreach ( $this->results[ $status ] as $result ) {
+					$description [] = '<div class="stanza">';
+					$description [] = '<div>' . $result['description'] . '</div>';
+					if ( array_key_exists( 'actions', $result ) && is_string( $result['actions'] ) && strlen( $result['actions'] ) > 0 ) {
+						$description [] = '<div class="actions">' . $result['actions'] . '</div>';
+					}
+					$description [] = '</div>';
+				}
+			}
+		}
+
+		$description = implode( PHP_EOL, $description );
+		switch ( $result_status ) {
+			case 'critical':
+			case 'recommended':
+				$label = sprintf(
+					/* translators: 1:  MySQL or MariaDB */
+					__( 'Configure your %1$s database server for improved performance', 'performance-lab' ),
+					$this->name
+				);
+				break;
+			case 'good':
+			default:
+				$label = sprintf(
+				/* translators: 1:  MySQL or MariaDB */
+					__( 'Your %1$s server appears to be configured for good performance', 'performance-lab' ),
+					$this->name
+				);
+		}
+		$action = ( 'good' !== $result_status )
+			? sprintf(
+				/* translators: 1: MySQL or MariaDB */
+				__( 'Contact your server administrator or web hosting company for help configuring your %1$s server.', 'performance-lab' ),
+				$this->name
+			)
+			: null;
+		return $this->utilities->test_result( $label, $description, $action, $result_status );
+
 	}
 
 	/** Check server version for Barracuda availability.
@@ -120,11 +180,11 @@ class PerflabDbTests {
 				sprintf( __( 'Your %s server is outdated', 'performance-lab' ), $this->name ),
 				sprintf(
 				/* translators: 1:  MySQL or MariaDB 2: actual version of database software */
-					__( 'WordPress uses your %1$s server to store and retrieve all your site’s content and settings. The version you use (%2$s) does not offer the latest InnoDB Barracuda storage engine, the most efficient way to retrieve content. This affects you if you have many posts or users.', 'performance-lab' ),
+					__( 'WordPress uses your %1$s server to store and retrieve all your site’s content and settings. The version you use (%2$s) does not offer the most modern and efficient way to store your content and settings. This affects you if you have many posts or users.', 'performance-lab' ),
 					$this->name,
 					$this->version->version
 				),
-				__( 'For best performance we recommend running MySQL version 5.7 or higher or MariaDB version 10.3 or higher. Contact your hosting provider and ask them to upgrade their server.', 'performance-lab' ),
+				__( 'For optimal performance we recommend running MySQL version 5.7 or higher or MariaDB version 10.3 or higher. Contact your web hosting company to correct this.', 'performance-lab' ),
 				'recommended'
 			);
 		}
@@ -133,10 +193,9 @@ class PerflabDbTests {
 		/* translators: 1:  MySQL or MariaDB */
 			sprintf( __( 'Your %s server is a recent version', 'performance-lab' ), $this->name ),
 			sprintf(
-			/* translators: 1:  MySQL or MariaDB 2: actual version string (e.g. '10.3.34-MariaDB-0ubuntu0.20.04.1' or '  of database software */
-				__( 'WordPress uses your %1$s SQL server to store your site’s content and settings. Your version (%2$s) offers the modern InnoDB Barracuda storage engine, an efficient way to retrieve your content and settings.', 'performance-lab' ),
-				$this->name,
-				$this->version->version
+			/* translators: 1:  MySQL or MariaDB  */
+				__( 'WordPress uses your %1$s SQL server to store your site’s content and settings. Your version offers the modern and efficient way to store your content and settings.', 'performance-lab' ),
+				$this->name
 			)
 		);
 	}
@@ -145,38 +204,25 @@ class PerflabDbTests {
 	 *
 	 * @return array
 	 */
-	public function core_tables_format_test() {
+	public function tables_format_test() {
 		if ( $this->skip_all_tests ) {
 			return array();
 		}
 		global $wpdb;
-		$tables = $wpdb->tables();
 		/* Get stats for the core tables. */
-		$stats                 = array_intersect_key( $this->table_stats, array_combine( $tables, $tables ) );
+		$stats                 = $this->table_stats;
 		$target_storage_engine = $this->utilities->get_threshold_value( 'target_storage_engine' );
 		$target_row_format     = $this->utilities->get_threshold_value( 'target_row_format' );
 		$wrong_format_tables   = $this->metrics->get_wrong_format_tables( $stats, $target_storage_engine, $target_row_format );
 		if ( count( $wrong_format_tables ) === 0 ) {
 			return $this->utilities->test_result(
-				__( 'Your WordPress tables use the modern storage format', 'performance-lab' ),
-				( 1 === $this->version->unconstrained )
-					? sprintf(
-						/* translators: 1 storage engine name, usually InnoDB  2: row format name, usually Dynamic */
-						__( 'Your WordPress tables use the %1$s storage engine and the %2$s row format for good database performance.', 'performance-lab' ),
-						$target_storage_engine,
-						$target_row_format
-					)
-					: sprintf(
-						/* translators: 1 storage engine name, usually InnoDB. */
-						__( 'Your WordPress tables use the %1$s storage engine for good database performance.', 'performance-lab' ),
-						$target_storage_engine,
-						$target_row_format
-					)
+				__( 'Your tables use the modern storage engine', 'performance-lab' ),
+				__( 'Your tables use the latest storage engine for good database performance.', 'performance-lab' )
 			);
 		} else {
 			$label = count( $wrong_format_tables ) === count( $stats )
-				? __( 'Your WordPress tables use an obsolete storage format', 'performance-lab' )
-				: __( 'Some WordPress tables use an obsolete storage format', 'performance-lab' );
+				? __( 'Your tables all use an obsolete storage format', 'performance-lab' )
+				: __( 'Some tables use an obsolete storage format', 'performance-lab' );
 
 			$explanation = sprintf(
 				/* translators: 1 MySQL or MariaDB */
@@ -193,64 +239,6 @@ class PerflabDbTests {
 				'orange'
 			);
 
-		}
-	}
-
-	/** Check extra tables format
-	 *
-	 * @return array
-	 */
-	public function extra_tables_format_test() {
-		if ( $this->skip_all_tests ) {
-			return array();
-		}
-		global $wpdb;
-		$tables = $wpdb->tables();
-		/* Get stats for the non-core tables: the ones not mentioned in $wpdb->tables() */
-		$stats = array_diff_key( $this->table_stats, array_combine( $tables, $tables ) );
-		if ( 0 === count( $stats ) ) {
-			/* don't pester the user when they have no plugin tables */
-			return array();
-		}
-		$target_storage_engine = $this->utilities->get_threshold_value( 'target_storage_engine' );
-		$target_row_format     = $this->utilities->get_threshold_value( 'target_row_format' );
-		$wrong_format_tables   = $this->metrics->get_wrong_format_tables( $stats, $target_storage_engine, $target_row_format );
-		if ( count( $wrong_format_tables ) === 0 ) {
-			return $this->utilities->test_result(
-				__( 'Your plugin tables use the modern storage format', 'performance-lab' ),
-				( 1 === $this->version->unconstrained )
-					? sprintf(
-						/* translators: 1 storage engine name, usually InnoDB  2: row format name, usually Dynamic */
-						__( 'Tables managed by your plugins use the %1$s storage engine and the %2$s row format for good database performance.', 'performance-lab' ),
-						$target_storage_engine,
-						$target_row_format
-					)
-					: sprintf(
-						/* translators: 1 storage engine name, usually InnoDB   */
-						__( 'Tables managed by your plugins use the %1$s storage engine for good database performance.', 'performance-lab' ),
-						$target_storage_engine,
-						$target_row_format
-					)
-			);
-		} else {
-			$label = count( $wrong_format_tables ) === count( $stats )
-				? __( 'Your plugin tables use an obsolete storage format', 'performance-lab' )
-				: __( 'Some plugin tables use an obsolete storage format', 'performance-lab' );
-
-			$explanation = sprintf(
-			/* translators: 1 MySQL or MariaDB */
-				__( '%1$s performance improves when your plugins\' tables use the modern storage format.', 'performance-lab' ),
-				$this->name
-			);
-			$actions = $this->list_wrong_format_tables( $wrong_format_tables, $target_storage_engine, $target_row_format );
-
-			return $this->utilities->test_result(
-				$label,
-				$explanation,
-				$actions,
-				'recommended',
-				'orange'
-			);
 		}
 	}
 
@@ -285,41 +273,41 @@ class PerflabDbTests {
 
 		if ( $myisam_size > 0 && ( $myisam_pool_size / $fraction ) < $myisam_size ) {
 			$too_small = true;
-			$msgs[]    = '<p>' . sprintf(
+			$msgs[]    = sprintf(
 				/* translators: 1: memory size like 512MiB  2: server name like MySQL  3: memory size */
 				__( 'The keys on your MyISAM tables (the obsolete storage engine) use %1$s, but %2$s\'s MyISAM key buffer size (the system variable named \'key_buffer_size\') is only %3$s. That may be too small.', 'performance-lab' ),
 				$this->utilities->format_bytes( $myisam_size ),
 				$this->name,
 				$this->utilities->format_bytes( $myisam_pool_size )
-			) . '</p>';
+			);
 		} elseif ( 0 === $myisam_size ) {
 			/* empty, intentionally. Don't pester the user about MyISAM here if they don't use it. */
 		} else {
-			$msgs[] = '<p>' . sprintf(
+			$msgs[] = sprintf(
 				/* translators: 1: memory size like 512MiB  2: server name like MySQL  3: memory size */
 				__( 'The keys on your MyISAM tables (the obsolete storage engine) use %1$s and %2$s\'s MyISAM key buffer size is %3$s. That is big enough in most cases.', 'performance-lab' ),
 				$this->utilities->format_bytes( $myisam_size ),
 				$this->name,
 				$this->utilities->format_bytes( $myisam_pool_size )
-			) . '</p>';
+			);
 		}
 		if ( $innodb_size > 0 && ( $innodb_pool_size / $fraction ) < $innodb_size ) {
 			$too_small = true;
-			$msgs[]    = '<p>' . sprintf(
+			$msgs[]    = sprintf(
 				/* translators: 1: memory size like 512MiB  2: server name like MySQL  3: memory size */
 				__( 'Your InnoDB tables (the modern storage engine) use %1$s, but %2$s\'s InnoDb buffer pool size (the system variable named \'innodb_buffer_pool_size\') is only %3$s. That may be too small.', 'performance-lab' ),
 				$this->utilities->format_bytes( $innodb_size ),
 				$this->name,
 				$this->utilities->format_bytes( $innodb_pool_size )
-			) . '</p>';
+			);
 		} else {
-			$msgs[] = '<p>' . sprintf(
+			$msgs[] = sprintf(
 				/* translators: 1: memory size like 512MiB  2: server name like MySQL  3: memory size */
 				__( 'Your InnoDB tables (the modern storage engine) use %1$s and %2$s\'s InnoDb buffer pool size is %3$s. That is big enough in most cases.', 'performance-lab' ),
 				$this->utilities->format_bytes( $innodb_size ),
 				$this->name,
 				$this->utilities->format_bytes( $innodb_pool_size )
-			) . '</p>';
+			);
 		}
 		if ( $too_small ) {
 			return $this->utilities->test_result(
@@ -330,10 +318,10 @@ class PerflabDbTests {
 				),
 				sprintf(
 				/* translators: 1 server name like MariaDB */
-					'<p>' . __( 'Your %1$s buffer pool is too small. That makes it take longer to retrieve your content, especially when your site is busy.', 'performance-lab' ),
+					__( 'Your %1$s buffer pool is too small. That makes it take longer to retrieve your content, especially when your site is busy.', 'performance-lab' ),
 					$this->name
-				) . '</p><p>' . implode( ' ', $msgs ) . '</p>',
-				__( 'Consider asking your hosting provider to upgrade your SQL server\'s buffer pool size.', 'performance-lab' ),
+				) . implode( '<br/>', $msgs ),
+				__( 'Consider asking your server administrator to upgrade your SQL server\'s buffer pool size.', 'performance-lab' ),
 				'recommended',
 				'orange'
 			);
@@ -344,10 +332,10 @@ class PerflabDbTests {
 					__( 'Your %1$s server buffer pool is big enough for your data', 'performance-lab' ),
 					$this->name
 				),
-				implode( '', $msgs ),
+				implode( '<br/>', $msgs ),
 				sprintf(
 				/* translators: 1 server name like MariaDB */
-					__( 'Some hosting providers share their %1$s servers among multiple WordPress sites. In that case the buffer pool you share with those other sites may still be inadequate. Site Health cannot detect shared %1$s servers.', 'performance-lab' ),
+					__( 'Some web hosting companies share their %1$s servers among multiple WordPress sites. In that case the buffer pool you share with those other sites may still be inadequate. Site Health cannot detect shared %1$s servers.', 'performance-lab' ),
 					$this->name
 				)
 			);
@@ -376,11 +364,11 @@ class PerflabDbTests {
 				sprintf( __( 'Your %s server connects and responds very slowly', 'performance-lab' ), $this->name ),
 				sprintf(
 				/* translators: 1:  number of milliseconds 2: milliseconds */
-					__( 'Your SQL server connects and responds to simple requests from WordPress in %1$s milliseconds. For best performance it should respond in under %2$s.', 'performance-lab' ),
+					__( 'Your SQL server connects and responds to simple requests from WordPress very slowly, in %1$s milliseconds. For best performance it should respond in under %2$s.', 'performance-lab' ),
 					$formatted_results,
 					$formatted_threshold
 				),
-				__( 'This usually means your SQL server is overburdened. Possibly many sites share it, or possibly it needs more RAM. Contact your web hosting company to correct this.', 'performance-lab' ),
+				__( 'This usually means your SQL server is overburdened. Too many sites may share it, or it may need more RAM. Contact your web hosting company to correct this.', 'performance-lab' ),
 				'critical',
 				'red'
 			);
@@ -390,11 +378,11 @@ class PerflabDbTests {
 				sprintf( __( 'Your %s server connects and responds slowly', 'performance-lab' ), $this->name ),
 				sprintf(
 				/* translators: 1:  number of milliseconds  2: milliseconds */
-					__( 'Your SQL server connects and responds to simple requests from WordPress in %1$s milliseconds. For best performance it should respond in under %2$s.', 'performance-lab' ),
+					__( 'Your SQL server connects and responds to simple requests from WordPress slowly, in %1$s milliseconds. For best performance it should respond in under %2$s.', 'performance-lab' ),
 					$formatted_results,
 					$formatted_threshold
 				),
-				__( 'This usually means your SQL server is busy. Possibly it is shared among many sites, or possibly it needs more RAM. Contact your web hosting company to correct this.', 'performance-lab' ),
+				__( 'This usually means your SQL server is busy. Too many sites may share it, or it may need more RAM. Contact your web hosting company to correct this.', 'performance-lab' ),
 				'recommended',
 				'orange'
 			);
@@ -423,13 +411,13 @@ class PerflabDbTests {
 		$exhortation = 1 === $this->version->unconstrained
 			? sprintf(
 			/* translators: 1 storage engine name, usually InnoDB  2: row format name, usually Dynamic */
-				__( 'Ask your hosting provider to upgrade these tables to use the %1$s storage engine and the %2$s row format.', 'performance-lab' ),
+				__( 'Ask your server administrator to upgrade these tables to use the %1$s storage engine and the %2$s row format.', 'performance-lab' ),
 				$target_storage_engine,
 				$target_row_format
 			)
 			: sprintf(
 			/* translators: 1 storage engine name, usually InnoDB   */
-				__( 'Ask your hosting provider to upgrade these tables to use the %1$s storage engine.', 'performance-lab' ),
+				__( 'Ask your server administrator to upgrade these tables to use the %1$s storage engine.', 'performance-lab' ),
 				$target_storage_engine,
 				$target_row_format
 			);
