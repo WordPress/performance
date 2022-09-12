412ae47e6baf79bc12517477ce578dbdac5ed492
pbearne <paul@bearne.ca>
2022-09-12 18:17:09 -0400

    added basic tests

--- tests/modules/images/fetchpriority/Fetchpriority-Tests.php
+++ tests/modules/images/fetchpriority/Fetchpriority-Tests.php
@@ -1,14 +1,12 @@
 <?php
 
-namespace modules\images\fetchpriority;
-
 class Fetchpriority_Tests extends WP_UnitTestCase {
 
 	public function test_wp_filter_content_tags_filter_adds_fetchpriority_fist_image_only() {
 		$attachment_id = $this->factory->attachment->create_upload_object( TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/leafs.jpg' );
 		$img           = get_image_tag( $attachment_id, '', '', '', 'large' );
-		$content       = "$img\n$img";
 
-		$this->assertStringContainsString( 'fetchpriority ', wp_filter_content_tags( $content ) );
+		$this->assertStringContainsString( 'fetchpriority="high"', fetchpriority_img_tag_add( $img, 'the_content' ) );
+		$this->assertStringNotContainsString( 'fetchpriority="high"', fetchpriority_img_tag_add( $img, 'not_content' ) );
 	}
 }
