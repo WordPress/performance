142b3204205f097442b0a7a579816ff25f326348
Ankit Gade <ankit.gade@rtcamp.com>
2022-09-15 17:41:56 +0530

    Move job utilities to helper file

---
+++ tests/modules/images/regenerate-existing-images/helper-tests.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Tests for regenerate-existing-images module helper.php.
+ *
+ * @package performance-lab
+ * @group regenerate-existing-images
+ */
+
+class Regenerate_Existing_Images_Helper_Test extends WP_UnitTestCase {
+
+	public function test_perflab_create_background_job() {
+		$name = 'test';
+		$data = array( 'post' => 123 );
+		$job  = perflab_create_background_job( $name, $data );
+
+		$this->assertInstanceOf( Perflab_Background_Job::class, $job );
+		$job_data = $job->get_data();
+		$this->assertSame( $data, $job_data );
+		$job_status = $job->get_status();
+		$this->assertEquals( 'perflab_job_queued', $job_status );
+	}
+
+	public function test_perflab_delete_background_job() {
+		print_r( 'Helper test' );
+		die;
+		$name    = 'test';
+		$data    = array( 'post' => 123 );
+		$job     = perflab_create_background_job( $name, $data );
+		$deleted = perflab_delete_background_job( $job->get_id() );
+
+		$this->assertTrue( $deleted );
+	}
+}
