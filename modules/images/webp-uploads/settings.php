ab65615023d29ee1839cae928a23e67739947d32
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2022-10-06 12:21:43 +0530

    Update structure and add css

--- modules/images/webp-uploads/settings.php
+++ modules/images/webp-uploads/settings.php
@@ -37,7 +37,7 @@ function webp_uploads_add_media_settings_field() {
 		'webp_uploads_generate_webp_jpeg_setting_callback',
 		'media',
 		'uploads',
-		array( 'class' => 'hidden' )
+		array( 'class' => 'perflab-generate-webp-and-jpeg' )
 	);
 }
 add_action( 'admin_init', 'webp_uploads_add_media_settings_field' );
@@ -49,14 +49,34 @@ add_action( 'admin_init', 'webp_uploads_add_media_settings_field' );
  */
 function webp_uploads_generate_webp_jpeg_setting_callback() {
 	?>
-	<tr>
-		<td class="td-full">
-			<label for="perflab_generate_webp_and_jpeg">
-				<input name="perflab_generate_webp_and_jpeg" type="checkbox" id="perflab_generate_webp_and_jpeg" aria-describedby="perflab_generate_webp_and_jpeg_description" value="1"<?php checked( '1', get_option( 'perflab_generate_webp_and_jpeg' ) ); ?> />
-				<?php esc_html_e( 'Generate JPEG files in addition to WebP', 'performance-lab' ); ?>
-			</label>
-			<p class="description" id="perflab_generate_webp_and_jpeg_description"><?php esc_html_e( 'Enabling JPEG in addition to WebP can improve compatibility, but will effectively double the filesystem storage use of your images.', 'performance-lab' ); ?></p>
-		</td>
-	</tr>
+	<label for="perflab_generate_webp_and_jpeg">
+		<input name="perflab_generate_webp_and_jpeg" type="checkbox" id="perflab_generate_webp_and_jpeg" aria-describedby="perflab_generate_webp_and_jpeg_description" value="1"<?php checked( '1', get_option( 'perflab_generate_webp_and_jpeg' ) ); ?> />
+		<?php esc_html_e( 'Generate JPEG files in addition to WebP', 'performance-lab' ); ?>
+	</label>
+	<p class="description" id="perflab_generate_webp_and_jpeg_description"><?php esc_html_e( 'Enabling JPEG in addition to WebP can improve compatibility, but will effectively double the filesystem storage use of your images.', 'performance-lab' ); ?></p>
 	<?php
 }
+
+/**
+ * Adds custom style for media settings.
+ *
+ * @since n.e.x.t
+ *
+ * @global string $pagenow The filename of the current screen.
+ */
+function webp_uploads_media_setting_style() {
+	global $pagenow;
+
+	if ( 'options-media.php' === $pagenow ) {
+		echo '<style>
+		.form-table .perflab-generate-webp-and-jpeg th {
+			display: none;
+		}
+		.form-table .perflab-generate-webp-and-jpeg td {
+			width: auto;
+			padding-left: 0;
+		}
+		</style>';
+	}
+}
+add_action( 'admin_head', 'webp_uploads_media_setting_style' );
