638816b4d752775a60b0966f5aa5ce6b6e2dc0cb
Eugene Manuilov <manuilov@google.com>
2022-10-28 21:23:07 +0300

    Implement the benchmarking tool.

--- bin/plugin/commands/benchmark.js
+++ bin/plugin/commands/benchmark.js
@@ -1,12 +1,35 @@
 /**
+ * External dependencies
+ */
+const autocannon = require( 'autocannon' );
+const { mean, floor } = require( 'lodash' );
+
+/**
+ * Internal dependencies
+ */
+const { log } = require( '../lib/logger' );
+
+/**
  * @typedef BenchmarkCommandOptions
  *
- * @property {string} url An URL.
+ * @property {string} url         An URL.
+ * @property {number} concurrency Number of multiple requests to make at a time.
+ * @property {number} requests    Number of requests to perform.
  */
 exports.options = [
 	{
 		argname: '-u, --url <url>',
 		description: 'An URL to run benchmark tests for',
+	},
+	{
+		argname: '-c, --concurrency <concurrency>',
+		description: 'Number of multiple requests to make at a time',
+		defaults: 1,
+	},
+	{
+		argname: '-n, --requests <requests>',
+		description: 'Number of requests to perform',
+		defaults: 1,
 	},
 ];
 
@@ -16,4 +39,47 @@ exports.options = [
  * @param {BenchmarkCommandOptions} opt Command options.
  */
 exports.handler = async ( opt ) => {
+	const { url, concurrency: connections, requests: amount } = opt;
+	const metrics = {};
+	const responseTimes = [];
+
+	const instance = autocannon( {
+		url,
+		connections,
+		amount,
+		setupClient( client ) {
+			client.on( 'headers', ( { headers } ) => {
+				for ( let i = 0, len = headers.length; i < len; i += 2 ) {
+					if ( headers[ i ].toLowerCase() !== 'server-timing' ) {
+						continue;
+					}
+
+					headers[ i + 1 ]
+						.split( ',' )
+						.map( ( timing ) => timing.trim().split( ';dur=' ) )
+						.forEach( ( [ key, value ] ) => {
+							metrics[ key ] = metrics[ key ] || [];
+							metrics[ key ].push( +value );
+						} );
+				}
+			} );
+
+			client.on( 'response', ( statusCode, resBytes, responseTime ) => {
+				responseTimes.push( responseTime );
+			} );
+		},
+	} );
+
+	process.once( 'SIGINT', () => {
+		instance.stop();
+	} );
+
+	instance.on( 'done', () => {
+		log( `Avr response time: ${ floor( mean( responseTimes ), 2 ) }ms` );
+
+		for ( const metric of Object.keys( metrics ) ) {
+			const metricAvgMs = floor( mean( metrics[ metric ] ), 2 );
+			log( ` - ${ metric }: ${ metricAvgMs }ms` );
+		}
+	} );
 };
