bf515a285a368f496a9eb1ab5d2783fb5dcc3717
Adam Silverstein <adamjs@google.com>
2022-10-28 09:44:56 -0600

    Improve tests with multiple attachments, in_the_loop true

--- modules/images/fetchpriority/load.php
+++ modules/images/fetchpriority/load.php
@@ -20,7 +20,6 @@
 function fetchpriority_img_tag_add_attr( $filtered_image, $context ) {
 
 	if ( 'the_content' !== $context && 'the_post_thumbnail' !== $context ) {
-
 		return $filtered_image;
 	}
 
--- tests/modules/images/fetchpriority/fetchpriority-tests.php
+++ tests/modules/images/fetchpriority/fetchpriority-tests.php
@@ -1,23 +1,28 @@
 <?php
-
 /**
  * Tests the adding of fetchpriority to img tags in the content.
  *
  * @covers ::fetchpriority_img_tag_add_attr
  */
-
 class Fetchpriority_Test extends WP_UnitTestCase {
 	protected static $post;
 	protected static $attachment_id;
+	protected static $attachment_id_2;
 
 	protected $current_size_filter_data   = null;
 	protected $current_size_filter_result = null;
 
 	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
-		self::$post = $factory->post->create_and_get();
-
-		$file                = TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/leafs.jpg';
-		self::$attachment_id = $factory->attachment->create_upload_object(
+		self::$post            = $factory->post->create_and_get();
+		$file                  = DIR_TESTDATA . '/images/canola.jpg';
+		self::$attachment_id   = $factory->attachment->create_upload_object(
+			$file,
+			self::$post->ID,
+			array(
+				'post_mime_type' => 'image/jpeg',
+			)
+		);
+		self::$attachment_id_2 = $factory->attachment->create_upload_object(
 			$file,
 			self::$post->ID,
 			array(
@@ -25,12 +30,15 @@ class Fetchpriority_Test extends WP_UnitTestCase {
 			)
 		);
 	}
-	public function test_fetchpriority_img_tag_add_attr_based_on_context_and_loading_lazy() {
-		$attachment_id = $this->factory->attachment->create_upload_object(
-			TESTS_PLUGIN_DIR . '/tests/testdata/modules/images/leafs.jpg'
-		);
 
-		$img = get_image_tag( $attachment_id, '', '', '', 'large' );
+	public static function tear_down_after_class() {
+		wp_delete_attachment( self::$attachment_id, true );
+		wp_delete_attachment( self::$attachment_id_2, true );
+		parent::tear_down_after_class();
+	}
+
+	public function test_fetchpriority_img_tag_add_attr_based_on_context_and_loading_lazy() {
+		$img = get_image_tag( self::$attachment_id, '', '', '', 'large' );
 
 		$this->assertStringContainsString( 'fetchpriority="high"', fetchpriority_img_tag_add_attr( $img, 'the_content' ) );
 		$this->assertStringNotContainsString( 'fetchpriority="high"', fetchpriority_img_tag_add_attr( $img, 'not_content' ) );
@@ -40,26 +48,30 @@ class Fetchpriority_Test extends WP_UnitTestCase {
 	}
 
 	public function test_fetchpriority_img_tag_add_in_wp_filter_content_tags() {
-		$img = get_image_tag( self::$attachment_id, '', '', '', 'large' );
+		global $wp_query;
 
-		$img     = '<!-- wp:image {"id":' . self::$attachment_id . ',"sizeSlug":"large","linkDestination":"none"} -->
+		$img   = get_image_tag( self::$attachment_id, '', '', '', 'large' );
+		$img_2 = get_image_tag( self::$attachment_id_2, '', '', '', 'large' );
+
+		$img = '<!-- wp:image {"id":' . self::$attachment_id . ',"sizeSlug":"large","linkDestination":"none"} -->
 <figure class="wp-block-image size-large">' . $img . '</figure>
 <!-- /wp:image -->
 <!-- wp:paragraph -->
-<p>This is an example page. It\'s different from a blog post because it will stay in one place and will show up in your site navigation (in most themes). Most people start with an About page that introduces them to potential site visitors. It might say something like this:</p>
+<p>This is an example page.</p>
 <!-- /wp:paragraph -->
-<!-- wp:image {"id":' . self::$attachment_id . ',"sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large">' . $img . '</figure>
+<!-- wp:image {"id":' . self::$attachment_id_2 . ',"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large">' . $img_2 . '</figure>
 <!-- /wp:image -->
 ';
-		$content = wp_filter_content_tags( $img, 'the_content' );
+		// Ensure image filtering occurs 'in_the_loop'.
+		$wp_query->in_the_loop = true;
+		$content               = wp_filter_content_tags( $img, 'the_content' );
+		$wp_query->in_the_loop = false;
 		$this->assertStringContainsString( 'fetchpriority="high"', $content );
+		$this->assertStringContainsString( 'loading="lazy"', $content );
+		$this->assertTrue( strpos( $content, 'fetchpriority="high"' ) < strpos( $content, 'loading="lazy"' ) );
 
 		$spilt = explode( 'fetchpriority', $content );
 		$this->assertEquals( 2, count( $spilt ) );
-	}
-	public function test_get_the_post_thumbnail() {
-		set_post_thumbnail( self::$post, self::$attachment_id );
-		$this->assertStringContainsString( 'fetchpriority="high"', get_the_post_thumbnail( self::$post ) );
 	}
 }
