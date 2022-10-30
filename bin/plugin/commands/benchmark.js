cfd4746a00b981b8a3ec16f2236d8585d71cef3e
Eugene Manuilov <manuilov@google.com>
2022-10-30 23:35:15 +0200

    Add the ability to benchmark URLs in a file.

--- bin/plugin/commands/benchmark.js
+++ bin/plugin/commands/benchmark.js
@@ -1,6 +1,8 @@
 /**
  * External dependencies
  */
+const fs = require( 'fs' );
+const readline = require( 'readline' );
 const autocannon = require( 'autocannon' );
 const { mean, floor } = require( 'lodash' );
 
@@ -8,6 +10,21 @@ const { mean, floor } = require( 'lodash' );
  * Internal dependencies
  */
 const { log } = require( '../lib/logger' );
+
+/**
+ * @typedef BenchmarkOptions
+ *
+ * @property {string} url         An URL.
+ * @property {number} connections Number of multiple requests to make at a time.
+ * @property {number} amount      Number of requests to perform.
+ */
+
+/**
+ * @typedef BenchmarkResults
+ *
+ * @property {Array.<number>} responseTimes An array of response times.
+ * @property {Object}         metrics       The Server-Timing metrics object.
+ */
 
 /**
  * @typedef BenchmarkCommandOptions
@@ -31,6 +48,10 @@ exports.options = [
 		description: 'Number of requests to perform',
 		defaults: 1,
 	},
+	{
+		argname: '-f, --file <file>',
+		description: 'File with URLs to run benchmark tests for',
+	},
 ];
 
 /**
@@ -39,47 +60,122 @@ exports.options = [
  * @param {BenchmarkCommandOptions} opt Command options.
  */
 exports.handler = async ( opt ) => {
-	const { url, concurrency: connections, requests: amount } = opt;
-	const metrics = {};
-	const responseTimes = [];
+	const { concurrency: connections, requests: amount } = opt;
+	const results = [];
 
-	const instance = autocannon( {
-		url,
-		connections,
-		amount,
-		setupClient( client ) {
-			client.on( 'headers', ( { headers } ) => {
-				for ( let i = 0, len = headers.length; i < len; i += 2 ) {
-					if ( headers[ i ].toLowerCase() !== 'server-timing' ) {
-						continue;
-					}
+	for await ( const url of getURLs( opt ) ) {
+		const { responseTimes, metrics } = await benchmarkURL( {
+			url,
+			connections,
+			amount,
+		} );
 
-					headers[ i + 1 ]
-						.split( ',' )
-						.map( ( timing ) => timing.trim().split( ';dur=' ) )
-						.forEach( ( [ key, value ] ) => {
-							metrics[ key ] = metrics[ key ] || [];
-							metrics[ key ].push( +value );
-						} );
-				}
-			} );
+		results.push( [ url, responseTimes, metrics ] );
+	}
 
-			client.on( 'response', ( statusCode, resBytes, responseTime ) => {
-				responseTimes.push( responseTime );
-			} );
-		},
-	} );
+	for ( let i = 0, len = results.length; i < len; i++ ) {
+		const [ url, responseTimes, metrics ] = results[ i ];
 
-	process.once( 'SIGINT', () => {
-		instance.stop();
-	} );
-
-	instance.on( 'done', () => {
-		log( `Avr response time: ${ floor( mean( responseTimes ), 2 ) }ms` );
+		log(
+			`${ url } avr response time: ${ floor(
+				mean( responseTimes ),
+				2
+			) }ms`
+		);
 
 		for ( const metric of Object.keys( metrics ) ) {
 			const metricAvgMs = floor( mean( metrics[ metric ] ), 2 );
 			log( ` - ${ metric }: ${ metricAvgMs }ms` );
 		}
-	} );
+	}
 };
+
+/**
+ * Generates URLs to benchmark based on command arguments. If both "<url>" and "<file>" arguments
+ * are passed to the command, then both will be used to generate URLs.
+ *
+ * @param {BenchmarkCommandOptions} opt Command options.
+ */
+async function* getURLs( opt ) {
+	if ( !! opt.url ) {
+		yield opt.url;
+	}
+
+	if ( !! opt.file ) {
+		const rl = readline.createInterface( {
+			input: fs.createReadStream( opt.file ),
+			crlfDelay: Infinity,
+		} );
+
+		for await ( const url of rl ) {
+			if ( url.length > 0 ) {
+				yield url;
+			}
+		}
+	}
+}
+
+/**
+ * Benchmarks an URL and returns response time and server-timing metrics for every request.
+ *
+ * @param {BenchmarkOptions} params Benchmark parameters.
+ * @return {BenchmarkResults} Response times and metrics arrays.
+ */
+function benchmarkURL( params ) {
+	const metrics = {};
+	const responseTimes = [];
+
+	const onHeaders = ( { headers } ) => {
+		const responseMetrics = getServerTimingMetricsFromHeaders( headers );
+		Object.entries( responseMetrics ).forEach( ( [ key, value ] ) => {
+			metrics[ key ] = metrics[ key ] || [];
+			metrics[ key ].push( +value );
+		} );
+	};
+
+	const onResponse = ( statusCode, resBytes, responseTime ) => {
+		responseTimes.push( responseTime );
+	};
+
+	const instance = autocannon( {
+		...params,
+		setupClient( client ) {
+			client.on( 'headers', onHeaders );
+			client.on( 'response', onResponse );
+		},
+	} );
+
+	const onStop = instance.stop.bind( instance );
+	process.once( 'SIGINT', onStop );
+
+	return new Promise( ( resolve ) => {
+		instance.on( 'done', () => {
+			process.off( 'SIGINT', onStop );
+			resolve( { responseTimes, metrics } );
+		} );
+	} );
+}
+
+/**
+ * Reads the Server-Timing metrics from the response headers.
+ *
+ * @param {Array.<string>} headers Array of response headers information where each even element is a header name and an odd element is the header value.
+ * @return {Object} An object where keys are metric names and values are metric values.
+ */
+function getServerTimingMetricsFromHeaders( headers ) {
+	for ( let i = 0, len = headers.length; i < len; i += 2 ) {
+		if ( headers[ i ].toLowerCase() !== 'server-timing' ) {
+			continue;
+		}
+
+		return headers[ i + 1 ]
+			.split( ',' )
+			.map( ( timing ) => timing.trim().split( ';dur=' ) )
+			.reduce(
+				( obj, [ key, value ] ) => ( { ...obj, [ key ]: value } ),
+				{}
+			);
+	}
+
+	return {};
+}
