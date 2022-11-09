885919601c9cd3129a094026a0e3192bb3b33b01
Paul Bearne <pbearne@gmail.com>
2022-11-09 07:56:15 -0500

    Update modules/images/dominant-color/load.php
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>

--- modules/images/dominant-color/load.php
+++ modules/images/dominant-color/load.php
@@ -129,8 +129,10 @@ function dominant_color_img_tag_add_dominant_color( $filtered_image, $context, $
 	if ( ! empty( $data ) || ! empty( $style ) || ! str_contains( $data, $filtered_image ) ) {
 		if ( str_contains( $filtered_image, ' style=' ) ) {
 			$filtered_image = str_replace( ' style="', ' ' . $data . 'style="' . $style, $filtered_image );
+		} elseif ( ! empty( $style ) ) {
+			$filtered_image = str_replace( '<img ', '<img ' . $data . 'style="' . $style . '" ', $filtered_image );
 		} else {
-			$filtered_image = str_replace( '<img ', '<img ' . $data . ' style="' . $style . '" ', $filtered_image );
+			$filtered_image = str_replace( '<img ', '<img ' . $data, $filtered_image );
 		}
 	}
 	if ( ! empty( $extra_class ) ) {
