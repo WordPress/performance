dd31a9b6aaa2ea22fea2bacc4ff21bf37bb8787e
Ari Stathopoulos <aristath@gmail.com>
2022-12-05 10:12:03 +0200

    Update admin/load.php
    Co-authored-by: Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>

--- admin/load.php
+++ admin/load.php
@@ -193,7 +193,7 @@ function perflab_render_modules_page_field( $module_slug, $module_data, $module_
 				<?php endif; ?>
 			<?php else : ?>
 				<?php // Don't use the WP notice classes here, as that makes them move to the top of the page. ?>
-				<p style="background:#fff;border:1px solid #c3c4c7;border-left-width: 4px;border-left-color:#dba617;box-shadow: 0 1px 1px rgba(0, 0, 0, 0.04);padding:1em;max-width:50em;">
+				<p class="notice notice-warning" style="padding:1em;max-width:50em;">
 					<?php esc_html_e( 'Enabling this module will switch to a separate database and install WordPress in it. You will need to reconfigure your site, and start with a fresh site. Disabling the module you will get back to your previous MySQL database, with all your previous data intact. Warning: Do not activate if your site is implemented in a multi-server environment.', 'performance-lab' ); ?>
 				</p>
 			<?php endif; ?>
