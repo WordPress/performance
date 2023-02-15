6bf932872abec6a79a7a535aff8c99e63ea15a36
Felix Arntz <felixarntz@google.com>
2023-02-15 09:54:53 -0800

    Provide more accurate info about MariaDB vs MySQL, enhance doc block.

--- modules/database/sqlite/load.php
+++ modules/database/sqlite/load.php
@@ -14,7 +14,7 @@
 require_once __DIR__ . '/site-health.php';
 
 /**
- * Add admin notices.
+ * Adds admin notices.
  *
  * When the plugin gets merged in wp-core, this is not to be ported.
  *
@@ -60,18 +60,32 @@ function perflab_sqlite_plugin_admin_notice() {
 add_action( 'admin_notices', 'perflab_sqlite_plugin_admin_notice' ); // Add the admin notices.
 
 /**
- * Add a link to the admin bar.
+ * Adds a link to the admin bar.
+ *
+ * @since n.e.x.t
+ *
+ * @global wpdb $wpdb WordPress database abstraction object.
  *
  * @param WP_Admin_Bar $admin_bar The admin bar object.
  */
 function perflab_sqlite_plugin_adminbar_item( $admin_bar ) {
+	global $wpdb;
+
+	if ( defined( 'PERFLAB_SQLITE_DB_DROPIN_VERSION' ) && defined( 'DATABASE_TYPE' ) && 'sqlite' === DATABASE_TYPE ) {
+		$title = '<span style="color:#46B450;">' . __( 'Database: SQLite', 'performance-lab' ) . '</span>';
+	} else {
+		$db_info = $wpdb->db_server_info();
+		if ( stripos( $db_info, 'maria' ) !== false ) {
+			$title = '<span style="color:#DC3232;">' . __( 'Database: MariaDB', 'performance-lab' ) . '</span>';
+		} else {
+			$title = '<span style="color:#DC3232;">' . __( 'Database: MySQL', 'performance-lab' ) . '</span>';
+		}
+	}
+
 	$args = array(
 		'id'     => 'performance-lab-sqlite',
 		'parent' => 'top-secondary',
-		/* translators: %s: SQLite icon. */
-		'title'  => defined( 'PERFLAB_SQLITE_DB_DROPIN_VERSION' ) && defined( 'DATABASE_TYPE' ) && 'sqlite' === DATABASE_TYPE
-			? '<span style="color:#46B450;">' . __( 'Database: SQLite', 'performance-lab' ) . '</span>'
-			: '<span style="color:#DC3232;">' . __( 'Database: MySQL', 'performance-lab' ) . '</span>',
+		'title'  => $title,
 		'href'   => esc_url( admin_url( 'options-general.php?page=' . PERFLAB_MODULES_SCREEN ) ),
 		'meta'   => false,
 	);
