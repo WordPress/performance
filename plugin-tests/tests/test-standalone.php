a453083c7e06c5b1def470a97bc0686d029f846c
Simon Dowdles <simon.dowdles@10up.com>
2023-03-14 01:58:26 +0200

    Track plugin test assets to be copied to each built plugin within ./build prior to unit testing of each.

---
+++ plugin-tests/tests/test-standalone.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Class PluginTest
+ *
+ * @package Webp_Uploads
+ */
+
+/**
+ * Plugin test case.
+ */
+class PluginTest extends WP_UnitTestCase {
+
+	public function test_plugin_does_not_fatal() {
+		return $this->assertTrue( true );
+	}
+}
