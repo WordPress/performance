6489f320bf611165fb2d818de0cf012d664c6f62
Ari Stathopoulos <aristath@gmail.com>
2023-04-10 09:35:02 +0300

    remove more file headers which no longer apply

--- modules/database/sqlite/load.php
+++ modules/database/sqlite/load.php
@@ -9,9 +9,6 @@
  * Requires PHP: 5.6
  * Textdomain: sqlite-database-integration
  *
- * This project is based on the original work of Kojima Toshiyasu and his SQLite Integration plugin,
- * and the work of Evan Mattson and his WP SQLite DB plugin - See https://github.com/aaemnnosttv/wp-sqlite-db
- *
  * @package performance-lab
  */
 
