a6792eb48cbc87aa136f83b079b24d87a4f67bd3
Felix Arntz <felixarntz@google.com>
2023-04-17 09:38:09 -0700

    Update default enabled modules.

--- default-enabled-modules.php
+++ default-enabled-modules.php
@@ -2,6 +2,7 @@
 /* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
 return array(
 	'images/dominant-color-images',
+	'images/fetchpriority',
 	'images/webp-support',
 	'images/webp-uploads',
 );
