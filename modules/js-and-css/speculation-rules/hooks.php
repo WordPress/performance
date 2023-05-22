286e50a35c980cef5bc4850be075de123a4ab6b3
Felix Arntz <felixarntz@google.com>
2023-05-22 16:25:34 -0700

    Add hook to make it easy to use origin trial.

--- modules/js-and-css/speculation-rules/hooks.php
+++ modules/js-and-css/speculation-rules/hooks.php
@@ -42,3 +42,25 @@ JS;
 	);
 }
 add_action( 'wp_footer', 'plsr_print_speculation_rules' );
+
+/**
+ * Prints the tag to opt in to the Chrome origin trial if the token constant is defined.
+ *
+ * After opting in to the origin trial via https://github.com/WICG/nav-speculation/blob/main/chrome-2023q1-experiment-overview.md,
+ * please set your token in a `PLSR_ORIGIN_TRIAL_TOKEN` constant, e.g. in `wp-config.php`.
+ *
+ * This function is here temporarily and will eventually be removed.
+ *
+ * @since n.e.x.t
+ * @access private
+ * @ignore
+ */
+function plsr_print_origin_trial_optin() {
+	if ( ! defined( 'PLSR_ORIGIN_TRIAL_TOKEN' ) || ! PLSR_ORIGIN_TRIAL_TOKEN ) {
+		return;
+	}
+	?>
+	<meta http-equiv="origin-trial" content="<?php echo esc_attr( PLSR_ORIGIN_TRIAL_TOKEN ); ?>">
+	<?php
+}
+add_action( 'wp_head', 'plsr_print_origin_trial_optin' );
