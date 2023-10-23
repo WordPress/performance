c2ed6392a402c02803b5cf6361bb5de06bf7b637
Simon Dowdles <72872375+10upsimon@users.noreply.github.com>
2023-10-23 10:28:10 +0200

    Update includes/classes/class-plugin-manager.php, include escaping on `$action_links` entries.
    Co-authored-by: Weston Ruter <westonruter@google.com>

--- includes/classes/class-plugin-manager.php
+++ includes/classes/class-plugin-manager.php
@@ -172,7 +172,7 @@ class Plugin_Manager {
 						} else {
 							$action_links[] = sprintf(
 								'<button type="button" class="button button-disabled" disabled="disabled">%s</button>',
-								_x( 'Cannot Update', 'plugin', 'default' )
+								esc_html( _x( 'Cannot Update', 'plugin', 'default' ) )
 							);
 						}
 					}
