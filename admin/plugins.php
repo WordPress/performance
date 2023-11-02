19358e26b5ed33582f101b2ff51a94d5a817e3eb
Simon Dowdles <72872375+10upsimon@users.noreply.github.com>
2023-11-02 15:30:43 +0200

    Update admin/plugins.php, formatting enhancements.
    Co-authored-by: Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>

--- admin/plugins.php
+++ admin/plugins.php
@@ -276,7 +276,7 @@ function perflab_render_plugin_card( string $standalone_plugin ) {
 				esc_html_e( 'This plugin does not work with your versions of WordPress and PHP.', 'default' );
 				if ( current_user_can( 'update_core' ) && current_user_can( 'update_php' ) ) {
 					echo wp_kses_post(
-					/* translators: 1: URL to WordPress Updates screen, 2: URL to Update PHP page. */
+						/* translators: 1: URL to WordPress Updates screen, 2: URL to Update PHP page. */
 						' ' . __( '<a href="%1$s">Please update WordPress</a>, and then <a href="%2$s">learn more about updating PHP</a>.', 'default' ),
 						esc_url( self_admin_url( 'update-core.php' ) ),
 						esc_url( wp_get_update_php_url() )
