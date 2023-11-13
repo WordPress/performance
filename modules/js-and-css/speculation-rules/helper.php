25fc9da1b305ba9d848f8f75bc7e8cdb9540f7ef
Felix Arntz <felixarntz@google.com>
2023-11-13 13:06:41 -0800

    Introduce filter for which paths to exclude for speculative prerendering.

--- modules/js-and-css/speculation-rules/helper.php
+++ modules/js-and-css/speculation-rules/helper.php
@@ -16,6 +16,40 @@
  * @return array Associative array of speculation rules by type.
  */
 function plsr_get_speculation_rules() {
+	$base_href_exclude_paths = array(
+		'/wp-login.php',
+		'/wp-admin/*',
+	);
+	$href_exclude_paths      = $base_href_exclude_paths;
+
+	/**
+	 * Filters the paths for which speculative prerendering should be disabled.
+	 *
+	 * All paths should start in a forward slash, relative to the root document. The `*` can be used as a wildcard.
+	 * By default, the array includes `/wp-login.php` and `/wp-admin/*`.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param array $href_exclude_paths Paths to disable speculative prerendering for.
+	 */
+	$href_exclude_paths = (array) apply_filters( 'plsr_speculation_rules_href_exclude_paths', $href_exclude_paths );
+
+	// Ensure that there are no duplicates and that the base paths cannot be removed.
+	$href_exclude_paths = array_map(
+		static function ( $exclude_path ) {
+			if ( ! str_starts_with( $exclude_path, '/' ) ) {
+				$exclude_path = '/' . $exclude_path;
+			}
+			return $exclude_path . '\\?*#*';
+		},
+		array_unique(
+			array_merge(
+				$base_href_exclude_paths,
+				$href_exclude_paths
+			)
+		)
+	);
+
 	$prerender_rules = array(
 		array(
 			'source'    => 'document',
@@ -29,10 +63,7 @@ function plsr_get_speculation_rules() {
 					// Except for WP login and admin URLs.
 					array(
 						'not' => array(
-							'href_matches' => array(
-								'/wp-login.php\\?*#*',
-								'/wp-admin/*\\?*#*',
-							),
+							'href_matches' => $href_exclude_paths,
 						),
 					),
 					// And except for any links marked with a class to not prerender.
