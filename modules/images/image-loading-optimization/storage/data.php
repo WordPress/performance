8c6b55984b7604ac01886bf8c87e8720748f7354
Weston Ruter <westonruter@google.com>
2023-11-13 17:21:49 -0800

    Remove unused ilo_get_current_url()

--- modules/images/image-loading-optimization/storage/data.php
+++ modules/images/image-loading-optimization/storage/data.php
@@ -27,48 +27,6 @@ function ilo_get_page_metric_freshness_ttl() {
 }
 
 /**
- * Get the URL for the current request.
- *
- * This is essentially the REQUEST_URI prefixed by the scheme and host for the home URL.
- * This is needed in particular due to subdirectory installs.
- *
- * @return string Current URL.
- */
-function ilo_get_current_url() {
-	$parsed_url = wp_parse_url( home_url() );
-
-	if ( ! is_array( $parsed_url ) ) {
-		$parsed_url = array();
-	}
-
-	if ( empty( $parsed_url['scheme'] ) ) {
-		$parsed_url['scheme'] = is_ssl() ? 'https' : 'http';
-	}
-	if ( ! isset( $parsed_url['host'] ) ) {
-		$parsed_url['host'] = isset( $_SERVER['HTTP_HOST'] ) ? wp_unslash( $_SERVER['HTTP_HOST'] ) : 'localhost';
-	}
-
-	$current_url = $parsed_url['scheme'] . '://';
-	if ( isset( $parsed_url['user'] ) ) {
-		$current_url .= $parsed_url['user'];
-		if ( isset( $parsed_url['pass'] ) ) {
-			$current_url .= ':' . $parsed_url['pass'];
-		}
-		$current_url .= '@';
-	}
-	$current_url .= $parsed_url['host'];
-	if ( isset( $parsed_url['port'] ) ) {
-		$current_url .= ':' . $parsed_url['port'];
-	}
-	$current_url .= '/';
-
-	if ( isset( $_SERVER['REQUEST_URI'] ) ) {
-		$current_url .= ltrim( wp_unslash( $_SERVER['REQUEST_URI'] ), '/' );
-	}
-	return esc_url_raw( $current_url );
-}
-
-/**
  * Gets the normalized query vars for the current request.
  *
  * This is used as a cache key for stored page metrics.
@@ -289,7 +247,6 @@ function ilo_get_needed_minimum_viewport_widths( $slug ) {
 
 	return $needed_minimum_viewport_widths;
 }
-
 
 /**
  * Checks whether there is a page metric needed for one of the breakpoints.
