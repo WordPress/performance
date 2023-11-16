ea7bea84f9fec6b3fe94e715b61d57d897ec2c5e
Simon Dowdles <72872375+10upsimon@users.noreply.github.com>
2023-11-16 17:58:02 +0200

    Update admin/plugins.php, add reference to core where code was lifted from.
    Co-authored-by: Weston Ruter <westonruter@google.com>

--- admin/plugins.php
+++ admin/plugins.php
@@ -101,7 +101,12 @@ function perflab_render_plugins_ui() {
 /**
  * Renders individual plugin cards.
  *
+ * This is adapted from `WP_Plugin_Install_List_Table::display_rows()` in core.
+ *
  * @since n.e.x.t
+ *
+ * @see WP_Plugin_Install_List_Table::display_rows()
+ * @link https://github.com/WordPress/wordpress-develop/blob/0b8ca16ea3bd9722bd1a38f8ab68901506b1a0e7/src/wp-admin/includes/class-wp-plugin-install-list-table.php#L467-L830
  *
  * @param array $plugin_data Plugin data from the WordPress.org API.
  */
