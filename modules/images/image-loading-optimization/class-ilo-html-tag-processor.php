acde9b4d9e6e4943f52a95b86dc234b16e69a5bc
Weston Ruter <westonruter@google.com>
2023-11-28 12:50:00 -0800

    Introduce ILO_HTML_Tag_Processor

---
+++ modules/images/image-loading-optimization/class-ilo-html-tag-processor.php
@@ -0,0 +1,310 @@
+<?php
+/**
+ * Image Loading Optimization: ILO_HTML_Tag_Processor class
+ *
+ * @package performance-lab
+ * @since n.e.x.t
+ */
+
+/**
+ * Subclass of WP_HTML_Tag_Processor that adds support for breadcrumbs and a visiting callback.
+ *
+ * @since n.e.x.t
+ */
+class ILO_HTML_Tag_Processor {
+
+	/**
+	 * HTML elements that are self-closing.
+	 *
+	 * @link https://www.w3.org/TR/html5/syntax.html#serializing-html-fragments
+	 * @link https://github.com/ampproject/amp-toolbox-php/blob/c79a0fe558a3c042aee4789bbf33376cca7a733d/src/Html/Tag.php#L206-L232
+	 *
+	 * @var string[]
+	 */
+	const SELF_CLOSING_TAGS = array(
+		'AREA',
+		'BASE',
+		'BASEFONT',
+		'BGSOUND',
+		'BR',
+		'COL',
+		'EMBED',
+		'FRAME',
+		'HR',
+		'IMG',
+		'INPUT',
+		'KEYGEN',
+		'LINK',
+		'META',
+		'PARAM',
+		'SOURCE',
+		'TRACK',
+		'WBR',
+	);
+
+	/**
+	 * The set of HTML tags whose presence will implicitly close a <p> element.
+	 * For example '<p>foo<h1>bar</h1>' should parse the same as '<p>foo</p><h1>bar</h1>'.
+	 *
+	 * @link https://www.w3.org/TR/html-markup/p.html
+	 * @link https://github.com/ampproject/amp-toolbox-php/blob/c79a0fe558a3c042aee4789bbf33376cca7a733d/src/Html/Tag.php#L262-L293
+	 */
+	const P_CLOSING_TAGS = array(
+		'ADDRESS',
+		'ARTICLE',
+		'ASIDE',
+		'BLOCKQUOTE',
+		'DIR',
+		'DL',
+		'FIELDSET',
+		'FOOTER',
+		'FORM',
+		'H1',
+		'H2',
+		'H3',
+		'H4',
+		'H5',
+		'H6',
+		'HEADER',
+		'HR',
+		'MENU',
+		'NAV',
+		'OL',
+		'P',
+		'PRE',
+		'SECTION',
+		'TABLE',
+		'UL',
+	);
+
+	/**
+	 * Open stack tags.
+	 *
+	 * @var string[]
+	 */
+	private $open_stack_tags = array();
+
+	/**
+	 * Open stag indices.
+	 *
+	 * @var int[]
+	 */
+	private $open_stack_indices = array();
+
+	/**
+	 * Processor.
+	 *
+	 * @var WP_HTML_Tag_Processor
+	 */
+	private $processor;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $html HTML to process.
+	 */
+	public function __construct( string $html ) {
+		$this->processor = new WP_HTML_Tag_Processor( $html );
+	}
+
+	/**
+	 * Walk over the document.
+	 *
+	 * Whenever an open tag is encountered, invoke the supplied $open_tag_callback and pass the tag name and breadcrumbs.
+	 *
+	 * @param Closure $open_tag_callback Open tag callback. The processor instance is passed as the sole argument.
+	 */
+	public function walk( Closure $open_tag_callback ) {
+		$p = $this->processor;
+
+		/*
+		 * The keys for the following two arrays correspond to each other. Given the following document:
+		 *
+		 * <html>
+		 *   <head>
+		 *   </head>
+		 *   <body>
+		 *     <p>Hello!</p>
+		 *     <img src="lcp.png">
+		 *   </body>
+		 * </html>
+		 *
+		 * The two upon processing the IMG element, the two arrays should be equal to the following:
+		 *
+		 * $open_stack_tags    = array( 'HTML', 'BODY', 'IMG' );
+		 * $open_stack_indices = array( 0, 1, 1 );
+		 */
+		$this->open_stack_tags    = array();
+		$this->open_stack_indices = array();
+		while ( $p->next_tag( array( 'tag_closers' => 'visit' ) ) ) {
+			$tag_name = $p->get_tag();
+			if ( ! $p->is_tag_closer() ) {
+
+				// Close an open P tag when a P-closing tag is encountered.
+				if ( in_array( $tag_name, self::P_CLOSING_TAGS, true ) ) {
+					$i = array_search( 'P', $this->open_stack_tags, true );
+					if ( false !== $i ) {
+						array_splice( $this->open_stack_tags, $i );
+						array_splice( $this->open_stack_indices, count( $this->open_stack_tags ) );
+					}
+				}
+
+				$level                   = count( $this->open_stack_tags );
+				$this->open_stack_tags[] = $tag_name;
+
+				if ( ! isset( $this->open_stack_indices[ $level ] ) ) {
+					$this->open_stack_indices[ $level ] = 0;
+				} else {
+					++$this->open_stack_indices[ $level ];
+				}
+
+				// TODO: We should consider not collecting metrics when the admin bar is shown and the user is logged-in.
+				// Only increment the tag index at this level only if it isn't the admin bar, since the presence of the
+				// admin bar can throw off the indices.
+				if ( 'DIV' === $tag_name && $p->get_attribute( 'id' ) === 'wpadminbar' ) {
+					--$this->open_stack_indices[ $level ];
+				}
+
+				// Invoke the callback to do processing.
+				$open_tag_callback( $tag_name, $this->get_breadcrumbs() );
+
+				// Immediately pop off self-closing tags.
+				if ( in_array( $tag_name, self::SELF_CLOSING_TAGS, true ) ) {
+					array_pop( $this->open_stack_tags );
+				}
+			} else {
+				// If the closing tag is for self-closing tag, we ignore it since it was already handled above.
+				if ( in_array( $tag_name, self::SELF_CLOSING_TAGS, true ) ) {
+					continue;
+				}
+
+				// Since SVG and MathML can have a lot more self-closing/empty tags, potentially pop off the stack until getting to the open tag.
+				$did_splice = false;
+				if ( 'SVG' === $tag_name || 'MATH' === $tag_name ) {
+					$i = array_search( $tag_name, $this->open_stack_tags, true );
+					if ( false !== $i ) {
+						array_splice( $this->open_stack_tags, $i );
+						$did_splice = true;
+					}
+				}
+
+				if ( ! $did_splice ) {
+					$popped_tag_name = array_pop( $this->open_stack_tags );
+					if ( $popped_tag_name !== $tag_name ) {
+						error_log( "Expected popped tag stack element $popped_tag_name to match the currently visited closing tag $tag_name." ); // phpcs:ignore
+					}
+				}
+				array_splice( $this->open_stack_indices, count( $this->open_stack_tags ) + 1 );
+			}
+		}
+	}
+
+	/**
+	 * Returns the uppercase name of the matched tag.
+	 *
+	 * This is a wrapper around the underlying HTML_Tag_Processor method of the same name since only a limited number of
+	 * methods can be exposed to prevent moving the pointer in such a way as the breadcrumb calculation is invalidated.
+	 *
+	 * @see WP_HTML_Tag_Processor::get_tag()
+	 *
+	 * @return string|null Name of currently matched tag in input HTML, or `null` if none found.
+	 */
+	public function get_tag() {
+		return $this->processor->get_tag();
+	}
+
+	/**
+	 * Gets breadcrumbs for the current open tag.
+	 *
+	 * Breadcrumbs are constructed to match the format from detect.js.
+	 *
+	 * @return array<array{tagName: string, index: int}> Breadcrumbs.
+	 */
+	public function get_breadcrumbs(): array {
+		$breadcrumbs = array();
+		foreach ( $this->open_stack_tags as $i => $breadcrumb_tag_name ) {
+			$breadcrumbs[] = array(
+				'tagName' => $breadcrumb_tag_name, // TODO: Just 'tag'.
+				'index'   => $this->open_stack_indices[ $i ],
+			);
+		}
+		return $breadcrumbs;
+	}
+
+	/**
+	 * Removes the fetchpriority attribute from the current node being walked over.
+	 *
+	 * Also sets an attribute to indicate that the attribute was removed.
+	 *
+	 * @return bool Whether an attribute was removed.
+	 */
+	public function remove_fetchpriority_attribute(): bool {
+		$p = $this->processor;
+		if ( $p->get_attribute( 'fetchpriority' ) ) {
+			$p->set_attribute( 'data-ilo-removed-fetchpriority', $p->get_attribute( 'fetchpriority' ) );
+			return $p->remove_attribute( 'fetchpriority' );
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Returns the value of a requested attribute from a matched tag opener if that attribute exists.
+	 *
+	 * This is a wrapper around the underlying HTML_Tag_Processor method of the same name since only a limited number of
+	 * methods can be exposed to prevent moving the pointer in such a way as the breadcrumb calculation is invalidated.
+	 *
+	 * @see WP_HTML_Tag_Processor::get_attribute()
+	 *
+	 * @param string $name Name of attribute whose value is requested.
+	 * @return string|true|null Value of attribute or `null` if not available. Boolean attributes return `true`.
+	 */
+	public function get_attribute( string $name ) {
+		return $this->processor->get_attribute( $name );
+	}
+
+	/**
+	 * Updates or creates a new attribute on the currently matched tag with the passed value.
+	 *
+	 * This is a wrapper around the underlying HTML_Tag_Processor method of the same name since only a limited number of
+	 * methods can be exposed to prevent moving the pointer in such a way as the breadcrumb calculation is invalidated.
+	 *
+	 * @see WP_HTML_Tag_Processor::set_attribute()
+	 *
+	 * @param string      $name  The attribute name to target.
+	 * @param string|bool $value The new attribute value.
+	 * @return bool Whether an attribute value was set.
+	 */
+	public function set_attribute( string $name, $value ): bool {
+		return $this->processor->set_attribute( $name, $value );
+	}
+
+	/**
+	 * Removes an attribute from the currently-matched tag.
+	 *
+	 * This is a wrapper around the underlying HTML_Tag_Processor method of the same name since only a limited number of
+	 * methods can be exposed to prevent moving the pointer in such a way as the breadcrumb calculation is invalidated.
+	 *
+	 * @see WP_HTML_Tag_Processor::remove_attribute()
+	 *
+	 * @param string $name The attribute name to remove.
+	 * @return bool Whether an attribute was removed.
+	 */
+	public function remove_attribute( string $name ): bool {
+		return $this->processor->remove_attribute( $name );
+	}
+
+	/**
+	 * Returns the string representation of the HTML Tag Processor.
+	 *
+	 * This is a wrapper around the underlying HTML_Tag_Processor method of the same name since only a limited number of
+	 * methods can be exposed to prevent moving the pointer in such a way as the breadcrumb calculation is invalidated.
+	 *
+	 * @see WP_HTML_Tag_Processor::get_updated_html()
+	 *
+	 * @return string The processed HTML.
+	 */
+	public function get_updated_html(): string {
+		return $this->processor->get_updated_html();
+	}
+}
--- modules/images/image-loading-optimization/optimization.php
+++ modules/images/image-loading-optimization/optimization.php
@@ -10,69 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-/**
- * HTML elements that are self-closing.
- *
- * @link https://www.w3.org/TR/html5/syntax.html#serializing-html-fragments
- * @link https://github.com/ampproject/amp-toolbox-php/blob/c79a0fe558a3c042aee4789bbf33376cca7a733d/src/Html/Tag.php#L206-L232
- *
- * @var string[]
- */
-const ILO_SELF_CLOSING_TAGS = array(
-	'AREA',
-	'BASE',
-	'BASEFONT',
-	'BGSOUND',
-	'BR',
-	'COL',
-	'EMBED',
-	'FRAME',
-	'HR',
-	'IMG',
-	'INPUT',
-	'KEYGEN',
-	'LINK',
-	'META',
-	'PARAM',
-	'SOURCE',
-	'TRACK',
-	'WBR',
-);
-
-/**
- * The set of HTML tags whose presence will implicitly close a <p> element.
- * For example '<p>foo<h1>bar</h1>' should parse the same as '<p>foo</p><h1>bar</h1>'.
- *
- * @link https://www.w3.org/TR/html-markup/p.html
- * @link https://github.com/ampproject/amp-toolbox-php/blob/c79a0fe558a3c042aee4789bbf33376cca7a733d/src/Html/Tag.php#L262-L293
- */
-const ILO_P_CLOSING_TAGS = array(
-	'ADDRESS',
-	'ARTICLE',
-	'ASIDE',
-	'BLOCKQUOTE',
-	'DIR',
-	'DL',
-	'FIELDSET',
-	'FOOTER',
-	'FORM',
-	'H1',
-	'H2',
-	'H3',
-	'H4',
-	'H5',
-	'H6',
-	'HEADER',
-	'HR',
-	'MENU',
-	'NAV',
-	'OL',
-	'P',
-	'PRE',
-	'SECTION',
-	'TABLE',
-	'UL',
-);
+require_once __DIR__ . '/class-ilo-html-tag-processor.php';
 
 /**
  * Adds template output buffer filter for optimization if eligible.
@@ -137,122 +75,6 @@ function ilo_construct_preload_links( array $lcp_images_by_minimum_viewport_widt
 }
 
 /**
- * Walks the provided HTML document, invoking the callback at each open tag.
- *
- * @param string   $html              Complete HTML document.
- * @param callable $open_tag_callback Callback to invoke at each open tag. Callback is passed instance of
- *                                    WP_HTML_Tag_Processor as well as the breadcrumbs for the current element.
- * @return string Updated HTML if modified by callback.
- */
-function ilo_walk_document( string $html, callable $open_tag_callback ): string {
-	$p = new WP_HTML_Tag_Processor( $html );
-
-	/*
-	 * The keys for the following two arrays correspond to each other. Given the following document:
-	 *
-	 * <html>
-	 *   <head>
-	 *   </head>
-	 *   <body>
-	 *     <p>Hello!</p>
-	 *     <img src="lcp.png">
-	 *   </body>
-	 * </html>
-	 *
-	 * The two upon processing the IMG element, the two arrays should be equal to the following:
-	 *
-	 * $open_stack_tags    = array( 'HTML', 'BODY', 'IMG' );
-	 * $open_stack_indices = array( 0, 1, 1 );
-	 */
-	$open_stack_tags    = array();
-	$open_stack_indices = array();
-	while ( $p->next_tag( array( 'tag_closers' => 'visit' ) ) ) {
-		$tag_name = $p->get_tag();
-		if ( ! $p->is_tag_closer() ) {
-
-			// Close an open P tag when a P-closing tag is encountered.
-			if ( in_array( $tag_name, ILO_P_CLOSING_TAGS, true ) ) {
-				$i = array_search( 'P', $open_stack_tags, true );
-				if ( false !== $i ) {
-					array_splice( $open_stack_tags, $i );
-					array_splice( $open_stack_indices, count( $open_stack_tags ) );
-				}
-			}
-
-			$level             = count( $open_stack_tags );
-			$open_stack_tags[] = $tag_name;
-
-			if ( ! isset( $open_stack_indices[ $level ] ) ) {
-				$open_stack_indices[ $level ] = 0;
-			} else {
-				++$open_stack_indices[ $level ];
-			}
-
-			// TODO: We should consider not collecting metrics when the admin bar is shown and the user is logged-in.
-			// Only increment the tag index at this level only if it isn't the admin bar, since the presence of the
-			// admin bar can throw off the indices.
-			if ( 'DIV' === $tag_name && $p->get_attribute( 'id' ) === 'wpadminbar' ) {
-				--$open_stack_indices[ $level ];
-			}
-
-			// Construct the breadcrumbs to match the format from detect.js.
-			$breadcrumbs = array();
-			foreach ( $open_stack_tags as $i => $breadcrumb_tag_name ) {
-				$breadcrumbs[] = array(
-					'tagName' => $breadcrumb_tag_name,
-					'index'   => $open_stack_indices[ $i ],
-				);
-			}
-
-			// Invoke the callback to do processing.
-			$open_tag_callback( $p, $breadcrumbs );
-
-			// Immediately pop off self-closing tags.
-			if ( in_array( $tag_name, ILO_SELF_CLOSING_TAGS, true ) ) {
-				array_pop( $open_stack_tags );
-			}
-		} else {
-			// If the closing tag is for self-closing tag, we ignore it since it was already handled above.
-			if ( in_array( $tag_name, ILO_SELF_CLOSING_TAGS, true ) ) {
-				continue;
-			}
-
-			// Since SVG and MathML can have a lot more self-closing/empty tags, potentially pop off the stack until getting to the open tag.
-			$did_splice = false;
-			if ( 'SVG' === $tag_name || 'MATH' === $tag_name ) {
-				$i = array_search( $tag_name, $open_stack_tags, true );
-				if ( false !== $i ) {
-					array_splice( $open_stack_tags, $i );
-					$did_splice = true;
-				}
-			}
-
-			if ( ! $did_splice ) {
-				$popped_tag_name = array_pop( $open_stack_tags );
-				if ( $popped_tag_name !== $tag_name ) {
-					error_log( "Expected popped tag stack element {$popped_tag_name} to match the currently visited closing tag $tag_name." ); // phpcs:ignore
-				}
-			}
-			array_splice( $open_stack_indices, count( $open_stack_tags ) + 1 );
-		}
-	}
-
-	return $p->get_updated_html();
-}
-
-/**
- * Removes fetchpriority from the current tag if present.
- *
- * @param WP_HTML_Tag_Processor $p Processor instance.
- */
-function ilo_remove_fetchpriority_from_current_tag_processor_node( WP_HTML_Tag_Processor $p ) {
-	if ( $p->get_attribute( 'fetchpriority' ) ) {
-		$p->set_attribute( 'data-ilo-removed-fetchpriority', $p->get_attribute( 'fetchpriority' ) );
-		$p->remove_attribute( 'fetchpriority' );
-	}
-}
-
-/**
  * Optimizes template output buffer.
  *
  * @param string $buffer Template output buffer.
@@ -272,45 +94,50 @@ function ilo_optimize_template_output_buffer( string $buffer ): string {
 		if ( 1 === count( $lcp_images_by_minimum_viewport_widths ) && 1 === count( $breakpoint_lcp_images ) ) {
 			$lcp_element = current( $lcp_images_by_minimum_viewport_widths );
 
-			$buffer = ilo_walk_document(
-				$buffer,
-				static function ( WP_HTML_Tag_Processor $p, array $breadcrumbs ) use ( $lcp_element ) {
-					if ( 'IMG' !== $p->get_tag() ) {
+			$processor = new ILO_HTML_Tag_Processor( $buffer );
+			$processor->walk(
+				static function () use ( $processor, $lcp_element ) {
+					if ( $processor->get_tag() !== 'IMG' ) {
 						return;
 					}
-					if ( $breadcrumbs === $lcp_element['breadcrumbs'] ) {
-						$p->set_attribute( 'fetchpriority', 'high' );
-						$p->set_attribute( 'data-ilo-added-fetchpriority', true );
+
+					if ( $processor->get_breadcrumbs() === $lcp_element['breadcrumbs'] ) {
+						$processor->set_attribute( 'fetchpriority', 'high' );
+						$processor->set_attribute( 'data-ilo-added-fetchpriority', true );
 					} else {
-						ilo_remove_fetchpriority_from_current_tag_processor_node( $p );
+						$processor->remove_fetchpriority_attribute();
 					}
 				}
 			);
+			$buffer = $processor->get_updated_html();
+
 			// TODO: We could also add the preload links here.
 		} else {
 			// If there is not exactly one LCP element, we need to remove fetchpriority from all images while also
 			// capturing the attributes from the LCP element which we can then use for preload links.
-			$buffer = ilo_walk_document(
-				$buffer,
-				static function ( WP_HTML_Tag_Processor $p, array $breadcrumbs ) use ( &$lcp_images_by_minimum_viewport_widths ) {
-					if ( 'IMG' !== $p->get_tag() ) {
+			$processor = new ILO_HTML_Tag_Processor( $buffer );
+			$processor->walk(
+				static function () use ( $processor, &$lcp_images_by_minimum_viewport_widths ) {
+					if ( $processor->get_tag() !== 'IMG' ) {
 						return;
 					}
-					ilo_remove_fetchpriority_from_current_tag_processor_node( $p );
 
-					// Capture the attributes from the LCP element to use in preload links.
-					if ( count( $lcp_images_by_minimum_viewport_widths ) > 1 ) {
+					$processor->remove_fetchpriority_attribute();
+
+					// Capture the attributes from the LCP elements to use in preload links.
+					if ( count( $lcp_images_by_minimum_viewport_widths ) > 1 ) { // TODO: Why?
 						foreach ( $lcp_images_by_minimum_viewport_widths as &$lcp_element ) {
-							if ( $lcp_element && $lcp_element['breadcrumbs'] === $breadcrumbs ) {
+							if ( $lcp_element && $lcp_element['breadcrumbs'] === $processor->get_breadcrumbs() ) {
 								$lcp_element['attributes'] = array();
 								foreach ( array( 'src', 'srcset', 'sizes', 'crossorigin', 'integrity' ) as $attr_name ) {
-									$lcp_element['attributes'][ $attr_name ] = $p->get_attribute( $attr_name );
+									$lcp_element['attributes'][ $attr_name ] = $processor->get_attribute( $attr_name );
 								}
 							}
 						}
 					}
 				}
 			);
+			$buffer = $processor->get_updated_html();
 
 			$preload_links = ilo_construct_preload_links( $lcp_images_by_minimum_viewport_widths );
 
