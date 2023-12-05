c0ef9377e539822501f6cfa2c4aa80416ddb656e
Weston Ruter <westonruter@google.com>
2023-12-05 15:44:16 -0800

    Add stubs for data tests

---
+++ tests/modules/images/image-loading-optimization/storage/data-tests.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Tests for image-loading-optimization module storage/data.php.
+ *
+ * @package performance-lab
+ * @group   image-loading-optimization
+ */
+
+class Image_Loading_Optimization_Storage_Data_Tests extends WP_UnitTestCase {
+
+	/**
+	 * Test ilo_get_url_metric_freshness_ttl().
+	 *
+	 * @test
+	 * @covers ::ilo_get_url_metric_freshness_ttl
+	 */
+	public function test_ilo_get_url_metric_freshness_ttl() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test ilo_can_optimize_response().
+	 *
+	 * @test
+	 * @covers ::ilo_can_optimize_response
+	 */
+	public function test_ilo_can_optimize_response() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test ilo_get_normalized_query_vars().
+	 *
+	 * @test
+	 * @covers ::ilo_get_normalized_query_vars
+	 */
+	public function test_ilo_get_normalized_query_vars() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test ilo_get_url_metrics_slug().
+	 *
+	 * @test
+	 * @covers ::ilo_get_url_metrics_slug
+	 */
+	public function test_ilo_get_url_metrics_slug() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test ilo_get_url_metrics_storage_nonce().
+	 *
+	 * @test
+	 * @covers ::ilo_get_url_metrics_storage_nonce
+	 */
+	public function test_ilo_get_url_metrics_storage_nonce() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test ilo_verify_url_metrics_storage_nonce().
+	 *
+	 * @test
+	 * @covers ::ilo_verify_url_metrics_storage_nonce
+	 */
+	public function test_ilo_verify_url_metrics_storage_nonce() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test ilo_unshift_url_metrics().
+	 *
+	 * @test
+	 * @covers ::ilo_unshift_url_metrics
+	 */
+	public function test_ilo_unshift_url_metrics() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test ilo_get_breakpoint_max_widths().
+	 *
+	 * @test
+	 * @covers ::ilo_get_breakpoint_max_widths
+	 */
+	public function test_ilo_get_breakpoint_max_widths() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test ilo_get_url_metrics_breakpoint_sample_size().
+	 *
+	 * @test
+	 * @covers ::ilo_get_url_metrics_breakpoint_sample_size
+	 */
+	public function test_ilo_get_url_metrics_breakpoint_sample_size() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test ilo_group_url_metrics_by_breakpoint().
+	 *
+	 * @test
+	 * @covers ::ilo_group_url_metrics_by_breakpoint
+	 */
+	public function test_ilo_group_url_metrics_by_breakpoint() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test ilo_get_lcp_elements_by_minimum_viewport_widths().
+	 *
+	 * @test
+	 * @covers ::ilo_get_lcp_elements_by_minimum_viewport_widths
+	 */
+	public function test_ilo_get_lcp_elements_by_minimum_viewport_widths() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test ilo_get_needed_minimum_viewport_widths().
+	 *
+	 * @test
+	 * @covers ::ilo_get_needed_minimum_viewport_widths
+	 */
+	public function test_ilo_get_needed_minimum_viewport_widths() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test ilo_needs_url_metric_for_breakpoint().
+	 *
+	 * @test
+	 * @covers ::ilo_needs_url_metric_for_breakpoint
+	 */
+	public function test_ilo_needs_url_metric_for_breakpoint() {
+		$this->markTestIncomplete();
+	}
+}
