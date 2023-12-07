65f15b01e86c20075ad13e3092bae8eebe7fe8f8
Pascal Birchler <pascalb@google.com>
2023-12-07 11:23:57 +0100

    Skip tests if extension is missing

--- tests/modules/images/dominant-color-images/dominant-color-image-editor-imageick-test.php
+++ tests/modules/images/dominant-color-images/dominant-color-image-editor-imageick-test.php
@@ -31,6 +31,9 @@ class Dominant_Color_Image_Editor_Imageick_Test extends DominantColorTestCase {
 	 * @covers       Dominant_Color_Image_Editor_GD::get_dominant_color
 	 */
 	public function test_get_dominant_color( $image_path, $expected_color, $expected_transparency ) {
+		if ( ! extension_loaded( 'imagick' ) || ! class_exists( 'Imagick', false ) ) {
+			$this->markTestSkipped( 'The Imagick PHP extension is not loaded.' );
+		}
 
 		$attachment_id = self::factory()->attachment->create_upload_object( $image_path );
 		wp_maybe_generate_attachment_metadata( get_post( $attachment_id ) );
@@ -52,6 +55,9 @@ class Dominant_Color_Image_Editor_Imageick_Test extends DominantColorTestCase {
 	 * @covers       Dominant_Color_Image_Editor_GD::get_dominant_color
 	 */
 	public function test_get_dominant_color_invalid( $image_path, $expected_color, $expected_transparency ) {
+		if ( ! extension_loaded( 'imagick' ) || ! class_exists( 'Imagick', false ) ) {
+			$this->markTestSkipped( 'The Imagick PHP extension is not loaded.' );
+		}
 
 		$attachment_id = self::factory()->attachment->create_upload_object( $image_path );
 		wp_maybe_generate_attachment_metadata( get_post( $attachment_id ) );
@@ -71,6 +77,9 @@ class Dominant_Color_Image_Editor_Imageick_Test extends DominantColorTestCase {
 	 * @covers       Dominant_Color_Image_Editor_GD::get_dominant_color
 	 */
 	public function test_get_dominant_color_none_images( $image_path ) {
+		if ( ! extension_loaded( 'imagick' ) || ! class_exists( 'Imagick', false ) ) {
+			$this->markTestSkipped( 'The Imagick PHP extension is not loaded.' );
+		}
 
 		$attachment_id = self::factory()->attachment->create_upload_object( $image_path );
 		wp_maybe_generate_attachment_metadata( get_post( $attachment_id ) );
