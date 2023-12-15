23e8d3dcb5b349244402837cb3930040bbd4cef7
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2023-12-15 10:24:53 +0530

    Apply suggestions from code review
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>
    Co-authored-by: Weston Ruter <westonruter@google.com>

--- admin/load.php
+++ admin/load.php
@@ -551,6 +551,7 @@ function perflab_activate_plugin() {
 
 	check_admin_referer( "perflab_activate_plugin_{$plugin_slug}" );
 
+	// Since we know the plugin folder name, for efficiency we can simply look for the plugin main file there.
 	$plugins = get_plugins( '/' . $plugin_slug );
 
 	if ( empty( $plugins ) ) {
@@ -664,10 +665,10 @@ function perflab_print_plugin_activation_script() {
 				return;
 			}
 			/*
-				* WordPress core uses a 1s timeout for updating the activation link,
-				* so we set a 1.5 timeout here to ensure our changes get updated after
-				* the core changes have taken place.
-				*/
+			* WordPress core uses a 1s timeout for updating the activation link,
+			* so we set a 1.5 timeout here to ensure our changes get updated after
+			* the core changes have taken place.
+			*/
 			setTimeout( function() {
 				var plugin_url = target_element.attr( 'href' );
 				if ( ! plugin_url ) {
