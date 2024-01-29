cef64a3060a069fd15fae1af3bb5820924e7d5ba
Mukesh Panchal <mukeshpanchal27@gmail.com>
2024-01-29 17:18:01 +0530

    Add test file

---
+++ tests/plugins/webp-uploads/test-plugins.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Class Plugin Test Suite
+ *
+ * @package performance-lab
+ */
+
+/**
+ * Plugin test case.
+ */
+class PluginTest extends WP_UnitTestCase {
+
+	public function test_plugin_does_not_fatal() {
+		$this->assertTrue( true );
+	}
+}
