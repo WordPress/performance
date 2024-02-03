c15ac74b20844101a3e95bb2a40e5aa0130d630d
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-02-03 23:03:52 +0530

    Update partytown module tests cases

--- tests/modules/js-and-css/partytown-web-worker/partytown-web-worker-test.php
+++ tests/modules/js-and-css/partytown-web-worker/partytown-web-worker-test.php
@@ -1,70 +1,106 @@
 <?php
 /**
- * Test cases for Web Worker Module
+ * Test cases for Partytown Web Worker module.
  *
  * @package performance-lab
  */
-class Web_Worker_Test extends WP_UnitTestCase {
+class Partytown_Web_Worker_Tests extends WP_UnitTestCase {
 
 	/**
 	 * @covers ::perflab_partytown_web_worker_configuration
 	 */
 	public function test_perflab_partytown_web_worker_configuration() {
-		$this->assertNotFalse(
-			has_action( 'wp_head', 'perflab_partytown_web_worker_configuration' )
-		);
+		$wp_content_dir        = WP_CONTENT_DIR;
+		$partytown_assets_path = 'assets/js/partytown/';
+		$config                = perflab_partytown_web_worker_configuration();
 
-		$config_output = get_echo( 'perflab_partytown_web_worker_configuration' );
+		$this->assertArrayHasKey( 'lib', $config );
+		$this->assertArrayHasKey( 'forward', $config );
+		$this->assertEmpty( $config['forward'] );
+		$this->assertIsArray( $config['forward'] );
+		$this->assertStringStartsWith( '/' . basename( $wp_content_dir ), $config['lib'] );
+		$this->assertStringEndsWith( $partytown_assets_path, $config['lib'] );
 
-		$desired_output_string_chunks = array(
-			'<script>',
-			'window.partytown = ',
-			'lib',
-			'</script>',
-		);
-
-		foreach ( $desired_output_string_chunks as $chunk ) {
-			$this->assertStringContainsString( $chunk, $config_output );
-		}
-
-		// Add a filter to modify the PartyTown configuration.
+		// Test `perflab_partytown_configuration` filter.
 		add_filter(
 			'perflab_partytown_configuration',
 			static function ( $config ) {
-				$config['lib']   = '/partytown/';
-				$config['debug'] = true;
+				$config['forward'] = array( 'datalayer.push' );
+				$config['debug']   = true;
 				return $config;
 			}
 		);
 
-		$config_output = get_echo( 'perflab_partytown_web_worker_configuration' );
+		$config = perflab_partytown_web_worker_configuration();
 
-		$desired_output_string_chunks = array(
-			'<script>',
-			'window.partytown = ',
-			'lib',
-			'/partytown',
-			'debug',
-			'</script>',
-		);
-
-		foreach ( $desired_output_string_chunks as $chunk ) {
-			$this->assertStringContainsString( $chunk, $config_output );
-		}
+		$this->assertArrayHasKey( 'forward', $config );
+		$this->assertArrayHasKey( 'debug', $config );
+		$this->assertNotEmpty( $config['forward'] );
+		$this->assertIsArray( $config['forward'] );
+		$this->assertTrue( $config['debug'] );
+		$this->assertContains( 'datalayer.push', $config['forward'] );
+		remove_all_filters( 'perflab_partytown_configuration' );
 	}
 
 	/**
 	 * @covers ::perflab_partytown_web_worker_init
 	 */
 	public function test_perflab_partytown_web_worker_init() {
-		$this->assertNotFalse(
-			has_action(
-				'wp_enqueue_scripts',
-				'perflab_partytown_web_worker_init'
-			)
-		);
+		global $wp_scripts;
 
-		perflab_partytown_web_worker_init();
+		$this->assertEquals( 10, has_action( 'wp_enqueue_scripts', 'perflab_partytown_web_worker_init' ) );
+
+		$wp_content_dir   = WP_CONTENT_DIR;
+		$partytown_config = perflab_partytown_web_worker_configuration();
+		$partytown_lib    = dirname( $wp_content_dir ) . $partytown_config['lib'];
+		$before_data      = $wp_scripts->get_inline_script_data( 'partytown', 'before' );
+		$after_data       = $wp_scripts->get_inline_script_data( 'partytown', 'after' );
+
+		$this->assertTrue( wp_script_is( 'partytown', 'registered' ) );
+
+		$this->assertNotEmpty( $before_data );
+		$this->assertNotEmpty( $after_data );
+		$this->assertEquals(
+			sprintf( 'window.partytown = %s;', wp_json_encode( $partytown_config ) ),
+			$before_data
+		);
+		$this->assertEquals( file_get_contents( $partytown_lib . 'partytown.js' ), $after_data );
+		$this->assertTrue( wp_script_is( 'partytown', 'registered' ) );
+
+		// Ensure that Partytown is enqueued when a script depends on it.
+		wp_enqueue_script( 'partytown-test', 'https://example.com/test.js', array( 'partytown' ) );
 		$this->assertTrue( wp_script_is( 'partytown', 'enqueued' ) );
+		$this->assertTrue( wp_script_is( 'partytown-test', 'enqueued' ) );
+
+		// Reset the state.
+		$wp_scripts->remove( 'partytown' );
+		$wp_scripts->remove( 'partytown-test' );
+	}
+
+	/**
+	 * @covers ::perflab_get_partytown_handles
+	 */
+	public function test_perflab_get_partytown_handles() {
+		global $wp_scripts;
+
+		$handles = perflab_get_partytown_handles();
+
+		$this->assertEmpty( $handles );
+		$this->assertIsArray( $handles );
+
+		// Enqueue a script that depends on Partytown.
+		wp_enqueue_script( 'partytown-test', 'https://example.com/test.js', array( 'partytown' ) );
+
+		$handles = perflab_get_partytown_handles();
+
+		$this->assertIsArray( $handles );
+		$this->assertNotEmpty( $handles );
+		$this->assertContains( 'partytown-test', $handles );
+		$this->assertTrue( wp_script_is( 'partytown', 'enqueued' ) );
+		$this->assertTrue( wp_script_is( 'partytown-test', 'enqueued' ) );
+
+		// Reset the state.
+		$wp_scripts->remove( 'partytown' );
+		$wp_scripts->remove( 'partytown-test' );
 	}
 }
