5dfcfdfee96f090cd18889dbc6bc9b3a68d875ac
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-02-03 19:58:50 +0530

    Add webpack config for copying partytown lib

--- webpack.config.js
+++ webpack.config.js
@@ -15,3 +15,62 @@ const sharedConfig = {
 	entry: {},
 	output: {},
 };
+
+/**
+ * Transformer to get version from package.json and return it as a PHP file.
+ *
+ * @param {Buffer} content      The content as a Buffer of the file being transformed.
+ * @param {string} absoluteFrom The absolute path to the file being transformed.
+ *
+ * @return {Buffer|string} The transformed content.
+ */
+const assetDataTransformer = ( content, absoluteFrom ) => {
+	if ( 'package.json' !== path.basename( absoluteFrom ) ) {
+		return content;
+	}
+
+	const contentAsString = content.toString();
+	const contentAsJson = JSON.parse( contentAsString );
+	const { version } = contentAsJson;
+
+	return `<?php return array('dependencies' => array(), 'version' => '${ version }');`;
+};
+
+const partytown = () => {
+	const source = path.resolve(
+		__dirname,
+		'node_modules/@builder.io/partytown'
+	);
+	const destination = path.resolve(
+		__dirname,
+		'modules/js-and-css/partytown-web-worker/assets/js/partytown'
+	);
+
+	return {
+		...sharedConfig,
+		plugins: [
+			new CopyWebpackPlugin( {
+				patterns: [
+					{
+						from: `${ source }/lib/`,
+						to: `${ destination }`,
+					},
+					{
+						from: `${ source }/package.json`,
+						to: `${ destination }/partytown.asset.php`,
+						transform: {
+							transformer: assetDataTransformer,
+							cache: false,
+						},
+					},
+				],
+			} ),
+			new WebpackBar( {
+				name: 'PartyTown',
+				color: '#FFC107',
+			} ),
+		],
+	};
+};
+
+module.exports = [ partytown ];
