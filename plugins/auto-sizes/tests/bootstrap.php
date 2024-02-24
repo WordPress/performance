679d583016925e97ef021c541e8d940fc174c07b
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-02-24 09:50:53 +0530

    Add tests bootstrap file for standalone plugins

---
+++ plugins/auto-sizes/tests/bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package auto-sizes
+ */
+
+define( 'TESTS_PLUGIN_DIR', dirname( __DIR__ ) );
+define( 'TESTS_PLUGIN_ROOT', dirname( dirname( TESTS_PLUGIN_DIR ) ) );
+
+// Determine correct location for plugins directory to use.
+if ( false !== getenv( 'WP_PLUGIN_DIR' ) ) {
+	define( 'WP_PLUGIN_DIR', getenv( 'WP_PLUGIN_DIR' ) );
+} else {
+	define( 'WP_PLUGIN_DIR', dirname( TESTS_PLUGIN_DIR ) );
+}
+
+// Load Composer dependencies if applicable.
+if ( file_exists( TESTS_PLUGIN_ROOT . '/vendor/autoload.php' ) ) {
+	require_once TESTS_PLUGIN_ROOT . '/vendor/autoload.php';
+}
+
+// Detect where to load the WordPress tests environment from.
+$_tests_dir = WPP_Tests_Helpers::get_path_to_wp_test_dir();
+require_once $_tests_dir . '/includes/functions.php';
+
+// Force plugin to be active.
+$GLOBALS['wp_tests_options'] = array(
+	'active_plugins' => array( basename( TESTS_PLUGIN_DIR ) . '/auto-sizes.php' ),
+);
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
