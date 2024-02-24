4808f24790cee62fba4b8782828f2044111db5eb
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-02-24 08:51:14 +0530

    Add tests helpers.php file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -20,74 +20,30 @@ if ( file_exists( TESTS_PLUGIN_DIR . '/vendor/autoload.php' ) ) {
 }
 
 // Detect where to load the WordPress tests environment from.
-if ( false !== getenv( 'WP_TESTS_DIR' ) ) {
-	$_test_root = getenv( 'WP_TESTS_DIR' );
-} elseif ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	$_test_root = getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit';
-} elseif ( false !== getenv( 'WP_PHPUNIT__DIR' ) ) {
-	$_test_root = getenv( 'WP_PHPUNIT__DIR' );
-} elseif ( file_exists( TESTS_PLUGIN_DIR . '/../../../../tests/phpunit/includes/functions.php' ) ) {
-	$_test_root = TESTS_PLUGIN_DIR . '/../../../../tests/phpunit';
-} else { // Fallback.
-	$_test_root = '/tmp/wordpress-tests-lib';
-}
+$_tests_dir = get_path_to_wp_test_dir();
+require_once $_tests_dir . '/includes/functions.php';
 
-require_once $_test_root . '/includes/functions.php';
+// Force plugin to be active.
+$GLOBALS['wp_tests_options'] = array(
+	'active_plugins' => array( basename( TESTS_PLUGIN_DIR ) . '/load.php' ),
+);
 
-// Check if we use the plugin's test suite. If so, disable the PL plugin and only load the requested plugin.
-$plugin_name = '';
-foreach ( $_SERVER['argv'] as $index => $arg ) {
-	if (
-		'--testsuite' === $arg &&
-		isset( $_SERVER['argv'][ $index + 1 ] ) &&
-		'performance-lab' !== $_SERVER['argv'][ $index + 1 ] &&
-		file_exists( TESTS_PLUGIN_DIR . '/plugins/' . $_SERVER['argv'][ $index + 1 ] )
-	) {
-		$plugin_name = $_SERVER['argv'][ $index + 1 ];
-	}
-}
-
-if ( $plugin_name ) {
-	$plugin_test_path = TESTS_PLUGIN_DIR . '/plugins/' . $plugin_name;
-	tests_add_filter(
-		'plugins_loaded',
-		static function () use ( $plugin_test_path, $plugin_name ) {
-			// Check if plugin has a "plugin/plugin.php" file.
-			if ( file_exists( $plugin_test_path . '/' . $plugin_name . '.php' ) ) {
-				require_once $plugin_test_path . '/' . $plugin_name . '.php';
-			} elseif ( file_exists( $plugin_test_path . '/load.php' ) ) {
-				// Check if plugin has a "plugin/load.php" file.
-				require_once $plugin_test_path . '/load.php';
-			} else {
-				echo "Unable to locate standalone plugin bootstrap file in $plugin_test_path.";
-				exit( 1 );
+// Add filter to ensure the plugin's admin integration and all modules are loaded for tests.
+tests_add_filter(
+	'plugins_loaded',
+	static function () {
+		require_once TESTS_PLUGIN_DIR . '/admin/load.php';
+		require_once TESTS_PLUGIN_DIR . '/admin/server-timing.php';
+		require_once TESTS_PLUGIN_DIR . '/admin/plugins.php';
+		$module_files = glob( TESTS_PLUGIN_DIR . '/modules/*/*/load.php' );
+		if ( $module_files ) {
+			foreach ( $module_files as $module_file ) {
+				require_once $module_file;
 			}
-		},
-		1
-	);
-} else {
-	// Force plugin to be active.
-	$GLOBALS['wp_tests_options'] = array(
-		'active_plugins' => array( basename( TESTS_PLUGIN_DIR ) . '/load.php' ),
-	);
-
-	// Add filter to ensure the plugin's admin integration and all modules are loaded for tests.
-	tests_add_filter(
-		'plugins_loaded',
-		static function () {
-			require_once TESTS_PLUGIN_DIR . '/admin/load.php';
-			require_once TESTS_PLUGIN_DIR . '/admin/server-timing.php';
-			require_once TESTS_PLUGIN_DIR . '/admin/plugins.php';
-			$module_files = glob( TESTS_PLUGIN_DIR . '/modules/*/*/load.php' );
-			if ( $module_files ) {
-				foreach ( $module_files as $module_file ) {
-					require_once $module_file;
-				}
-			}
-		},
-		1
-	);
-}
+		}
+	},
+	1
+);
 
 // Start up the WP testing environment.
-require $_test_root . '/includes/bootstrap.php';
+require $_tests_dir . '/includes/bootstrap.php';
---
+++ tests/helpers.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * This function is documented at <https://github.com/Yoast/wp-test-utils/blob/46566884fe74d2f758273efd71f6c07c7d2e4f53/src/WPIntegration/bootstrap-functions.php>.
+ *
+ * @return string|false The path to the WP test directory, or false if it could not be determined.
+ */
+function get_path_to_wp_test_dir() {
+	// phpcs:disable WordPress.PHP.YodaConditions.NotYoda
+	// phpcs:disable WordPress.PHP.NoSilencedErrors.Discouraged
+	/**
+	 * Normalizes all slashes in a file path to forward slashes.
+	 *
+	 * @param string $path File path.
+	 *
+	 * @return string The file path with normalized slashes.
+	 */
+	$normalize_path = static function ( $path ) {
+		return \str_replace( '\\', '/', $path );
+	};
+
+	if ( \getenv( 'WP_TESTS_DIR' ) !== false ) {
+		$tests_dir = \getenv( 'WP_TESTS_DIR' );
+		$tests_dir = \realpath( $tests_dir );
+		if ( $tests_dir !== false ) {
+			$tests_dir = $normalize_path( $tests_dir ) . '/';
+			if ( \is_dir( $tests_dir ) === true
+				&& @\file_exists( $tests_dir . 'includes/bootstrap.php' )
+			) {
+				return $tests_dir;
+			}
+		}
+
+		unset( $tests_dir );
+	}
+
+	if ( \getenv( 'WP_DEVELOP_DIR' ) !== false ) {
+		$dev_dir = \getenv( 'WP_DEVELOP_DIR' );
+		$dev_dir = \realpath( $dev_dir );
+		if ( $dev_dir !== false ) {
+			$dev_dir = $normalize_path( $dev_dir ) . '/';
+			if ( \is_dir( $dev_dir ) === true
+				&& @\file_exists( $dev_dir . 'tests/phpunit/includes/bootstrap.php' )
+			) {
+				return $dev_dir . 'tests/phpunit/';
+			}
+		}
+
+		unset( $dev_dir );
+	}
+
+	/*
+	 * If neither of the constants was set, check whether the plugin is installed
+	 * in `src/wp-content/plugins`. In that case, this file would be in
+	 * `src/wp-content/plugins/plugin-name/vendor/yoast/wp-test-utils/src/WPIntegration`.
+	 */
+	if ( @\file_exists( __DIR__ . '/../../../../../../../../../tests/phpunit/includes/bootstrap.php' ) ) {
+		$tests_dir = __DIR__ . '/../../../../../../../../../tests/phpunit/';
+		$tests_dir = \realpath( $tests_dir );
+		if ( $tests_dir !== false ) {
+			return $normalize_path( $tests_dir ) . '/';
+		}
+
+		unset( $tests_dir );
+	}
+
+	/*
+	 * Last resort: see if this is a typical WP-CLI scaffold command set-up where a subset of
+	 * the WP test files have been put in the system temp directory.
+	 */
+	$tests_dir = \sys_get_temp_dir() . '/wordpress-tests-lib';
+	$tests_dir = \realpath( $tests_dir );
+	if ( $tests_dir !== false ) {
+		$tests_dir = $normalize_path( $tests_dir ) . '/';
+		if ( \is_dir( $tests_dir ) === true
+			&& @\file_exists( $tests_dir . 'includes/bootstrap.php' )
+		) {
+			return $tests_dir;
+		}
+	}
+
+	return false;
+	// phpcs:enable WordPress.PHP.YodaConditions.NotYoda
+	// phpcs:enable WordPress.PHP.NoSilencedErrors.Discouraged
+}
