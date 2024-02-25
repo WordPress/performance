7f5243be99bab96c3e6cc43494ff4f830586db27
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-02-25 20:33:53 +0530

    Update auto-enabled modules

--- default-enabled-modules.php
+++ default-enabled-modules.php
@@ -1,8 +1,6 @@
 <?php
 /* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
 return array(
-	'images/dominant-color-images',
 	'images/webp-support',
-	'images/webp-uploads',
 );
 /* THIS IS THE END OF THE GENERATED FILE */
