4e739afb8c13252312cb1cad51544e04d221294f
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-03-09 15:37:25 +0530

    Add loader file for site health tests

---
+++ includes/site-health/load.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Site Health tests loader.
+ *
+ * @package performance-lab
+ * @since n.e.x.t
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+if ( ! has_filter( 'user_has_cap', 'wp_maybe_grant_site_health_caps' ) ) {
+	return;
+}
+
+// Site health tests to load.
+$site_health_tests = array(
+	'audit-autoloaded-options',
+	'audit-enqueued-assets',
+	'audit-webp-support'
+);
+
+foreach ( $site_health_tests as $test ) {
+	require_once __DIR__ . '/' . $test . '/helper.php';
+	require_once __DIR__ . '/' . $test . '/hooks.php';
+}
