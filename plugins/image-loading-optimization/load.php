679a5fe425d2990aa8b36df23192cce2a9235d27
Weston Ruter <westonruter@google.com>
2024-03-09 15:03:18 +0800

    Update module to be plugin

--- plugins/image-loading-optimization/load.php
+++ plugins/image-loading-optimization/load.php
@@ -1,8 +1,16 @@
 <?php
 /**
- * Module Name: Image Loading Optimization
+ * Plugin Name: Image Loading Optimization
+ * Plugin URI: https://github.com/WordPress/performance/issues/869
  * Description: Improves accuracy of optimizing the loading of the LCP image by leveraging client-side detection with real user metrics. Also enables output buffering of template rendering which can be filtered.
- * Experimental: Yes
+ * Requires at least: 6.3
+ * Requires PHP: 7.0
+ * Version: 0.1.0
+ * Author: WordPress Performance Team
+ * Author URI: https://make.wordpress.org/performance/
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: image-loading-optimization
  *
  * @package performance-lab
  * @since n.e.x.t
