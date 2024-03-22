5058adf0fa9bb5159a8a5440bbe77a85916d9cdf
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-03-22 17:52:47 +0530

    Add tests bootstrap file for standalone plugins

---
+++ plugins/webp-uploads/tests/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package webp-uploads
+ */
+
+define( 'TESTS_PLUGIN_DIR', dirname( __DIR__ ) );
+define( 'TESTS_PLUGIN_ROOT', dirname( dirname( TESTS_PLUGIN_DIR ) ) );
+
+// Load bootstrap functions.
+require_once TESTS_PLUGIN_ROOT . '/tests/bootstrap-functions.php';
+
+// Initialize the WP testing environment.
+$_tests_dir = PerformanceLab\Tests\init( TESTS_PLUGIN_ROOT, TESTS_PLUGIN_DIR );
+
+// Force plugin to be active.
+$GLOBALS['wp_tests_options'] = array(
+	'active_plugins' => array( basename( TESTS_PLUGIN_DIR ) . '/load.php' ),
+);
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
