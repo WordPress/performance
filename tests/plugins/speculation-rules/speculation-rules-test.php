08e31f73057b2216c3b9a4b28949b006e9cb7b72
Felix Arntz <felixarntz@google.com>
2024-04-01 09:16:23 -0700

    Add tests for generator tags of dominant-color-images and webp-uploads.

--- tests/plugins/speculation-rules/speculation-rules-test.php
+++ tests/plugins/speculation-rules/speculation-rules-test.php
@@ -24,18 +24,6 @@ class Speculation_Rules_Tests extends WP_UnitTestCase {
 		$this->assertSame( 10, has_action( 'wp_head', 'plsr_render_generator_meta_tag' ) );
 	}
 
-	/**
-	 * Test printing the meta generator tag.
-	 *
-	 * @covers ::plsr_render_generator_meta_tag
-	 */
-	public function test_plsr_render_generator_meta_tag() {
-		$tag = get_echo( 'plsr_render_generator_meta_tag' );
-		$this->assertStringStartsWith( '<meta', $tag );
-		$this->assertStringContainsString( 'generator', $tag );
-		$this->assertStringContainsString( 'speculation-rules ' . SPECULATION_RULES_VERSION, $tag );
-	}
-
 	public function data_provider_to_test_print_speculation_rules(): array {
 		return array(
 			'xhtml' => array(
@@ -72,5 +60,17 @@ class Speculation_Rules_Tests extends WP_UnitTestCase {
 		} else {
 			$this->assertStringContainsString( '/* <![CDATA[ */', wp_get_inline_script_tag( '/*...*/' ) );
 		}
+	}
+
+	/**
+	 * Test printing the meta generator tag.
+	 *
+	 * @covers ::plsr_render_generator_meta_tag
+	 */
+	public function test_plsr_render_generator_meta_tag() {
+		$tag = get_echo( 'plsr_render_generator_meta_tag' );
+		$this->assertStringStartsWith( '<meta', $tag );
+		$this->assertStringContainsString( 'generator', $tag );
+		$this->assertStringContainsString( 'speculation-rules ' . SPECULATION_RULES_VERSION, $tag );
 	}
 }
