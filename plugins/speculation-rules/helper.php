7d46229aa1f4ed334ed781258f0a26e301e1f28c
Weston Ruter <westonruter@google.com>
2024-04-15 15:51:51 -0700

    Improve wording
    Co-authored-by: Barry Pollard <barrypollard@google.com>

--- plugins/speculation-rules/helper.php
+++ plugins/speculation-rules/helper.php
@@ -87,7 +87,7 @@ function plsr_get_speculation_rules() {
 							'href_matches' => $href_exclude_paths,
 						),
 					),
-					// Also exclude rel=nofollow links, as plugins like WooCommerce attribute their add-to-cart links.
+					// Also exclude rel=nofollow links, as plugins like WooCommerce use that on their add-to-cart links.
 					array(
 						'not' => array(
 							'selector_matches' => 'a[rel=nofollow]',
