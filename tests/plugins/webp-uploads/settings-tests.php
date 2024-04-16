a6036989965c62e33460168952204d5d8983bde1
Weston Ruter <westonruter@google.com>
2024-04-16 09:22:42 -0700

    Fix deactivate link to refer to correct plugin

--- tests/plugins/webp-uploads/settings-tests.php
+++ tests/plugins/webp-uploads/settings-tests.php
@@ -15,7 +15,7 @@ class WebP_Uploads_Settings_Tests extends WP_UnitTestCase {
 		$this->assertFalse( webp_uploads_add_settings_action_link( false ) );
 
 		$default_action_links = array(
-			'deactivate' => '<a href="plugins.php?action=deactivate&amp;plugin=webp-uploads%2Fload.php&amp;plugin_status=all&amp;paged=1&amp;s&amp;_wpnonce=48f74bdd74" id="deactivate-speculation-rules" aria-label="Deactivate Speculative Loading">Deactivate</a>',
+			'deactivate' => '<a href="plugins.php?action=deactivate&amp;plugin=webp-uploads%2Fload.php&amp;plugin_status=all&amp;paged=1&amp;s&amp;_wpnonce=48f74bdd74" id="deactivate-webp-uploads" aria-label="Deactivate Modern Image Formats">Deactivate</a>',
 		);
 
 		$this->assertSame(
