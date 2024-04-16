587f706c95d2029fecfd31421bce64369688fd67
Matteo Enna <matteo.enna89@gmail.com>
2024-04-16 18:31:45 +0200

    Update includes/admin/load.php
    Co-authored-by: Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>

--- includes/admin/load.php
+++ includes/admin/load.php
@@ -81,7 +81,7 @@ function perflab_admin_pointer( $hook_suffix ) {
 
 	if ( ! in_array( $hook_suffix, array( 'index.php', 'plugins.php' ), true ) ) {
 
-		// Do not show on the settings page and dismiss the pointer
+		// Do not show on the settings page and dismiss the pointer.
 		if ( !empty( $_GET['page'] ) && $_GET['page'] === PERFLAB_SCREEN ) {
 			$dismissed[] = 'perflab-admin-pointer';
 			update_user_meta( $current_user, 'dismissed_wp_pointers', implode( ',', $dismissed ) );
