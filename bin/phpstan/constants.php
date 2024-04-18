7ae4e449a2804354bdfbfdf24f0a984c86301030
Weston Ruter <westonruter@google.com>
2024-04-18 09:19:11 -0700

    Use 0.0.0 as SPECULATION_RULES_VERSION for PHPStan

--- bin/phpstan/constants.php
+++ bin/phpstan/constants.php
@@ -5,5 +5,5 @@
  * @package speculation-rules
  */
 
-define( 'SPECULATION_RULES_VERSION', '1.2.1' );
+define( 'SPECULATION_RULES_VERSION', '0.0.0' );
 define( 'SPECULATION_RULES_MAIN_FILE', 'plugins/speculation-rules/load.php' );
