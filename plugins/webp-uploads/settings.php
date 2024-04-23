711369056e12d379e60d93bfadb30762c7bb4b2f
Adam Silverstein <adamjs@google.com>
2024-04-23 14:35:40 -0600

    Add AVIF output settings, cleanup

--- plugins/webp-uploads/settings.php
+++ plugins/webp-uploads/settings.php
@@ -12,9 +12,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Registers setting for generating both JPEG and WebP versions for image uploads.
+ * Registers setting for generating JPEG in addition to the selected modern format for image uploads.
  *
  * @since 1.0.0
+ * @since n.e.x.t The setting was made more general to cover all modern formats. The "webp" option naming was left unchanged.
  */
 function webp_uploads_register_media_settings_field() {
 	register_setting(
@@ -23,6 +24,16 @@ function webp_uploads_register_media_settings_field() {
 		array(
 			'type'         => 'boolean',
 			'default'      => false,
+			'show_in_rest' => false,
+		)
+	);
+
+	register_setting(
+		'media',
+		'perflab_generate_avif_and_webp',
+		array(
+			'type'         => 'string',
+			'default'      => wp_image_editor_supports( array( 'mime_type' => 'image/avif' ) ) ?  'avif' : 'webp',
 			'show_in_rest' => false,
 		)
 	);
@@ -35,10 +46,24 @@ add_action( 'init', 'webp_uploads_register_media_settings_field' );
  * @since 1.0.0
  */
 function webp_uploads_add_media_settings_field() {
+
+	// If AVIF is supported, add a dropdown to select the output format between AVIF and WebP.
+	if ( wp_image_editor_supports( array( 'mime_type' => 'image/avif' ) ) ) {
+		add_settings_field(
+			'perflab_generate_avif_and_webp',
+			__( 'Modern image format', 'webp-uploads' ),
+			'webp_uploads_generate_avif_webp_setting_callback',
+			'media',
+			is_multisite() ? 'default' : 'uploads',
+			array( 'class' => 'perflab-generate-avif-and-webp' )
+		);
+
+	}
+
 	// Add settings field.
 	add_settings_field(
 		'perflab_generate_webp_and_jpeg',
-		__( 'WebP and JPEG', 'webp-uploads' ),
+		__( 'Also output JPEG', 'webp-uploads' ),
 		'webp_uploads_generate_webp_jpeg_setting_callback',
 		'media',
 		is_multisite() ? 'default' : 'uploads',
@@ -48,23 +73,39 @@ function webp_uploads_add_media_settings_field() {
 add_action( 'admin_init', 'webp_uploads_add_media_settings_field' );
 
 /**
+ * Renders the settings field for the 'perflab_generate_avif_and_webp' setting.
+ *
+ * @since n.e.x.t
+ */
+function webp_uploads_generate_avif_webp_setting_callback() {
+
+	$selected = get_option( 'perflab_generate_avif_and_webp', 'avif' );
+	?>
+			<label for="perflab_generate_avif_and_webp">
+				<select name="perflab_generate_avif_and_webp" id="perflab_generate_avif_and_webp" aria-describedby="perflab_generate_avif_and_webp_description">
+					<option value="webp"<?php selected( 'webp', $selected ); ?>><?php esc_html_e( 'WebP', 'webp-uploads' ); ?></option>
+					<option value="avif"<?php selected( 'avif', $selected ); ?>><?php esc_html_e( 'AVIF', 'webp-uploads' ); ?></option>
+				</select>
+				<?php esc_html_e( 'Generate images in this format', 'webp-uploads' ); ?>
+			</label>
+		<p class="description" id="perflab_generate_avif_and_webp_description"><?php esc_html_e( 'Select the format to use when generating new images from uploaded JPEGs.', 'webp-uploads' ); ?></p>
+	<?php
+}
+
+/**
  * Renders the settings field for the 'perflab_generate_webp_and_jpeg' setting.
  *
  * @since 1.0.0
  */
 function webp_uploads_generate_webp_jpeg_setting_callback() {
-	if ( ! is_multisite() ) {
-		?>
-			</td>
-			<td class="td-full">
-		<?php
-	}
+
 	?>
+
 		<label for="perflab_generate_webp_and_jpeg">
 			<input name="perflab_generate_webp_and_jpeg" type="checkbox" id="perflab_generate_webp_and_jpeg" aria-describedby="perflab_generate_webp_and_jpeg_description" value="1"<?php checked( '1', get_option( 'perflab_generate_webp_and_jpeg' ) ); ?> />
-			<?php esc_html_e( 'Generate JPEG files in addition to WebP', 'webp-uploads' ); ?>
+			<?php esc_html_e( 'Output JPEG images in addition to the modern format', 'webp-uploads' ); ?>
 		</label>
-		<p class="description" id="perflab_generate_webp_and_jpeg_description"><?php esc_html_e( 'Enabling JPEG in addition to WebP can improve compatibility, but will effectively double the filesystem storage use of your images.', 'webp-uploads' ); ?></p>
+		<p class="description" id="perflab_generate_webp_and_jpeg_description"><?php esc_html_e( 'Enabling JPEG in addition to AVIF or WebP can improve compatibility, but will increase the filesystem storage use of your images.', 'webp-uploads' ); ?></p>
 	<?php
 }
 
@@ -79,10 +120,6 @@ function webp_uploads_media_setting_style() {
 	}
 	?>
 	<style>
-		.form-table .perflab-generate-webp-and-jpeg th,
-		.form-table .perflab-generate-webp-and-jpeg td:not(.td-full) {
-			display: none;
-		}
 	</style>
 	<?php
 }
