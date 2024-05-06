8cdebcbaa493ff659fd927f37808153f773eeb49
Adam Silverstein <adamsilverstein@earthboundhosting.com>
2024-05-06 12:16:27 -0600

    Update plugins/webp-uploads/helper.php
    Co-authored-by: Weston Ruter <westonruter@google.com>

--- plugins/webp-uploads/helper.php
+++ plugins/webp-uploads/helper.php
@@ -364,7 +364,7 @@ function webp_uploads_mime_type_supported( string $mime_type ): bool {
 	}
 
 	// In certain server environments GD can report a false positive for AVIF support, see https://github.com/php/php-src/issues/12197.
-	// To ensure the server actually supports AVIF, test for the function used by core..
+	// To ensure the server actually supports AVIF, test for the function used by core.
 	if ( 'image/avif' === $mime_type ) {
 		return function_exists( 'imageavif' );
 	}
