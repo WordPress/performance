f6951704453ef5bf5aeb05cd70766cbc19f0f365
Ashwin Parthasarathi <ashwin.parthasarathi@wisdmlabs.com>
2024-05-08 01:41:33 +0530

    Updated code as per review comments

--- includes/server-timing/hooks.php
+++ includes/server-timing/hooks.php
@@ -2,9 +2,9 @@
 /**
  * Hook callbacks used for Server Timing.
  *
- * @package dominant-color-images
+ * @package performance-lab
  *
- * @since 1.0.0
+ * @since n.e.x.t
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -19,20 +19,17 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @return WP_REST_Response|WP_Error Filtered response.
  */
 function rest_post_dispatch_add_server_timing( $response ) {
-	if ( ! defined( 'REST_REQUEST' ) || ! REST_REQUEST ) {
-		return $response;
-	}
-
-	if ( ! function_exists( 'perflab_server_timing' ) || ! $response instanceof WP_REST_Response ) {
+	if ( ! defined( 'REST_REQUEST' ) || ! REST_REQUEST || ! function_exists( 'perflab_server_timing' ) || ! $response instanceof WP_REST_Response ) {
 		return $response;
 	}
 
 	$server_timing = perflab_server_timing();
 
+	/** This filter is documented in includes/server-timing/class-perflab-server-timing.php */
 	do_action( 'perflab_server_timing_send_header' ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
 
 	$response->header( 'Server-Timing', $server_timing->get_header() );
 
 	return $response;
 }
-add_filter( 'rest_post_dispatch', 'rest_post_dispatch_add_server_timing' );
+add_filter( 'rest_post_dispatch', 'rest_post_dispatch_add_server_timing', PHP_INT_MAX );
