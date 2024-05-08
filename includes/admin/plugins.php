e88f1093bbc35ed3df908e8adc5103fe7bcf5c8d
Ashwin Parthasarathi <ashwin.parthasarathi@wisdmlabs.com>
2024-05-08 15:22:12 +0530

    Updated code to fetch plugin path from plugin list

--- includes/admin/plugins.php
+++ includes/admin/plugins.php
@@ -324,8 +324,16 @@ function perflab_render_plugin_card( array $plugin_data ): void {
 			'<button type="button" class="button button-disabled" disabled="disabled">%s</button>',
 			esc_html( _x( 'Active', 'plugin', 'default' ) )
 		);
-		$plugin_path    = $plugin_data['slug'] . '/load.php';
-		$plugin_links   = apply_filters( "plugin_action_links_{$plugin_path}", array() );
+		$all_plugins    = array_keys( get_plugins() );
+		$plugin_slug    = $plugin_data['slug'];
+		$plugin_path    = '';
+		foreach ( $all_plugins as $path ) {
+			if ( strpos( $path, $plugin_slug ) !== false ) {
+				$plugin_path = $path;
+				break;
+			}
+		}
+		$plugin_links = apply_filters( "plugin_action_links_{$plugin_path}", array() );
 
 		if ( array_key_exists( 'settings', $plugin_links ) ) {
 			$action_links[] = $plugin_links['settings'];
