511b670602e9a9a15a38fc3247101c4f52d72dd8
Weston Ruter <westonruter@google.com>
2024-05-08 14:18:14 -0700

    Move deprecated function to deprecated.php

--- plugins/webp-uploads/helper.php
+++ plugins/webp-uploads/helper.php
@@ -236,36 +236,6 @@ function webp_uploads_generate_image_size( int $attachment_id, string $size, str
 }
 
 /**
- * Returns the attachment sources array ordered by filesize.
- *
- * @since 1.0.0
- * @deprecated This function is not used anymore as of Performance Lab 1.1.0 when this was still part of the WebP Uploads module. It should have been removed as part of <https://github.com/WordPress/performance/pull/302>.
- *
- * @param int    $attachment_id The attachment ID.
- * @param string $size          The attachment size.
- * @return array<string, array{ file: string, filesize: int }> The attachment sources array.
- */
-function webp_uploads_get_attachment_sources( int $attachment_id, string $size = 'thumbnail' ): array {
-	_deprecated_function( __FUNCTION__, 'Performance Lab 1.1.0' );
-
-	// Check for the sources attribute in attachment metadata.
-	$metadata = wp_get_attachment_metadata( $attachment_id );
-
-	// Return full image size sources.
-	if ( 'full' === $size && ! empty( $metadata['sources'] ) ) {
-		return $metadata['sources'];
-	}
-
-	// Return the resized image sources.
-	if ( ! empty( $metadata['sizes'][ $size ]['sources'] ) ) {
-		return $metadata['sizes'][ $size ]['sources'];
-	}
-
-	// Return an empty array if no sources found.
-	return array();
-}
-
-/**
  * Returns mime types that should be used for an image in the specific context.
  *
  * @since 1.0.0
