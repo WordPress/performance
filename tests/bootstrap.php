af1b6023c9d10341d90357150a409fdfc3e73481
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-05-16 17:20:54 +0530

    Set default testsuite

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -46,38 +46,43 @@ foreach ( $_SERVER['argv'] as $index => $arg ) {
 	}
 }
 
-if ( $plugin_name ) {
-	$plugin_test_path = TESTS_PLUGIN_DIR . '/plugins/' . $plugin_name;
+// Set default plugin to load if not specified.
+if ( ! $plugin_name ) {
+	$plugin_name = 'performance-lab';
+}
+
+$plugin_test_path = TESTS_PLUGIN_DIR . '/plugins/' . $plugin_name;
+
+tests_add_filter(
+	'plugins_loaded',
+	static function () use ( $plugin_test_path, $plugin_name ): void {
+		// Check if plugin has a "plugin/plugin.php" file.
+		if ( file_exists( $plugin_test_path . '/' . $plugin_name . '.php' ) ) {
+			require_once $plugin_test_path . '/' . $plugin_name . '.php';
+		} elseif ( file_exists( $plugin_test_path . '/load.php' ) ) {
+			// Check if plugin has a "plugin/load.php" file.
+			require_once $plugin_test_path . '/load.php';
+		} else {
+			echo "Unable to locate standalone plugin bootstrap file in $plugin_test_path.";
+			exit( 1 );
+		}
+	},
+	1
+);
+
+if ( 'performance-lab' === $plugin_name ) {
+	// Add filter to ensure the plugin's admin integration is loaded for tests.
 	tests_add_filter(
 		'plugins_loaded',
-		static function () use ( $plugin_test_path, $plugin_name ): void {
-			// Check if plugin has a "plugin/plugin.php" file.
-			if ( file_exists( $plugin_test_path . '/' . $plugin_name . '.php' ) ) {
-				require_once $plugin_test_path . '/' . $plugin_name . '.php';
-			} elseif ( file_exists( $plugin_test_path . '/load.php' ) ) {
-				// Check if plugin has a "plugin/load.php" file.
-				require_once $plugin_test_path . '/load.php';
-			} else {
-				echo "Unable to locate standalone plugin bootstrap file in $plugin_test_path.";
-				exit( 1 );
-			}
+		static function () use ( $plugin_name ): void {
+			require_once TESTS_PLUGIN_DIR . '/plugins/' . $plugin_name . '/includes/admin/load.php';
+			require_once TESTS_PLUGIN_DIR . '/plugins/' . $plugin_name . '/includes/admin/server-timing.php';
+			require_once TESTS_PLUGIN_DIR . '/plugins/' . $plugin_name . '/includes/admin/plugins.php';
 		},
 		1
 	);
-
-	if ( 'performance-lab' === $plugin_name ) {
-		// Add filter to ensure the plugin's admin integration is loaded for tests.
-		tests_add_filter(
-			'plugins_loaded',
-			static function () use ( $plugin_name ): void {
-				require_once TESTS_PLUGIN_DIR . '/plugins/' . $plugin_name . '/includes/admin/load.php';
-				require_once TESTS_PLUGIN_DIR . '/plugins/' . $plugin_name . '/includes/admin/server-timing.php';
-				require_once TESTS_PLUGIN_DIR . '/plugins/' . $plugin_name . '/includes/admin/plugins.php';
-			},
-			1
-		);
-	}
 }
+
 
 // Start up the WP testing environment.
 require $_test_root . '/includes/bootstrap.php';
