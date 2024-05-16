ec6fa46925dad328f8cfe86ecb3611aea515347f
Lovekesh Kumar <lovekesh.kumar@rtcamp.com>
2024-05-16 15:30:22 +0530

    Update plugin description
    Co-authored-by: Weston Ruter <westonruter@google.com>

--- performance.php
+++ performance.php
@@ -23,7 +23,7 @@ function perflab_monorepo_is_not_a_plugin() {
 	printf(
 		wp_kses(
 			/* translators: Link to the Performance Lab plugin on WordPress.org */
-			__( 'The Performance Monorepo is not a plugin, and should not be installed as one. Download the <a href="%s">Performance Lab</a> plugin to install performance features instead.', 'performance-lab' ),
+			__( 'The Performance Monorepo is not a plugin, and should not be installed as one. Download the <a href="%s">Performance Lab</a> plugin to install performance features instead. If you are developing one of the plugins, you can find them in the <code>plugins</code> directory and they should automatically have been added as plugins in wp-env. You may need to restart wp-env for the changes to take effect. If you are not using wp-env, you can add symlinks in your plugins directory to each of the plugins.', 'performance-lab' ),
 			array(
 				'a' => array( 'href' => array() ),
 			)
