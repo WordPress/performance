56e204189fc83c0437e2da4ca4c50cb08dc9f740
Weston Ruter <westonruter@google.com>
2024-05-21 14:14:30 -0700

    Initially move all image optimization logic to Image Prioritizer

--- plugins/image-prioritizer/hooks.php
+++ plugins/image-prioritizer/hooks.php
@@ -11,3 +11,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 add_action( 'wp_head', 'ip_render_generator_meta_tag' );
+
+add_filter(
+	'od_html_tag_walker_visitors',
+	'ip_filter_tag_walker_visitors',
+	10,
+	4
+);
