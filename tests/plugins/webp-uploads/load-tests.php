b9b8136b6703ef16977f068019c0ebb57c4d1071
Adam Silverstein <adamsilverstein@earthboundhosting.com>
2024-05-28 16:23:21 -0600

    Update tests/plugins/webp-uploads/load-tests.php
    Co-authored-by: Weston Ruter <westonruter@google.com>

--- tests/plugins/webp-uploads/load-tests.php
+++ tests/plugins/webp-uploads/load-tests.php
@@ -731,12 +731,12 @@ class WebP_Uploads_Load_Tests extends ImagesTestCase {
 	/**
 	 * Data provider for tests returns the supported image types to run the tests against.
 	 *
-	 * @return array<array<string>> An array of valid image types.
+	 * @return array<string, array<string>> An array of valid image types.
 	 */
 	public function data_provider_supported_image_types(): array {
 		return array(
-			array( 'webp' ),
-			array( 'avif' ),
+			'webp' => array( 'webp' ),
+			'avif' => array( 'avif' ),
 		);
 	}
 
