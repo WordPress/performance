27e362b9cc8e92e8fae8c2a0c0d441c49c051f21
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-05-31 01:53:09 +0530

    Fix WP Filesystsem Mock file path

--- plugins/performance-lab/tests/test-load.php
+++ plugins/performance-lab/tests/test-load.php
@@ -171,7 +171,7 @@ class Test_Load extends WP_UnitTestCase {
 		add_filter(
 			'filesystem_method_file',
 			static function () {
-				return __DIR__ . '/../../utils/Filesystem/WP_Filesystem_MockFilesystem.php';
+				return __DIR__ . '/data/class-wp-filesystem-mockfilesystem.php';
 			}
 		);
 		add_filter(
