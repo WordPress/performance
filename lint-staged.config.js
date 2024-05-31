f66104ae7f005ded03b0bd2101ea9793c39bf6a9
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-06-01 00:21:35 +0530

    Add lint-staged.config.js

---
+++ lint-staged.config.js
@@ -0,0 +1,60 @@
+/**
+ * External dependencies
+ */
+const path = require( 'path' );
+const micromatch = require( 'micromatch' );
+
+/**
+ * Internal dependencies
+ */
+const { plugins } = require( './plugins.json' );
+
+/**
+ * Join and escape filenames for shell.
+ *
+ * @param {string[]} files Files to join.
+ *
+ * @return {string} Joined files.
+ */
+const joinFiles = ( files ) => {
+	return files.map( ( file ) => `'${ file }'` ).join( ' ' );
+};
+
+// Get plugin base name to match regex more accurately.
+// Else it can cause issues when this plugin is places in `wp-content/plugins` directory.
+const PLUGIN_BASE_NAME = path.basename( __dirname );
+
+module.exports = {
+	'**/*.js': ( files ) => `npm run lint-js ${ joinFiles( files ) }`,
+	'**/*.php': ( files ) => {
+		const commands = [ 'composer phpstan' ];
+
+		plugins.forEach( ( plugin ) => {
+			const pluginFiles = micromatch(
+				files,
+				`**/${ PLUGIN_BASE_NAME }/plugins/${ plugin }/**`,
+				{ dot: true }
+			);
+
+			if ( pluginFiles.length ) {
+				commands.push(
+					`npm run lint:php:plugins --plugin=${ plugin } ${ joinFiles(
+						pluginFiles
+					) }`
+				);
+			}
+		} );
+
+		const otherFiles = micromatch(
+			files,
+			`!**/${ PLUGIN_BASE_NAME }/plugins/**`,
+			{ dot: true }
+		);
+
+		if ( otherFiles.length ) {
+			commands.push( `composer lint ${ joinFiles( otherFiles ) }` );
+		}
+
+		return commands;
+	},
+};
