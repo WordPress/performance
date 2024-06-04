a2e8584adee69e5203314352a25038ee42c046b1
Adam Silverstein <adamsilverstein@earthboundhosting.com>
2024-06-04 14:21:48 -0600

    Update plugins/webp-uploads/settings.php
    Co-authored-by: Weston Ruter <westonruter@google.com>

--- plugins/webp-uploads/settings.php
+++ plugins/webp-uploads/settings.php
@@ -133,9 +133,8 @@ function webp_uploads_generate_avif_webp_setting_callback(): void {
 	}
 
 	// If only one of the two formats is supported, the dropdown defaults to that type and the other type is disabled.
-	if ( $avif_supported ) {
-		$selected = 'avif';
-	} else {
+	if ( ! $avif_supported && 'avif' === $selected ) {
+		$selected = 'webp';
 		$selected = 'webp';
 	}
 	?>
