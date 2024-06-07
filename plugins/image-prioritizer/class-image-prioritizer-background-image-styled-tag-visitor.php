e9d997c97a6b06b73669844ef31ad520cd5e90ff
Weston Ruter <westonruter@google.com>
2024-06-07 11:18:40 -0700

    Fix logic error for preg_match() return value

--- plugins/image-prioritizer/class-image-prioritizer-background-image-styled-tag-visitor.php
+++ plugins/image-prioritizer/class-image-prioritizer-background-image-styled-tag-visitor.php
@@ -39,7 +39,7 @@ final class Image_Prioritizer_Background_Image_Styled_Tag_Visitor extends Image_
 		if (
 			is_string( $style )
 			&&
-			false !== preg_match( '/background(-image)?\s*:[^;]*?url\(\s*[\'"]?\s*(?<background_image>.+?)\s*[\'"]?\s*\)/', $style, $matches )
+			0 < (int) preg_match( '/background(-image)?\s*:[^;]*?url\(\s*[\'"]?\s*(?<background_image>.+?)\s*[\'"]?\s*\)/', $style, $matches )
 			&&
 			'' !== $matches['background_image'] // PHPStan should ideally know that this is a non-empty string based on the `.+?` regular expression.
 			&&
