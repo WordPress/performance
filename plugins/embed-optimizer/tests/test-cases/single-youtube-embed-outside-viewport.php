8aa7e638368a8d13f063f6b3448cec07be01f753
Weston Ruter <westonruter@google.com>
2024-07-29 16:14:37 -0700

    Set the min-height of an embed prior to it loading

--- plugins/embed-optimizer/tests/test-cases/single-youtube-embed-outside-viewport.php
+++ plugins/embed-optimizer/tests/test-cases/single-youtube-embed-outside-viewport.php
@@ -33,7 +33,7 @@ return array(
 				<title>...</title>
 			</head>
 			<body>
-				<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+				<figure data-od-added-style style="min-height: 100px;" class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
 					<div class="wp-block-embed__wrapper">
 						<iframe data-od-added-loading loading="lazy" title="Matt Mullenweg: State of the Word 2023" width="750" height="422" src="https://www.youtube.com/embed/c7M4mBVgP3Y?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 					</div>
