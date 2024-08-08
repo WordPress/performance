0b14a5bffd3ba7a160e398ac028ae978420e34af
Adam Silverstein <adamjs@google.com>
2024-08-08 10:27:59 -0600

    update doc block for webp_uploads_picture_element_mime_types filter

--- plugins/webp-uploads/picture-element.php
+++ plugins/webp-uploads/picture-element.php
@@ -44,12 +44,14 @@ function webp_uploads_wrap_image_in_picture( string $image, string $context, int
 	/**
 	 * Filter the image mime types that can be used for the <picture> element.
 	 *
-	 * Default is: ['image/avif', 'image/webp', 'image/jpeg']. Returning an empty array will skip using the `picture` element.
+	 * Default is: ['image/avif', 'image/webp', 'image/jpeg', 'image/png]. Returning an empty array will skip using the `picture` element.
 	 *
 	 * The mime types will output in the picture element in the order they are provided.
 	 * The original image will be used as the fallback image for browsers that don't support the picture element.
 	 *
 	 * @since 2.0.0
+	 * @since n.e.x.t. The 'image/png' mime type was added to the default array.
+	 *
 	 * @param string[] $mime_types    Mime types than can be used.
 	 * @param int      $attachment_id The id of the image being evaluated.
 	 */
