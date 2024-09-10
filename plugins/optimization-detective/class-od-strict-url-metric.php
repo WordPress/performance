8917ce990922a6ee28d3f482f7989df30e29926e
Weston Ruter <westonruter@google.com>
2024-09-10 13:57:01 -0700

    Explicitly fork rest_default_additional_properties_to_false()

--- plugins/optimization-detective/class-od-strict-url-metric.php
+++ plugins/optimization-detective/class-od-strict-url-metric.php
@@ -30,34 +30,50 @@ final class OD_Strict_URL_Metric extends OD_URL_Metric {
 	 * @return array<string, mixed> Schema.
 	 */
 	public static function get_json_schema(): array {
-		return self::falsify_additional_properties( parent::get_json_schema() );
+		return self::set_additional_properties_to_false( parent::get_json_schema() );
 	}
 
 	/**
 	 * Recursively processes the schema to ensure that all objects have additionalProperties set to false.
 	 *
+	 * This is a forked version of `rest_default_additional_properties_to_false()` which isn't being used itself because
+	 * it does not override `additionalProperties` to be false, but rather only sets it when it is empty.
+	 *
 	 * @since n.e.x.t
+	 * @see rest_default_additional_properties_to_false()
 	 *
 	 * @param mixed $schema Schema.
 	 * @return mixed Processed schema.
 	 */
-	private static function falsify_additional_properties( $schema ) {
+	private static function set_additional_properties_to_false( $schema ) {
 		if ( ! isset( $schema['type'] ) ) {
 			return $schema;
 		}
-		if ( 'object' === $schema['type'] ) {
-			$schema['additionalProperties'] = false;
-			if ( isset( $schema['properties'] ) && is_array( $schema['properties'] ) ) {
-				$schema['properties'] = array_map(
-					static function ( $property_schema ) {
-						return self::falsify_additional_properties( $property_schema );
-					},
-					$schema['properties']
-				);
+
+		$type = (array) $schema['type'];
+
+		if ( in_array( 'object', $type, true ) ) {
+			if ( isset( $schema['properties'] ) ) {
+				foreach ( $schema['properties'] as $key => $child_schema ) {
+					$schema['properties'][ $key ] = self::set_additional_properties_to_false( $child_schema );
+				}
 			}
-		} elseif ( 'array' === $schema['type'] && isset( $schema['items'] ) && is_array( $schema['items'] ) ) {
-			$schema['items'] = self::falsify_additional_properties( $schema['items'] );
+
+			if ( isset( $schema['patternProperties'] ) ) {
+				foreach ( $schema['patternProperties'] as $key => $child_schema ) {
+					$schema['patternProperties'][ $key ] = self::set_additional_properties_to_false( $child_schema );
+				}
+			}
+
+			$schema['additionalProperties'] = false;
 		}
+
+		if ( in_array( 'array', $type, true ) ) {
+			if ( isset( $schema['items'] ) ) {
+				$schema['items'] = self::set_additional_properties_to_false( $schema['items'] );
+			}
+		}
+
 		return $schema;
 	}
 }
