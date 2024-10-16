ca0f538a9c662c4e6f62a8c513fa2cf7b41cfe87
Pascal Birchler <pascalb@google.com>
2024-10-16 14:43:05 +0200

    Extract other logic to own method too

--- plugins/image-prioritizer/class-image-prioritizer-video-tag-visitor.php
+++ plugins/image-prioritizer/class-image-prioritizer-video-tag-visitor.php
@@ -35,17 +35,67 @@ final class Image_Prioritizer_Video_Tag_Visitor extends Image_Prioritizer_Tag_Vi
 			return false;
 		}
 
+		// TODO: If $context->url_metric_group_collection->get_element_max_intersection_ratio( $xpath ) is 0.0, then the video is not in any initial viewport and the VIDEO tag could get the preload=none attribute added.
+
+		$this->reduce_poster_image_size( $context );
+		$this->preload_poster_image( $context );
+
+		return true;
+	}
+
+	/**
+	 * Reduce poster image size by choosing one that fits the maximum video size more closely.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param OD_Tag_Visitor_Context $context Tag visitor context, with the cursor currently at an embed block.
+	 */
+	private function reduce_poster_image_size( OD_Tag_Visitor_Context $context ): void {
+		$processor = $context->processor;
+
 		// Skip empty poster attributes and data: URLs.
 		$poster = trim( (string) $processor->get_attribute( 'poster' ) );
 		if ( '' === $poster || $this->is_data_url( $poster ) ) {
-			return false;
+			return;
 		}
-
-		$this->reduce_poster_image_size( $context );
 
 		$xpath = $processor->get_xpath();
 
-		// TODO: If $context->url_metric_group_collection->get_element_max_intersection_ratio( $xpath ) is 0.0, then the video is not in any initial viewport and the VIDEO tag could get the preload=none attribute added.
+		$max_element_width = 0;
+
+		$denormalized_elements = $context->url_metric_group_collection->get_all_denormalized_elements()[ $xpath ] ?? array();
+
+		foreach ( $denormalized_elements as list( , , $element ) ) {
+			$max_element_width = max( $max_element_width, $element['boundingClientRect']['width'] ?? 0 );
+		}
+
+		$poster_id = attachment_url_to_postid( $poster );
+
+		if ( $poster_id > 0 && $max_element_width > 0 ) {
+			$smaller_image_url = wp_get_attachment_image_url( $poster_id, array( (int) $max_element_width, 0 ) );
+			$processor->set_attribute( 'poster', $smaller_image_url );
+		}
+	}
+
+	/**
+	 * Preload poster image for the LCP <video> element.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param OD_Tag_Visitor_Context $context Tag visitor context, with the cursor currently at an embed block.
+	 */
+	private function preload_poster_image( OD_Tag_Visitor_Context $context ): void {
+		$processor = $context->processor;
+
+		// Skip empty poster attributes and data: URLs.
+		$poster = trim( (string) $processor->get_attribute( 'poster' ) );
+		if ( '' === $poster || $this->is_data_url( $poster ) ) {
+			return;
+		}
+
+		$xpath = $processor->get_xpath();
 
 		// If this element is the LCP (for a breakpoint group), add a preload link for it.
 		foreach ( $context->url_metric_group_collection->get_groups_by_lcp_element( $xpath ) as $group ) {
@@ -67,36 +117,6 @@ final class Image_Prioritizer_Video_Tag_Visitor extends Image_Prioritizer_Tag_Vi
 				$group->get_minimum_viewport_width(),
 				$group->get_maximum_viewport_width()
 			);
-		}
-
-		return true;
-	}
-
-	/**
-	 * Reduce poster image size by choosing one that fits the maximum video size more closely.
-	 *
-	 * @since n.e.x.t
-	 *
-	 * @param OD_Tag_Visitor_Context $context Tag visitor context, with the cursor currently at an embed block.
-	 */
-	private function reduce_poster_image_size( OD_Tag_Visitor_Context $context ): void {
-		$processor = $context->processor;
-		$xpath     = $processor->get_xpath();
-
-		$max_element_width = 0;
-
-		$denormalized_elements = $context->url_metric_group_collection->get_all_denormalized_elements()[ $xpath ] ?? array();
-
-		foreach ( $denormalized_elements as list( , , $element ) ) {
-			$max_element_width = max( $max_element_width, $element['boundingClientRect']['width'] ?? 0 );
-		}
-
-		$poster    = trim( (string) $processor->get_attribute( 'poster' ) );
-		$poster_id = attachment_url_to_postid( $poster );
-
-		if ( $poster_id > 0 && $max_element_width > 0 ) {
-			$smaller_image_url = wp_get_attachment_image_url( $poster_id, array( (int) $max_element_width, 0 ) );
-			$processor->set_attribute( 'poster', $smaller_image_url );
 		}
 	}
 }
