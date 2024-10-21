afc3339edb79e62b5c4dcd1e054572d4e2186df7
devansh016 <devanshchaudhary2002@gmail.com>
2024-10-21 15:36:33 +0530

    Overides .yml config to use 2 spaces

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,16 @@
 // Import the default config file and expose it in the project root.
 // Useful for editor integrations.
-module.exports = require( '@wordpress/prettier-config' );
+const wpPrettierConfig = require( '@wordpress/prettier-config' );
+
+module.exports = {
+	...wpPrettierConfig,
+	overrides: [
+		{
+			files: '*.yml',
+			options: {
+				useTabs: false,
+				tabWidth: 2,
+			},
+		},
+	],
+};
