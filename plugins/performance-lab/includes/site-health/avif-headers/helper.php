0f63d26dde8e727311fde39eb29a96f86437bdee
Adam Silverstein <adamsilverstein@earthboundhosting.com>
2024-10-22 07:52:32 -0600

    Update plugins/performance-lab/includes/site-health/avif-headers/helper.php
    Co-authored-by: Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>

--- plugins/performance-lab/includes/site-health/avif-headers/helper.php
+++ plugins/performance-lab/includes/site-health/avif-headers/helper.php
@@ -39,8 +39,8 @@ function avif_headers_check_avif_headers_test(): array {
 		$result['status']  = 'recommended';
 		$result['label']   = __( 'Your site does not send AVIF image headers correctly', 'performance-lab' );
 		$result['actions'] = sprintf(
-			'<p>%s</p>',
 			/* translators: Accessibility text. */
+			'<p>%s</p>',
 			esc_html__( 'AVIF headers can be enabled with a small configuration change by your hosting provider.', 'performance-lab' )
 		);
 	}
