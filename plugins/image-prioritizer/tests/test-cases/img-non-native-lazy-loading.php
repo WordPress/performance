5b4cbc0a3aaaddf033e3d0f5476ab8ae9b176bf4
Weston Ruter <westonruter@google.com>
2025-01-09 14:43:08 -0800

    Add failing test case where data: in srcset should not be preloaded

--- plugins/image-prioritizer/tests/test-cases/img-non-native-lazy-loading.php
+++ plugins/image-prioritizer/tests/test-cases/img-non-native-lazy-loading.php
@@ -14,6 +14,10 @@ return array(
 							'xpath' => '/*[1][self::HTML]/*[2][self::BODY]/*[1][self::IMG]',
 							'isLCP' => true,
 						),
+						array(
+							'xpath' => '/*[1][self::HTML]/*[2][self::BODY]/*[1][self::IMG]',
+							'isLCP' => false,
+						),
 					),
 				)
 			)
@@ -27,6 +31,19 @@ return array(
 				<script>/* custom lazy-loading */</script>
 			</head>
 			<body>
+				<!-- Example seen in the wild which caused a preload link to be added with a data: URL in the imagesrcset attribute. -->
+				<img
+					src="https://example.com/foo.webp"
+					data-orig-src="https://example.com/foo.webp"
+					width="1000"
+					height="800"
+					class="lazyload wp-image-1"
+					srcset="data:image/svg+xml,%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%20width%3D%271000%27%20height%3D%27800%27%20viewBox%3D%270%200%201000%20800%27%3E%3Crect%20width%3D%271000%27%20height%3D%27800%27%20fill-opacity%3D%220%22%2F%3E%3C%2Fsvg%3E"
+					data-srcset="https://example.com/foo-200x91.webp 200w, https://example.com/foo-300x136.webp 300w, https://example.com/foo-400x181.webp 400w, https://example.com/foo-600x272.webp 600w, https://example.com/foo-768x348.webp 768w, https://example.com/foo-800x362.webp 800w, https://example.com/foo-1024x463.webp 1024w, https://example.com/foo-1200x543.webp 1200w, https://example.com/foo-1536x695.webp 1536w, https://example.com/foo.webp 1920w"
+					data-sizes="auto"
+					data-orig-sizes="(max-width: 767px) 100vw, 1920px"
+				>
+
 				<!-- Example with an adjoining NOSCRIPT > IMG tag which should be excluded from URL Metrics. -->
 				<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="https://example.com/bar.jpg" data-srcset="https://example.com/bar-large.jpg 1000w, https://example.com/bar-large.jpg 1000w" sizes="(max-width: 556px) 100vw, 556px" alt="Bar" class="attachment-large size-large wp-image-2 has-transparency lazyload" width="500" height="300">
 				<noscript>
@@ -43,6 +60,19 @@ return array(
 				<script>/* custom lazy-loading */</script>
 			</head>
 			<body>
+				<!-- Example seen in the wild which caused a preload link to be added with a data: URL in the imagesrcset attribute. -->
+				<img data-od-xpath="/*[1][self::HTML]/*[2][self::BODY]/*[1][self::IMG]"
+					src="https://example.com/foo.webp"
+					data-orig-src="https://example.com/foo.webp"
+					width="1000"
+					height="800"
+					class="lazyload wp-image-1"
+					srcset="data:image/svg+xml,%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%20width%3D%271000%27%20height%3D%27800%27%20viewBox%3D%270%200%201000%20800%27%3E%3Crect%20width%3D%271000%27%20height%3D%27800%27%20fill-opacity%3D%220%22%2F%3E%3C%2Fsvg%3E"
+					data-srcset="https://example.com/foo-200x91.webp 200w, https://example.com/foo-300x136.webp 300w, https://example.com/foo-400x181.webp 400w, https://example.com/foo-600x272.webp 600w, https://example.com/foo-768x348.webp 768w, https://example.com/foo-800x362.webp 800w, https://example.com/foo-1024x463.webp 1024w, https://example.com/foo-1200x543.webp 1200w, https://example.com/foo-1536x695.webp 1536w, https://example.com/foo.webp 1920w"
+					data-sizes="auto"
+					data-orig-sizes="(max-width: 767px) 100vw, 1920px"
+				>
+
 				<!-- Example with an adjoining NOSCRIPT > IMG tag which should be excluded from URL Metrics. -->
 				<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="https://example.com/bar.jpg" data-srcset="https://example.com/bar-large.jpg 1000w, https://example.com/bar-large.jpg 1000w" sizes="(max-width: 556px) 100vw, 556px" alt="Bar" class="attachment-large size-large wp-image-2 has-transparency lazyload" width="500" height="300">
 				<noscript>
