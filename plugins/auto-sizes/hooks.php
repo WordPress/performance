f9b0351238b1f7a1e374b03ad3c1d97a838ad67d
Sarthak Jaiswal <sarthak.jaiswal@rtCamp.com>
2025-02-19 01:06:28 +0530

    Add coverage ignore notation

--- plugins/auto-sizes/hooks.php
+++ plugins/auto-sizes/hooks.php
@@ -10,7 +10,6 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
-// @codeCoverageIgnoreEnd
 
 /**
  * Displays the HTML generator tag for the plugin.
@@ -42,3 +41,4 @@ add_filter( 'render_block_core/image', 'auto_sizes_filter_image_tag', 10, 3 );
 add_filter( 'render_block_core/cover', 'auto_sizes_filter_image_tag', 10, 3 );
 add_filter( 'get_block_type_uses_context', 'auto_sizes_filter_uses_context', 10, 2 );
 add_filter( 'render_block_context', 'auto_sizes_filter_render_block_context', 10, 2 );
+// @codeCoverageIgnoreEnd
