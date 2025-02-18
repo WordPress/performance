caad68ef73b9db0f60c13077c2bd48c257584085
Sarthak Jaiswal <sarthak.jaiswal@rtCamp.com>
2025-02-18 20:27:54 +0530

    Refactor code, move functions from hooks.php to helper.php for web-worker-offloading plugin

--- plugins/auto-sizes/hooks.php
+++ plugins/auto-sizes/hooks.php
@@ -23,6 +23,7 @@ function auto_sizes_render_generator(): void {
 	// Use the plugin slug as it is immutable.
 	echo '<meta name="generator" content="auto-sizes ' . esc_attr( IMAGE_AUTO_SIZES_VERSION ) . '">' . "\n";
 }
+// @codeCoverageIgnoreStart
 add_action( 'wp_head', 'auto_sizes_render_generator' );
 
 /**
@@ -42,3 +43,4 @@ add_filter( 'render_block_core/image', 'auto_sizes_filter_image_tag', 10, 3 );
 add_filter( 'render_block_core/cover', 'auto_sizes_filter_image_tag', 10, 3 );
 add_filter( 'get_block_type_uses_context', 'auto_sizes_filter_uses_context', 10, 2 );
 add_filter( 'render_block_context', 'auto_sizes_filter_render_block_context', 10, 2 );
+// @codeCoverageIgnoreEnd
