eb7cc6e0378a28dc1fb0c5de42e2f3986e08ae04
Felix Arntz <felixarntz@google.com>
2025-02-24 15:47:23 -0800

    Add support for Speculative Loading WP Core API, loading the plugin's on API implementation conditionally while maintaining compatibility on plugin-specific filters.

---
+++ plugins/speculation-rules/tests/test-speculation-rules-wp-core-api.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Tests for speculation-rules WP Core API.
+ *
+ * @package speculation-rules
+ */
+
+class Test_Speculation_Rules_WP_Core_API extends WP_UnitTestCase {
+
+	public function test_plsr_filter_speculation_rules_configuration_with_regular(): void {
+		$this->assertSame(
+			array(
+				'mode'      => 'prerender',
+				'eagerness' => 'moderate',
+			),
+			plsr_filter_speculation_rules_configuration(
+				array(
+					'mode'      => 'prefetch',
+					'eagerness' => 'conservative',
+				)
+			)
+		);
+	}
+
+	public function test_plsr_filter_speculation_rules_configuration_with_invalid(): void {
+		$this->assertSame(
+			array(
+				'mode'      => 'prerender',
+				'eagerness' => 'moderate',
+			),
+			plsr_filter_speculation_rules_configuration( 'neither-an-array-nor-null' )
+		);
+	}
+
+	public function test_plsr_filter_speculation_rules_configuration_with_null(): void {
+		$this->assertNull( plsr_filter_speculation_rules_configuration( null ) );
+	}
+
+	public function test_plsr_filter_speculation_rules_configuration_with_pretty_permalinks_filter(): void {
+		// Providing null while pretty permalinks are disabled should be respected.
+		$this->disable_pretty_permalinks();
+		$this->assertNull( plsr_filter_speculation_rules_configuration( null ) );
+
+		// Unless the filter to enable speculative loading despite that is set to true.
+		add_filter( 'plsr_enabled_without_pretty_permalinks', '__return_true' );
+		$this->assertSame(
+			array(
+				'mode'      => 'prerender',
+				'eagerness' => 'moderate',
+			),
+			plsr_filter_speculation_rules_configuration( null )
+		);
+	}
+
+	public function test_plsr_filter_speculation_rules_exclude_paths_with_regular(): void {
+		$base_rules = array( '/membership-areas/*' );
+
+		$this->assertSame( $base_rules, plsr_filter_speculation_rules_exclude_paths( $base_rules, 'prefetch' ) );
+
+		add_filter(
+			'plsr_speculation_rules_href_exclude_paths',
+			static function ( $rules ) {
+				$rules[] = '/cart/*';
+				return $rules;
+			}
+		);
+
+		$this->assertSame(
+			array_merge( $base_rules, array( '/cart/*' ) ),
+			plsr_filter_speculation_rules_exclude_paths( $base_rules, 'prefetch' )
+		);
+	}
+
+	public function test_plsr_filter_speculation_rules_exclude_paths_with_invalid(): void {
+		$this->assertSame( array( '/personalized/*' ), plsr_filter_speculation_rules_exclude_paths( '/personalized/*', 'prefetch' ) );
+	}
+
+	private function disable_pretty_permalinks(): void {
+		update_option( 'permalink_structure', '' );
+	}
+}
