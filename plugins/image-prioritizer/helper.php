777569858284699928c0bb6f4290a3f7210085c1
hbhalodia <hit.bhalodia@rtcamp.com>
2025-03-25 10:19:18 +0530

    Revert the removal of constant deprecations

--- plugins/image-prioritizer/helper.php
+++ plugins/image-prioritizer/helper.php
@@ -297,15 +297,16 @@ function image_prioritizer_validate_background_image_url( string $url ) {
 function image_prioritizer_filter_rest_request_before_callbacks( $response, array $handler, WP_REST_Request $request ) {
 	unset( $handler ); // Unused.
 
+	// Check for class existence and use constant or class method calls accordingly.
+	$route_endpoint = class_exists( 'OD_REST_URL_Metrics_Store_Endpoint' )
+						? OD_REST_URL_Metrics_Store_Endpoint::ROUTE_NAMESPACE . OD_REST_URL_Metrics_Store_Endpoint::ROUTE_BASE
+						: OD_REST_API_NAMESPACE . OD_URL_METRICS_ROUTE; // @phpstan-ignore constant.deprecated, constant.deprecated (To be replaced with class method calls in subsequent release.)
+
 	if (
 		$request->get_method() !== 'POST'
 		||
 		// The strtolower() and outer trim are due to \WP_REST_Server::match_request_to_handler() using case-insensitive pattern match and using '$' instead of '\z'.
-		(
-			OD_REST_URL_Metrics_Store_Endpoint::ROUTE_NAMESPACE . OD_REST_URL_Metrics_Store_Endpoint::ROUTE_BASE
-			!==
-			rtrim( strtolower( ltrim( $request->get_route(), '/' ) ) )
-		)
+		( rtrim( strtolower( ltrim( $request->get_route(), '/' ) ) ) !== $route_endpoint )
 	) {
 		return $response;
 	}
