085d98e8ffeff15691fbe8d45cce29d91dfaa715
Mukesh Panchal <mukeshpanchal27@gmail.com>
2025-04-01 09:45:40 +0530

    Remove unuse variable

--- plugins/auto-sizes/includes/improve-calculate-sizes.php
+++ plugins/auto-sizes/includes/improve-calculate-sizes.php
@@ -95,11 +95,10 @@ function auto_sizes_filter_image_tag( $content, array $parsed_block, WP_Block $b
 		 */
 		$filter = static function ( $sizes, $size ) use ( $block ) {
 
-			$id                       = isset( $block->attributes['id'] ) ? (int) $block->attributes['id'] : 0;
-			$alignment                = $block->attributes['align'] ?? '';
-			$width                    = isset( $block->attributes['width'] ) ? (int) $block->attributes['width'] : 0;
-			$max_alignment            = $block->context['max_alignment'] ?? '';
-			$container_relative_width = $block->context['container_relative_width'] ?? '1.0';
+			$id            = isset( $block->attributes['id'] ) ? (int) $block->attributes['id'] : 0;
+			$alignment     = $block->attributes['align'] ?? '';
+			$width         = isset( $block->attributes['width'] ) ? (int) $block->attributes['width'] : 0;
+			$max_alignment = $block->context['max_alignment'] ?? '';
 
 			/*
 			 * Update width for cover block.
