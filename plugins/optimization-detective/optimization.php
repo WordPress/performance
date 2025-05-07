f065ce67ac52dd802f2bca025b0e7f30272a62ea
Weston Ruter <weston@ruter.net>
2025-05-07 15:23:17 -0700

    Remove unnecessary string casting

--- plugins/optimization-detective/optimization.php
+++ plugins/optimization-detective/optimization.php
@@ -122,7 +122,7 @@ function od_print_disabled_reasons( array $reasons ): void {
 		wp_print_inline_script_tag(
 			sprintf(
 				'console.info( %s );',
-				(string) wp_json_encode( '[Optimization Detective] ' . $reason )
+				wp_json_encode( '[Optimization Detective] ' . $reason )
 			),
 			array( 'type' => 'module' )
 		);
