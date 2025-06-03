48271bfa8e4b07a79b81f4427dcc4956205c945f
hbhalodia <hit.bhalodia@rtcamp.com>
2025-06-03 13:44:28 +0530

    Revert update test case function

--- plugins/performance-lab/tests/includes/site-health/effective-asset-cache-headers/test-effective-asset-cache-headers.php
+++ plugins/performance-lab/tests/includes/site-health/effective-asset-cache-headers/test-effective-asset-cache-headers.php
@@ -40,14 +40,6 @@ class Test_Effective_Asset_Cache_Headers extends WP_UnitTestCase {
 
 		$tests = perflab_effective_asset_cache_headers_add_test( $tests );
 
-		// Test for local env.
-		$this->assertArrayNotHasKey( 'effective_asset_cache_headers', $tests['direct'] );
-
-		// Mock the env to production and call test again.
-		// @todo Mock wp_get_environment_type here.
-		$tests = perflab_effective_asset_cache_headers_add_test( $tests );
-
-		// Test for production env.
 		$this->assertArrayHasKey( 'effective_asset_cache_headers', $tests['direct'] );
 		$this->assertEquals( 'Effective Caching Headers', $tests['direct']['effective_asset_cache_headers']['label'] );
 		$this->assertEquals( 'perflab_effective_asset_cache_headers_assets_test', $tests['direct']['effective_asset_cache_headers']['test'] );
