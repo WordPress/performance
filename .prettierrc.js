d59caf6a9b4b184301da86abdea9ed168a471acf
devansh016 <devanshchaudhary2002@gmail.com>
2025-06-10 21:22:13 +0530

    Remove duplicate prettier config file

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-// Import the default config file and expose it in the project root.
-// Useful for editor integrations.
-const wpPrettierConfig = require( '@wordpress/prettier-config' );
-
-module.exports = {
-	...wpPrettierConfig,
-};
