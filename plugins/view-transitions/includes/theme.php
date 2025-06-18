aa075e0c6943253269627c4f7acdf8dd92e4c390
Mukesh Panchal <mukeshpanchal27@gmail.com>
2025-06-18 10:08:56 +0530

    Add missing @since annotation

--- plugins/view-transitions/includes/theme.php
+++ plugins/view-transitions/includes/theme.php
@@ -363,6 +363,8 @@ function plvt_load_view_transitions(): void {
  * to enable or enhance view transitions within the theme or plugin.
  * It should be hooked to an appropriate action to ensure the styles
  * are included in the page output.
+ *
+ * @since n.e.x.t
  */
 function plvt_print_view_transitions_admin_style(): void {
 	$options = plvt_get_stored_setting_value();
