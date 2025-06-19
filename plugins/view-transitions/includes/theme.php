26af2a7d96a5dc1bc4c45c5080d3ec61a904dc60
Mukesh Panchal <mukeshpanchal27@gmail.com>
2025-06-19 09:44:21 +0530

    Add admin file for admin related functions

--- plugins/view-transitions/includes/theme.php
+++ plugins/view-transitions/includes/theme.php
@@ -355,25 +355,3 @@ function plvt_load_view_transitions(): void {
 	wp_add_inline_script( 'plvt-view-transitions', $init_script );
 	wp_enqueue_script( 'plvt-view-transitions' );
 }
-
-/**
- * Outputs the necessary CSS styles for view transitions.
- *
- * This function is responsible for printing the required inline styles
- * to enable or enhance view transitions within the theme or plugin.
- * It should be hooked to an appropriate action to ensure the styles
- * are included in the page output.
- *
- * @since n.e.x.t
- */
-function plvt_print_view_transitions_admin_style(): void {
-	$options = plvt_get_stored_setting_value();
-	if ( ! isset( $options['enable_admin_transitions'] ) || true !== $options['enable_admin_transitions'] ) {
-		return;
-	}
-	?>
-<style>
-	@view-transition { navigation: auto; }
-</style>
-	<?php
-}
