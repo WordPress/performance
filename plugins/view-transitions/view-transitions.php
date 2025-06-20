5913727d3e161a3a0fc3c619313e26acf9584368
Jeffrey Paul <jeffpaul@hotmail.com>
2025-06-20 08:17:38 -0500

    update plugin authors to performanceteam

--- plugins/view-transitions/view-transitions.php
+++ plugins/view-transitions/view-transitions.php
@@ -6,7 +6,7 @@
  * Requires at least: 6.6
  * Requires PHP: 7.2
  * Version: 1.0.1
- * Author: WordPress Performance Team
+ * Author: performanceteam
  * Author URI: https://make.wordpress.org/performance/
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
