f582186c4bd0c8dc5a5281d400fc8c073f071e17
Aditya Dhade <aditya.dhade@rtcamp.com>
2025-07-01 00:23:13 +0530

    Skip non-blocking stylesheets with media attribute hack

--- plugins/performance-lab/includes/site-health/audit-enqueued-assets/hooks.php
+++ plugins/performance-lab/includes/site-health/audit-enqueued-assets/hooks.php
@@ -105,6 +105,11 @@ function perflab_aea_audit_blocking_assets(): void {
 				continue;
 			}
 
+			$media = $processor->get_attribute( 'media' );
+			if ( is_string( $media ) && ! (bool) preg_match( '/^(all|screen)\b/i', $media ) ) {
+				continue;
+			}
+
 			$href = $processor->get_attribute( 'href' );
 			if ( ! is_string( $href ) ) {
 				continue;
