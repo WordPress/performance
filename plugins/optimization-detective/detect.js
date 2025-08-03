fb6f0cc4e7a0785b8895112b3e59bf78a5c98624
Weston Ruter <weston@ruter.net>
2025-08-03 09:21:21 -0700

    Avoid possible error when reading groups in debug mode

--- plugins/optimization-detective/detect.js
+++ plugins/optimization-detective/detect.js
@@ -555,7 +555,7 @@ export default async function detect( {
 	const { log, warn, error } = logger;
 	compressionEnabled = gzdecodeAvailable;
 
-	if ( isDebug ) {
+	if ( isDebug && Array.isArray( urlMetricGroupCollection?.groups ) ) {
 		const allUrlMetrics = /** @type Array<UrlMetricDebugData> */ [];
 		for ( const group of urlMetricGroupCollection.groups ) {
 			for ( const otherUrlMetric of group.url_metrics ) {
