21a89a8dc9ba333ed93d8cf05644a11afee29652
Weston Ruter <weston@ruter.net>
2026-01-06 13:01:56 -0800

    Use npm wrapper for lint-json in lint-staged
    Co-authored-by: Aditya Dhade <76063440+b1ink0@users.noreply.github.com>

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -13,7 +13,7 @@ const { plugins } = require( './plugins.json' );
  */
 const config = {
 	'**/*.{js,ts,mjs}': [ 'npm run lint-js', () => 'npm run tsc' ],
-	'**/*.json': [ 'node bin/validate-json-schema.js' ],
+	'**/*.json': [ 'npm run lint-json' ],
 	'**/*.php': () => 'composer phpstan',
 	'*.php': 'composer lint',
 	'/tools/**.php': 'composer lint',
