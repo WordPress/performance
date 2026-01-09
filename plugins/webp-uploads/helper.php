d073f860f584f16fcdca03f485b9f7542c6d24ae
Weston Ruter <weston@ruter.net>
2026-01-09 15:15:18 -0800

    Use null coalescing operator in ternaries

--- plugins/webp-uploads/helper.php
+++ plugins/webp-uploads/helper.php
@@ -156,7 +156,7 @@ function webp_uploads_generate_additional_image_source( int $attachment_id, stri
 
 	$height = isset( $size_data['height'] ) ? (int) $size_data['height'] : 0;
 	$width  = isset( $size_data['width'] ) ? (int) $size_data['width'] : 0;
-	$crop   = isset( $size_data['crop'] ) ? $size_data['crop'] : false;
+	$crop   = $size_data['crop'] ?? false;
 	if ( $width <= 0 && $height <= 0 ) {
 		return new WP_Error( 'image_wrong_dimensions', __( 'At least one of the dimensions must be a positive number.', 'webp-uploads' ) );
 	}
