5331e166fa1c98e826d40ee818926ff3b600edfc
Weston Ruter <weston@ruter.net>
2026-02-26 14:55:02 -0800

    Update normalization logic to handle change to attribute order

--- plugins/optimization-detective/tests/class-optimization-detective-test-helpers.php
+++ plugins/optimization-detective/tests/class-optimization-detective-test-helpers.php
@@ -317,10 +317,12 @@ trait Optimization_Detective_Test_Helpers {
 
 		// TODO: Once WP 6.7 is the minimum-supported version, replace this with WP_HTML_Tag_Processor::set_modifiable_text().
 		$buffer = preg_replace_callback(
-			'#(<script type="application/json" id="optimization-detective-detect-args">)(.+?)(</script>)#s',
+			'#<script (?:type="application/json" id="optimization-detective-detect-args"|id="optimization-detective-detect-args" type="application/json")>(.+?)(</script>)#s',
 			static function ( $matches ) {
 				array_shift( $matches );
-				list( $start_tag, $text, $end_tag ) = $matches;
+				list( $text, $end_tag ) = $matches;
+
+				$start_tag = '<script type="application/json" id="optimization-detective-detect-args">';
 
 				$data = json_decode( $text, true );
 				if ( is_array( $data ) && 2 === count( $data ) && is_string( $data[0] ) && is_array( $data[1] ) ) {
