a7f0b489ea7cc8c26d4be3802bdcd8b7ad3d2902
Mukesh Panchal <mukeshpanchal27@gmail.com>
2026-07-03 13:40:33 +0530

    Remove wp_is_mobile check

--- plugins/auto-sizes/includes/improve-calculate-sizes.php
+++ plugins/auto-sizes/includes/improve-calculate-sizes.php
@@ -358,10 +358,7 @@ function auto_sizes_filter_render_block_context( array $context, array $block, ?
 	}
 
 	if ( 'core/gallery' === $block['blockName'] ) {
-		if ( wp_is_mobile() ) {
-			// On mobile, the gallery is always 2 columns.
-			$context['gallery_column_count'] = 2;
-		} elseif ( isset( $block['attrs']['columns'] ) && is_numeric( $block['attrs']['columns'] ) && (int) $block['attrs']['columns'] > 0 ) {
+		if ( isset( $block['attrs']['columns'] ) && is_numeric( $block['attrs']['columns'] ) && (int) $block['attrs']['columns'] > 0 ) {
 			$context['gallery_column_count'] = (int) $block['attrs']['columns'];
 		} else {
 			/*
