7b383b9fd245b5e6428bbcc570239d2ae5b3aafc
Weston Ruter <weston@ruter.net>
2026-07-03 11:30:55 -0700

    Account for other plugins failing to return strings from filter callback

--- plugins/webp-uploads/hooks.php
+++ plugins/webp-uploads/hooks.php
@@ -582,15 +582,18 @@ function webp_uploads_filter_image_tag( string $filtered_image, string $context,
  *
  * @since 2.7.0
  *
- * @param string                   $html          HTML img element or empty string on failure.
+ * @param string|mixed             $html          HTML img element or empty string on failure.
  * @param int|numeric-string       $attachment_id Image attachment ID.
  * @param string|array{int, int}   $size          Requested image size.
- * @param bool                     $icon          Whether the image should fall back to a mime type icon.
+ * @param mixed                    $icon          Unused.
  * @param array<string, string>|'' $attr          Array of attribute values for the image markup, keyed by attribute name. An empty string if {@see wp_get_attachment_image_src()} did not return an image.
  * @phpstan-param int<1, max> $attachment_id
  * @return string The filtered HTML.
  */
-function webp_uploads_filter_wp_get_attachment_image( string $html, $attachment_id, $size, bool $icon, $attr ): string {
+function webp_uploads_filter_wp_get_attachment_image( $html, $attachment_id, $size, $icon, $attr ): string {
+	if ( ! is_string( $html ) ) {
+		$html = '';
+	}
 	$attachment_id = (int) $attachment_id;
 	if ( '' === $html || 0 === $attachment_id || true === $icon || ! webp_uploads_in_frontend_body() ) {
 		return $html;
